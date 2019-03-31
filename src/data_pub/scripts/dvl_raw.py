a81b9ac3ca17e6b21532ffbd66288e3ca5346f83
David Miron <dam80@duke.edu>
2019-03-31 13:17:26 -0700

    Add serial definition and printing out raw data as string

--- src/data_pub/scripts/dvl_raw.py
+++ src/data_pub/scripts/dvl_raw.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
+import serial
 import rospy
 from std_msgs.msg import String
 
 def do():
-	pub = rospy.Publisher('testtopic', String, queue_size=10)
-	rospy.init_node('davids_node')
-	rate = rospy.Rate(3)
-	while not rospy.is_shutdown():
-		pub.publish("Hello!")
-		rate.sleep()
+    s = serial.Serial('/dev/ttyUSB1', 9600, 
+            timeout=0.1, write_timeout=1.0,
+            bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE)
+    pub = rospy.Publisher('testtopic', String, queue_size=10)
+    rospy.init_node('davids_node')
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+    	pub.publish(s.readline())
+    	rate.sleep()
 
 if __name__ == '__main__':
-	try:
-		do()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+    	do()
+    except rospy.ROSInterruptException:
+    	pass
