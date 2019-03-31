519273ac242b95dd8b0ad49c57eb2063bd0d2137
David Miron <dam80@duke.edu>
2019-03-31 16:29:31 -0700

    Change DVL to DVLRaw, start implementing parse functions

--- src/data_pub/scripts/dvl_raw.py
+++ src/data_pub/scripts/dvl_raw.py
@@ -3,8 +3,11 @@
 import serial
 import serial.tools.list_ports as list_ports
 import rospy
+import tf.transformations as transform
+
 from std_msgs.msg import String
 from underwater_sensor_msgs.msg import DVL
+from data_pub.msg import DVLRaw
 
 class DvlRawPublisher:
 
@@ -12,11 +15,12 @@ class DvlRawPublisher:
     BAUDRATE = 9600
     TOPIC_NAME = 'dvl_raw'
     NODE_NAME = 'dvl_raw_publisher'
+    LINE_DELIM = ','
 
     def __init__(self):
-        self._pub = rospy.Publisher(self.TOPIC_NAME, String, queue_size=10)
+        self._pub = rospy.Publisher(self.TOPIC_NAME, DVLRaw, queue_size=10)
         
-        self._current_message = DVL()
+        self._current_msg = DVLRaw()
 
         self._serial_port = None
         self._serial = None
@@ -46,19 +50,38 @@ class DvlRawPublisher:
                 self._parse_line(line)
 
     def _parse_line(self, line):
-        rospy.logdebug('Parsing line %s', line)
         data_type = line[1:3]
-        self._dvl_line_parsers[data_type](data_type)
-        self._pub.publish(line)
+        self._dvl_line_parsers[data_type](self._clean_line(line))
+
+    def _clean_line(self, line):
+        return line[4:].replace('\r\n', '')
 
     def _parse_SA(self, line):
-        pass
+        fields = self._extract_floats(line, 0, None)
+        quat = transform.quaternion_from_euler(fields[1], 
+                fields[0], 
+                fields[2])
+        self._current_msg.orientation.x = quat[0]
+        self._current_msg.orientation.y = quat[1]
+        self._current_msg.orientation.z = quat[2]
+        self._current_msg.orientation.w = quat[3]
 
     def _parse_TS(self, line):
-        pass
+        fields = self._extract_floats(line, 1, None)
+        self._current_msg.dvl.salinity = fields[0]
+        self._current_msg.dvl.temperature = fields[1]
+        self._current_msg.dvl.depth = fields[2]
+        self._current_msg.dvl.sound_speed = fields[3]
+        self._current_msg.dvl.test = int(fields[4])
 
     def _parse_BI(self, line):
-        pass
+        fields = self._extract_floats(line, 0, 4)
+        self._current_msg.dvl.bi_x_axis = fields[0]
+        self._current_msg.dvl.bi_y_axis = fields[1]
+        self._current_msg.dvl.bi_z_axis = fields[2]
+        self._current_msg.dvl.bi_error = fields[3]
+        self._current_msg.dvl.bi_status = line.split(self.LINE_DELIM)[4]
+        self._publish_current_msg()
 
     def _parse_BS(self, line):
         pass
@@ -69,6 +92,16 @@ class DvlRawPublisher:
     def _parse_BD(self, line):
         pass
 
+    def _extract_floats(self, num_string, start, stop):
+        return [float(num) 
+            for num 
+            in num_string.split(self.LINE_DELIM)[start:stop]]
+    
+    def _publish_current_msg(self):
+        """Publish the current DVL message and set the message to empty
+        """
+        self._pub.publish(self._current_msg)
+        self._current_msg = DVLRaw()
 
 
 
