4c7f3b5de64c37d91469f85d6f411389318300d2
David Miron <dam80@duke.edu>
2019-04-19 12:52:04 -0700

    Change order of rpy given to quaternion, change child_frame_id from base_link to dvl

--- src/data_pub/scripts/dvl_to_odom.py
+++ src/data_pub/scripts/dvl_to_odom.py
@@ -22,7 +22,7 @@ def callback(msg):
     y = 0
     z = 0
 
-    # bs velocity
+    # bs velocity, normalized to meters (given in mm)
     vx = np.float64(msg.bs_transverse) / 1000
     vy = np.float64(msg.bs_longitudinal) / 1000
     vz = np.float64(msg.bs_normal) / 1000
@@ -31,11 +31,11 @@ def callback(msg):
     roll = np.float64(msg.sa_roll)
     pitch = np.float64(msg.sa_pitch)
     yaw = np.float64(msg.sa_heading)
-    odom_quat = quaternion_from_euler(yaw,pitch,roll)
+    odom_quat = quaternion_from_euler(roll, pitch, yaw)
 
     # set pose
     odom.pose.pose = Pose(Point(x, y, z), Quaternion(*odom_quat))
-    odom.child_frame_id = "base_link"
+    odom.child_frame_id = "dvl"
     # set twist (angular velocity to 0,0,0)
     odom.twist.twist = Twist(Vector3(vx, vy, vz), Vector3(0, 0, 0))
     odom_pub.publish(odom)
