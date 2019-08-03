8e3209772ea4ebc110f038dcd8240b36a4deb971
David <dam80@duke.edu>
2019-08-03 11:54:38 -0400

    Work on tasks
    Former-commit-id: 7bac3d9851926985209dce989f6df0adf4f80b2a

--- src/task_planning/scripts/tasks/to_buoy_level.py
+++ src/task_planning/scripts/tasks/to_buoy_level.py
@@ -6,17 +6,16 @@ import rospy
 
 class ToBuoyLevelTask(TaskBase):
 
-    DELTA_Z = -1
+    DELTA_Z = -0.25
 
     def __init__(self):
         super(ToBuoyLevelTask, self).__init__('buoylevel')
-        self.state = 0
 
     def pre_run(self):
         self.global_target_pose = self.get_global_target_pose_from_task_start(0, 0, self.DELTA_Z, 0, 0, 0)
 
     def run(self):
-        if rospy.Time.now() - self.time_start > rospy.Duration(10):
+        if rospy.Time.now() - self.time_start > rospy.Duration(6):
             return self.FINISHED
 
         result = self.move_to_point(self.global_target_pose)
