f75f73f40c163f07b7999026410218dae96f6f38
David <dam80@duke.edu>
2019-08-03 11:32:25 -0400

    Fix spin, yaw pid
    Former-commit-id: 9229c3d47916efdc87623a5a6d3630bbe602abff

--- src/task_planning/scripts/tasks/spin.py
+++ src/task_planning/scripts/tasks/spin.py
@@ -5,11 +5,11 @@ import rospy
 
 class SpinTask(TaskBase):
 
-    TURN_TIME = 6
+    TURN_TIME = 3
 
     def __init__(self):
         super(SpinTask, self).__init__('spin')
-        self.state = 0
+        self.state_num = 0
 
     def pre_run(self):
         self.global_target_pose_0 = self.get_global_target_pose_from_task_start(0, 0, 0, 0, 0, 0)
@@ -18,24 +18,31 @@ class SpinTask(TaskBase):
 
     def run(self):
 
-        if self.state is 0:
+        result = None
+        if self.state_num is 0:
             result = self.move_to_point(self.global_target_pose_1)
-        elif self.state is 1:
+        elif self.state_num is 1:
             result = self.move_to_point(self.global_target_pose_2)
-        elif self.state is 2:
+        elif self.state_num is 2:
             result = self.move_to_point(self.global_target_pose_0)
-        elif self.state is 3:
+        elif self.state_num is 3:
             result = self.move_to_point(self.global_target_pose_1)
-        elif self.state is 4:
+        elif self.state_num is 4:
             result = self.move_to_point(self.global_target_pose_2)
-        elif self.state is 5:
+        elif self.state_num is 5:
             result = self.move_to_point(self.global_target_pose_0)
 
         # Update state
-        if result or rospy.Time.now() - (self.time_start + rospy.Duration(self.TURN_TIME * self.state)) > self.TURN_TIME:
-            if self.state is 5:
+        if result or rospy.Time.now() - (self.time_start + rospy.Duration(self.turn_time_for_state() * self.state_num)) > rospy.Duration(self.turn_time_for_state()):
+            if self.state_num is 5:
                 return self.FINISHED
-            self.state += 1
+            self.state_num += 1
             return self.CONTINUE
 
         return self.CONTINUE
+
+    def turn_time_for_state(self):
+        if self.state_num != 5:
+            return self.TURN_TIME
+        else:
+            return 10
