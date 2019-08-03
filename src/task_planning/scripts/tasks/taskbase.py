d236d13409399bcd12282f84069d8c88e7026a56
David <dam80@duke.edu>
2019-08-03 03:17:36 -0400

    Debugging movement
    Former-commit-id: 2af39317965167bafce62b6e4d6ec8f82ceced9b

--- src/task_planning/scripts/tasks/taskbase.py
+++ src/task_planning/scripts/tasks/taskbase.py
@@ -3,6 +3,7 @@
 from enum import Enum
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
+from tf.transformations import euler_from_quaternion
 import rospy
 import numpy as np
 import tf2_ros
@@ -13,6 +14,7 @@ class TaskBase(object):
     FINISHED = 2
 
     AT_POINT_MARGIN = 0.05
+    AT_ANGLE_MARGIN = 0.1
     
     def __init__(self, name):
         self.name = name
@@ -38,7 +40,7 @@ class TaskBase(object):
     def move_to_point(self, location):
         if self.dist_from_self(location.pose.position.x,
                                location.pose.position.y,
-                               location.pose.position.z) < self.AT_POINT_MARGIN:
+                               location.pose.position.z) < self.AT_POINT_MARGIN and self.angle_error_ok(location.pose.orientation):
             return True
 
         self._desired_state_pub.publish(location)
@@ -48,6 +50,17 @@ class TaskBase(object):
         self_point = np.array([self.state.pose.pose.position.x, self.state.pose.pose.position.y, self.state.pose.pose.position.z])
         other_point = np.array([x, y, z])
         return np.linalg.norm(other_point - self_point)
+    
+    def angle_error_ok(self, desired_orientation):
+        goal_rpy = euler_from_quaternion([desired_orientation.x, desired_orientation.y, desired_orientation.z, desired_orientation.w])
+        self_rpy = euler_from_quaternion([self.state.pose.pose.orientation.x, self.state.pose.pose.orientation.y, self.state.pose.pose.orientation.z, self.state.pose.pose.orientation.w])
+        errors = np.array(goal_rpy) - np.array(self_rpy)
+        for err in errors:
+            if err > self.AT_ANGLE_MARGIN:
+                return False
+        return True
+
+
 
 
 class TaskResult(Enum):
