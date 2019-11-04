9209c02bfcddf72e291d610e9fd0db94a517f5e1
Neil Dhar <pranks16@gmail.com>
2019-11-03 22:34:42 -0500

    nevermind, python2, I've been a fool

--- src/avt_camera/scripts/mono_camera.py
+++ src/avt_camera/scripts/mono_camera.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python
 
-from pymba import Vimba, __version__
+from pymba import *
 import rospy
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+from time import sleep
+import sys
 class MonoCamera:
     TOPIC_NAME = 'mono_camera/raw'
     NODE_NAME = 'mono_camera'
@@ -16,23 +18,71 @@ class MonoCamera:
         self.TIMEOUT = rospy.get_param('~timeout', 10)
 
     def run(self):
-        rate = rospy.Rate(1/self.TIMEOUT)
-        with Vimba() as vimba:
-            camera = vimba.camera(0)
-            camera.open()
-
-            # arm the camera and provide a function to be called upon frame ready
-            camera.arm('Continuous', publish_frame)
-            camera.start_frame_acquisition()
-            while not rospy.is_shutdown():
-                rospy.spin()
-            camera.stop_frame_acquisition()
-            camera.disarm()
-            camera.close()
-    def publish_frame(frame):
-        image = frame.buffer_data_numpy()
-        self._pub.publish(self._bridge.cv2_to_imgmsg(cv_image, "bgr8"))
         
+        with Vimba() as vimba:
+            system = vimba.getSystem()
+            system.runFeatureCommand("GeVDiscoveryAllOnce")
+            sleep(0.2)
+
+            camera_ids = vimba.getCameraIds()
+            if not camera_ids:
+                rospy.logerr("No cameras found, big RIP")
+                sys.exit(0)
+
+
+            for cam_id in camera_ids:
+                rospy.loginfo("Camera found: ", cam_id)
+
+            c0 = vimba.getCamera(camera_ids[0])
+            c0.openCamera()
+
+            try:
+                #gigE camera
+                print("Packet size:", c0.GevSCPSPacketSize)
+                c0.StreamBytesPerSecond = 100000000
+                print("BPS:", c0.StreamBytesPerSecond)
+            except:
+                #not a gigE camera
+                pass
+
+            #set pixel format
+            c0.PixelFormat = "BGR8Packed"  # OPENCV DEFAULT
+            c0.AcquisitionMode = "Continuous"
+            sleep(0.2)
+            frame = c0.getFrame()
+            frame.announceFrame()
+
+            c0.startCapture()
+
+            framecount = 0
+            droppedframes = []
+            
+            c0.runFeatureCommand("AcquisitionStart")
+
+            rate = rospy.Rate(1/self.TIMEOUT)
+            while not rospy.is_shutdown():
+                try:
+                    frame.queueFrameCapture()
+                    success = True
+                except:
+                    droppedframes.append(framecount)
+                    success = False
+                
+                
+                frame.waitFrameCapture(1000)
+                frame_data = frame.getBufferByteData()
+                
+                if success:
+                    img = np.ndarray(buffer=frame_data,
+                                    dtype=np.uint8,
+                                    shape=(frame.height, frame.width, frame.pixel_bytes))
+                    self._pub.publish(self._bridge.cv2_to_imgmsg(img, "bgr8"))
+                rospy.sleep(rate)
+            c0.runFeatureCommand("AcquisitionStop")
+            c0.endCapture()
+            c0.revokeAllFrames()
+            c0.closeCamera()
+
 if __name__ == '__main__':
     try:
     	MonoCamera().run()
