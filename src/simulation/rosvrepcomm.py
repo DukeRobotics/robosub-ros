e9f9ccf5c0a437414e4043bf16b1624bdcfb862a
puzzler7 <maverick.chung@duke.edu>
2019-11-16 16:53:37 -0500

    communicating ros data from vrep to vrep

--- src/simulation/rosvrepcomm.py
+++ src/simulation/rosvrepcomm.py
@@ -46,17 +46,11 @@ if clientID!=-1 and winID!=-1:
         #returnCode,data=vrep.simxGetIntegerParameter(clientID,vrep.sim_intparam_mouse_x,vrep.simx_opmode_buffer) # Try to retrieve the streamed data
         #if returnCode==vrep.simx_return_ok: # After initialization of streaming, it will take a few ms before the first value arrives, so check the return code
         #    print ('Mouse position x: ',data) # Mouse position x is actualized when the cursor is over V-REP's window
-        res,objs=vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_streaming)
-        res,wobjs=vrep.simxGetObjects(winID,vrep.sim_handle_all,vrep.simx_opmode_streaming)
-        print (objs)
-        print (wobjs)
-        for i in range(len(objs)):
-            if i >=len(wobjs):
-                break
-            res,pos = vrep.simxGetObjectPosition(clientID, objs[i],-1,vrep.simx_opmode_streaming)
-            vrep.simxSetObjectPosition(winID, wobjs[i],-1,pos,vrep.simx_opmode_streaming)
-            res,quat = vrep.simxGetObjectQuaternion(clientID, objs[i],-1,vrep.simx_opmode_streaming)
-            vrep.simxSetObjectQuaternion(winID, wobjs[i],-1,quat,vrep.simx_opmode_streaming)
+        res, blah, data, blah, blah = vrep.simxCallScriptFunction(clientID, "Cuboid", vrep.sim_scripttype_childscript,"get_ros_data", 
+                                                                  [],[],[],bytearray(),vrep.simx_opmode_blocking)
+        print(data)
+        res, blah, blah, blah, blah = vrep.simxCallScriptFunction(winID, "Cuboid", vrep.sim_scripttype_childscript, "read_ros_data",
+                                                                  [],data,[],bytearray(),vrep.simx_opmode_blocking)
         time.sleep(0.005)
 
     # Now send some data to V-REP in a non-blocking fashion:
@@ -69,4 +63,6 @@ if clientID!=-1 and winID!=-1:
     vrep.simxFinish(clientID)
 else:
     print ('Failed connecting to remote API server')
+    print (clientID)
+    print(winID)
 print ('Program ended')
