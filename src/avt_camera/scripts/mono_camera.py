6e3ea41444b7309274e07481ae0ec69e18fc8596
Neil Dhar <neil.dhar@duke.edu>
2019-11-16 22:36:51 -0500

    Basic working camera code

--- src/avt_camera/scripts/mono_camera.py
+++ src/avt_camera/scripts/mono_camera.py
@@ -5,6 +5,7 @@ import rospy
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from time import sleep
+import numpy as np
 import sys
 class MonoCamera:
     TOPIC_NAME = 'mono_camera/raw'
@@ -15,40 +16,41 @@ class MonoCamera:
         rospy.init_node(self.NODE_NAME)
         self._pub = rospy.Publisher(self.TOPIC_NAME, Image, queue_size=10)
         self._bridge = CvBridge()
-        self.TIMEOUT = rospy.get_param('~timeout', 10)
 
     def run(self):
         
         with Vimba() as vimba:
             system = vimba.getSystem()
             system.runFeatureCommand("GeVDiscoveryAllOnce")
-            sleep(0.2)
+            rospy.sleep(0.1)
 
             camera_ids = vimba.getCameraIds()
             if not camera_ids:
                 rospy.logerr("No cameras found, big RIP")
                 sys.exit(0)
 
-
             for cam_id in camera_ids:
-                rospy.loginfo("Camera found: ", cam_id)
+                rospy.loginfo("Camera found: " + str(cam_id))
 
             c0 = vimba.getCamera(camera_ids[0])
             c0.openCamera()
 
             try:
                 #gigE camera
-                print("Packet size:", c0.GevSCPSPacketSize)
+                rospy.loginfo("Packet Size: " + str(c0.GevSCPSPacketSize))
+                rospy.loginfo("Stream Bytes Per Second: " + str(c0.StreamBytesPerSecond))
+                c0.runFeatureCommand("GVSPAdjustPacketSize")
                 c0.StreamBytesPerSecond = 100000000
-                print("BPS:", c0.StreamBytesPerSecond)
             except:
                 #not a gigE camera
                 pass
 
             #set pixel format
-            c0.PixelFormat = "BGR8Packed"  # OPENCV DEFAULT
+            c0.PixelFormat="Mono8"
             c0.AcquisitionMode = "Continuous"
-            sleep(0.2)
+            c0.ExposureAuto = "Continuous"
+            c0.Width = 1210
+            c0.Height = 760
             frame = c0.getFrame()
             frame.announceFrame()
 
@@ -57,27 +59,15 @@ class MonoCamera:
             framecount = 0
             droppedframes = []
             
-            c0.runFeatureCommand("AcquisitionStart")
-
-            rate = rospy.Rate(1/self.TIMEOUT)
+            c0.runFeatureCommand("AcquisitionStart")  
             while not rospy.is_shutdown():
-                try:
-                    frame.queueFrameCapture()
-                    success = True
-                except:
-                    droppedframes.append(framecount)
-                    success = False
-                
-                
-                frame.waitFrameCapture(1000)
+                frame.queueFrameCapture()
+                frame.waitFrameCapture()
                 frame_data = frame.getBufferByteData()
-                
-                if success:
-                    img = np.ndarray(buffer=frame_data,
-                                    dtype=np.uint8,
-                                    shape=(frame.height, frame.width, frame.pixel_bytes))
-                    self._pub.publish(self._bridge.cv2_to_imgmsg(img, "bgr8"))
-                rospy.sleep(rate)
+                img = np.ndarray(buffer=frame_data,
+                                dtype=np.uint8,
+                                shape=(frame.height, frame.width, frame.pixel_bytes))
+                self._pub.publish(self._bridge.cv2_to_imgmsg(img, "mono8"))
             c0.runFeatureCommand("AcquisitionStop")
             c0.endCapture()
             c0.revokeAllFrames()
