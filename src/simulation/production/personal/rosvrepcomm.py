184cae084b8a1ef101d4fb09ecff5f8ec6d41dd2
Jake Heller <jake.heller@duke.edu>
2020-01-18 13:01:38 -0500

    local changes that should be overwritten

--- src/simulation/production/personal/rosvrepcomm.py
+++ src/simulation/production/personal/rosvrepcomm.py
@@ -25,7 +25,7 @@ import time
 
 print ('Program started')
 vrep.simxFinish(-1) # just in case, close all opened connections
-clientID=vrep.simxStart('192.168.99.100',8080,True,True,5000,5) # Connect to V-REP
+clientID=vrep.simxStart('127.0.0.1',8080,True,True,5000,5) # Connect to V-REP
 winID=vrep.simxStart('127.0.0.1',20000,True,True,5000,5)
 if clientID!=-1 and winID!=-1:
     print ('Connected to remote API server')
