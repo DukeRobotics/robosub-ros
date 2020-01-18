f73ac3266de20bbaa9185e23d8c7964c0e53abec
Noah Citron <noah@jeff.org>
2020-01-18 13:28:58 -0500

    conforms to requirements of control group

--- src/motion_planning/scripts/moveTasks.py
+++ src/motion_planning/scripts/moveTasks.py
@@ -1,11 +1,11 @@
 import rospy
 from task import Task
-from geometry_msgs.msg import PoseStamped, TwistStamped
+from geometry_msgs.msg import Pose
 from tf.transformations import quaternion_from_euler
 
 class MoveRelative(Task):
 
-    DESIRED_POSE_TOPIC = 'poseTopic'  # actual name TBD
+    DESIRED_POSE_TOPIC = 'controls/desired_pose_global'
     NODE_NAME = 'moveTask'  # TBD
 
     def __init__(self, x, y, z, orientation_x, orientation_y, orientation_z, orientation_w):
@@ -21,29 +21,32 @@ class MoveRelative(Task):
     def _initialize(self):
         rospy.init_node(self.NODE_NAME, anonymous=True)
         Task._initialize(self)
-        self.posePublisher = rospy.Publisher(self.DESIRED_POSE_TOPIC, PoseStamped, queue_size=3)
+        self.posePublisher = rospy.Publisher(self.DESIRED_POSE_TOPIC, Pose, queue_size=3)
+        rospy.init_node(self.NODE_NAME, anonymous=True)
         self.rate = rospy.Rate(10)
 
     def _run_task(self):
-        pose = PoseStamped()
-        pose.header.stamp = rospy.Time.now()
-        pose.header.frame_id = ""
-        pose.pose.position.x = self.x
-        pose.pose.position.y = self.y
-        pose.pose.position.z = self.z
+        self.pose = Pose()
+        #pose.header.stamp = rospy.Time.now()
+        #pose.header.frame_id = ""
+        self.pose.position.x = self.x
+        self.pose.position.y = self.y
+        self.pose.position.z = self.z
 
         quaternion = quaternion_from_euler(self.orientation_x,
                                             self.orientation_y,
-                                            self.orientation_z)
-        
-	pose.pose.orientation.x = quaternion[0]
-        pose.pose.orientation.y = quaternion[1]
-        pose.pose.orientation.z = quaternion[2]
-        pose.pose.orientation.w = quaternion[3]
+                                            self.orientation_z,
+                                            self.orientation_w])
 
-	while(True):
-        	self.posePublisher.publish(pose)
+        self.pose.orientation.x = quaternion[0]
+        self.pose.orientation.y = quaternion[1]
+        self.pose.orientation.z = quaternion[2]
+        self.pose.orientation.w = quaternion[3]
 
-move = MoveRelative(1,0,0,0,0,0,0)
+        while not rospy.is_shutdown():
+            self.posePublisher.publish(self.pose)
+            self.rate.sleep()
+
+move = MoveRelative(1, 0, 0, 0, 0, 0, 0)
 move._initialize()
 move._run_task()
