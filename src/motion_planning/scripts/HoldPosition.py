a61806bf9ce8fa0d1a1bdea05cb1f389e3093baf
echang264 <eric.t.chang@duke.edu>
2020-01-21 19:44:07 -0500

    fix _run_task parameters

--- src/motion_planning/scripts/HoldPosition.py
+++ src/motion_planning/scripts/HoldPosition.py
@@ -2,22 +2,23 @@ import rospy
 from geometry_msgs.msg import Pose, Twist, Vector3
 
 class HoldPosition(Task):
-    def _task_run(self, seconds_to_hold=None):
-        """Hold robot position and orientation for a given time in seconds. If duration not provided, hold indefinitely"""
+    """Hold robot position and orientation for a given time in seconds. If duration not provided, hold indefinitely"""
 
+    def __init__(self, seconds_to_hold=None):
         self.hold_pose = self.state.pose
         self.hold_twist = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+        self.seconds_to_hold = seconds_to_hold
 
-        if (seconds_to_hold==None):
+    def _task_run(self):
+        if (self.seconds_to_hold==None):
             while (True):
-                self.publish_desired_pose_global(hold_pose)
-                self.publish_desired_twist_global(hold_twist)
-                self.publish_desired_twist_local(hold_twist)
-
+                self.publish_desired_pose_global(self.hold_pose)
+                self.publish_desired_twist_global(self.hold_twist)
+                self.publish_desired_twist_local(self.hold_twist)
         self.hold_start_time = rospy.get_time()
-        while (seconds_to_hold > (rospy.get_time() - self.hold_start_time)):
-            self.publish_desired_pose_global(hold_pose)
-            self.publish_desired_twist_global(hold_twist)
-            self.publish_desired_twist_local(hold_twist)
+        while (self.seconds_to_hold > (rospy.get_time() - self.hold_start_time)):
+            self.publish_desired_pose_global(self.hold_pose)
+            self.publish_desired_twist_global(self.hold_twist)
+            self.publish_desired_twist_local(self.hold_twist)
 
         self.finish()
