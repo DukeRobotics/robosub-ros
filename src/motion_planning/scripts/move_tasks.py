2e5df067c41317696bbc10e7b05b9a8bf8ee1028
Jordan Schwartz <1jordanschwartz@gmail.com>
2020-01-21 20:57:51 -0500

    move relative

--- src/motion_planning/scripts/move_tasks.py
+++ src/motion_planning/scripts/move_tasks.py
@@ -1,22 +1,25 @@
 from task import Task
 from geometry_msgs.msg import Pose
 from tf.transformations import quaternion_from_euler
+import tf
 
 class MoveToPoseGlobalTask(Task):
 
-	def __init__(self, x, y, z, roll, pitch, yaw):
-		self.x = x
-		self.y = y
-		self.z = z
-		self.roll = roll
-		self.pitch = pitch
-		self.yaw = yaw
+    def __init__(self, x, y, z, roll, pitch, yaw):
+    	self.desired_pose = Pose()
+    	self.desired_pose.point.x = x
+    	self.desired_pose.point.y = y
+    	self.desired_pose.point.z = z
+    	self.desired_pose.quaternion = quaternion_from_euler(roll, pitch, yaw)
 
-		self.desired_pose = Pose()
-		desired_pose.point.x = x
-		desired_pose.point.y = y
-		desired_pose.point.z = z
-		desired_pose.quaternion = quaternion_from_euler(roll, pitch, yaw)
+    def _task_run(self):
+    	self.publish_desired_pose_global(self.desired_pose)
 
-	def _task_run(self):
-		self.publish_desired_pose_global(self.desired_pose)
+class MoveToPoseLocalTask(MoveToPoseGlobalTask):
+
+    def __init__(self):
+        self.transformListener = tf.TransformListener()
+        self.transformed_pose = TransformListener.transformPose(self.desired_pose, self.state.pose)
+
+    def _task_run(self):
+        self.publish_desired_pose_global(self.transformed_pose)
