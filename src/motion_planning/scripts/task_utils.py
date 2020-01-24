15604a817abfd7287a7620d12f681bd4ab326445
echang264 <eric.t.chang@duke.edu>
2020-01-24 09:17:23 -0500

    #137 add functions to complete

--- src/motion_planning/scripts/task_utils.py
+++ src/motion_planning/scripts/task_utils.py
@@ -1,11 +1,34 @@
 import numpy as np
 
-class TaskUtils(object):
+def distance(self, x1, y1, z1, x2, y2, z2):
+    vector1 = np.array([x1, y1, z1])
+    vector2 = np.array([x2, y2, z2])
+    distance = np.linalg.norm(vector2 - vector1)
+    return distance
 
-    def distance(self, x1, y1, z1, x2, y2, z2):
-        vector1 = np.array([x1, y1, z1])
-        vector2 = np.array([x2, y2, z2])
-        distance = np.linalg.norm(vector2 - vector1)
-        return distance
+def linear_distance(point1, point2):
+    """
+    Return distance between two points
+    """
+    pass
 
-    def transform(self):
+def angular_distance_quat(quat1, quat2):
+    """
+    """
+    #convert to rpy, call rpy method
+    pass
+
+def angular_distance_rpy(rpy1, rpy2):
+    """
+    """
+    pass
+
+def at_pose(current_pose, desired_pose, linear_tol=0.1, angular_tol=3):
+    """
+    """
+    pass
+
+def transform(self):
+    """
+    """
+    pass
