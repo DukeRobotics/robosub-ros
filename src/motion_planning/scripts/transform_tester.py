6adb17942588738261884a82303d785dcd9455da
echang264 <eric.t.chang@duke.edu>
2020-01-28 22:13:25 -0500

    test tf methods

--- src/motion_planning/scripts/transform_tester.py
+++ src/motion_planning/scripts/transform_tester.py
@@ -1,10 +1,13 @@
-from task_utils import transform
-from geometry_msgs.msg import Pose, Odometry
+#!/usr/bin/env python
+
+from geometry_msgs.msg import PoseStamped
 import rospy
+import tf2_ros
+import tf2_geometry_msgs
 
+rospy.init_node('testing')
 
-
-def transform(origin, destination, odometry=None, pose=None):
+def transform(origin, destination, this_pose=None):
     """Transforms Odometry input from origin frame to destination frame
 
     Arguments:
@@ -16,25 +19,22 @@ def transform(origin, destination, odometry=None, pose=None):
     The transformed odometry message
     """
 
-    if(odometry != None):
+    if(this_pose != None):
         tfBuffer = tf2_ros.Buffer()
         listener = tf2_ros.TransformListener(tfBuffer)
-        trans = tfBuffer.lookup_transform(origin, destination, rospy.Time(0))
-        #TODO: transform odometry
-    elif(pose != None):
-        tfBuffer = tf2_ros.Buffer()
-        listener = tf2_ros.TransformListener(tfBuffer)
-        trans = tfBuffer.lookup_transform(origin, destination, rospy.Time(0))
-        transformed = tf2_geometry_msgs.do_transform_pose(pose, trans)
+        trans = tfBuffer.lookup_transform(destination, origin, rospy.Time(), rospy.Duration(0.5))
+        transformed_pose = tf2_geometry_msgs.do_transform_pose(this_pose, trans)
+
+    return transformed_pose
 
 
-pose = Pose()
-pose.position.x = 2
-pose.position.y = 4
-pose.position.z = 7
+pose_stamped = PoseStamped()
+pose_stamped.pose.position.x = 2
+pose_stamped.pose.position.y = 4
+pose_stamped.pose.position.z = 7
 
-transformedPose = transform('origin','desitnation', pose)
+transformed_pose = transform('origin1', 'destination1', pose_stamped)
 
-print(transformedPose.position.x)
-print(transformedPose.position.y)
-print(transformedPose.position.z)
+print(transformed_pose.pose.position.x)
+print(transformed_pose.pose.position.y)
+print(transformed_pose.pose.position.z)
