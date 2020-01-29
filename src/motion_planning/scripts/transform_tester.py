90ec24f11ecff4ff306110a516301b98d8642598
echang264 <eric.t.chang@duke.edu>
2020-01-28 20:25:35 -0500

    fix indentation

--- src/motion_planning/scripts/transform_tester.py
+++ src/motion_planning/scripts/transform_tester.py
@@ -6,7 +6,7 @@ import rospy
 
 def transform(origin, destination, odometry=None, pose=None):
     """Transforms Odometry input from origin frame to destination frame
-    
+
     Arguments:
     origin: the starting frame
     destination: the frame to trasform to
@@ -15,23 +15,23 @@ def transform(origin, destination, odometry=None, pose=None):
     Returns:
     The transformed odometry message
     """
- 
+
    if(odometry != None):
-        tfBuffer = tf2_ros.Buffer()
-        listener = tf2_ros.TransformListener(tfBuffer)
-        trans = tfBuffer.lookup_transform(origin, destination, rospy.Time(0))
-        #TODO: transform odometry    
+       tfBuffer = tf2_ros.Buffer()
+       listener = tf2_ros.TransformListener(tfBuffer)
+       trans = tfBuffer.lookup_transform(origin, destination, rospy.Time(0))
+       #TODO: transform odometry
    elif(pose != None):
-        tfBuffer = tf2_ros.Buffer()
-        listener = tf2_ros.TransformListener(tfBuffer)
-        trans = tfBuffer.lookup_transform(origin, destination, rospy.Time(0))
-	transformed = tf2_geometry_msgs.do_transform_pose(pose, trans)
+       tfBuffer = tf2_ros.Buffer()
+       listener = tf2_ros.TransformListener(tfBuffer)
+       trans = tfBuffer.lookup_transform(origin, destination, rospy.Time(0))
+	   transformed = tf2_geometry_msgs.do_transform_pose(pose, trans)
 
 
 pose = Pose()
 pose.position.x = 2
 pose.position.y = 4
-pose.position.z = 7 
+pose.position.z = 7
 
 transformedPose = transform('origin','desitnation', pose)
 
