4215d281d45d70512f5fd96267dcd16246dc8387
Jordan Schwartz <1jordanschwartz@gmail.com>
2020-01-28 20:23:43 -0500

    tester for transforms

--- src/motion_planning/scripts/task_utils.py
+++ src/motion_planning/scripts/task_utils.py
@@ -90,6 +90,5 @@ def transform(origin, destination, odometry=None, pose=None):
         tfBuffer = tf2_ros.Buffer()
         listener = tf2_ros.TransformListener(tfBuffer)
         trans = tfBuffer.lookup_transform(origin, destination, rospy.Time(0))
-        #TODO: transform pose
-
+	transformed = tf2_geometry_msgs.do_transform_pose(pose, trans)
     return transformed 
