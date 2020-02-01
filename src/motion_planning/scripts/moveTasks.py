c62b802793b128daff069990198afb312347c6d9
Noah Citron <noah@jeff.org>
2020-02-01 12:39:05 -0500

    moveGlobal works! (in theory)

--- src/motion_planning/scripts/moveTasks.py
+++ src/motion_planning/scripts/moveTasks.py
@@ -3,20 +3,19 @@ from task import Task
 from geometry_msgs.msg import Pose
 from tf.transformations import quaternion_from_euler
 
-class MoveRelative(Task):
+class MoveGlobal(Task):
 
     DESIRED_POSE_TOPIC = 'controls/desired_pose_global'
     NODE_NAME = 'moveTask'  # TBD
 
-    def __init__(self, x, y, z, orientation_x, orientation_y, orientation_z, orientation_w):
+    def __init__(self, x, y, z, roll, pitch, yaw):
         Task.__init__(self)
         self.x = x
         self.y = y
         self.z = z
-        self.orientation_x = orientation_x
-        self.orientation_y = orientation_y
-        self.orientation_z = orientation_z
-        self.orientation_w = orientation_w
+        self.roll = roll
+        self.pitch = pitch
+        self.yaw = yaw
 
     def _initialize(self):
         rospy.init_node(self.NODE_NAME, anonymous=True)
@@ -27,16 +26,11 @@ class MoveRelative(Task):
 
     def _run_task(self):
         self.pose = Pose()
-        #pose.header.stamp = rospy.Time.now()
-        #pose.header.frame_id = ""
         self.pose.position.x = self.x
         self.pose.position.y = self.y
         self.pose.position.z = self.z
 
-        quaternion = quaternion_from_euler(self.orientation_x,
-                                            self.orientation_y,
-                                            self.orientation_z,
-                                            self.orientation_w])
+        quaternion = quaternion_from_euler(self.roll, self.pitch, self.yaw)
 
         self.pose.orientation.x = quaternion[0]
         self.pose.orientation.y = quaternion[1]
@@ -47,6 +41,6 @@ class MoveRelative(Task):
             self.posePublisher.publish(self.pose)
             self.rate.sleep()
 
-move = MoveRelative(1, 0, 0, 0, 0, 0, 0)
+move = MoveGlobal(1, 0, 0, 0, 0, 0)
 move._initialize()
 move._run_task()
