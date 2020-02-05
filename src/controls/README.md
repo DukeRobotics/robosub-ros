2e02390fdfe93008c395eb5f9f4e9a81b8ebfdbc
Shaan Gondalia <shaan.gondalia@duke.edu>
2020-02-04 22:31:05 -0500

    Power processing and Updated Documentation

--- src/controls/README.md
+++ src/controls/README.md
@@ -13,17 +13,17 @@ Thruster information is read from `cthulhu.config`, which is written in YAML and
 2. On your local computer, cd to the git repo (probably a directory named "robosub-ros").
 3. On your local computer, run the docker container with the following command:
 
-    `docker run -td -p 2200:2200 --mount type=bind,source=$PWD/src,target=/home/duke/dev/robosub-ros/catkin_ws/src  dukerobotics/robosub-ros`
+    ```docker run -td -p 2200:2200 --mount type=bind,source=$PWD/src,target=/home/duke/dev/robosub-ros/catkin_ws/src  dukerobotics/robosub-ros```
 
 4. SSH into the docker container
 5. In the docker container, run the following command:
 
-    `. ~/dev/robosub-ros/catkin_ws/src/controls/build_node.sh`
+    ```. ~/dev/robosub-ros/catkin_ws/src/controls/build_node.sh```
 
     which sources, builds the catkin workspace, and creates packages.
 6. Initialize roscore and paste the following in the terminal to start the PID Loops:
 
-    `roslaunch controls controls.launch &`
+    ```roslaunch controls controls.launch &```
 
     ROS will warn you if nothing is being published to the desired or current state topics once the PID loops are launched.
 
@@ -31,21 +31,21 @@ Thruster information is read from `cthulhu.config`, which is written in YAML and
 
 To test the outputs of the PID Loops, edit the values in `test_state_publisher.py` to whatever current and desired state you wish to test. Then, run the following:
 
-`
+```
 rosrun controls test_state_publisher.py &
-`
+```
 
 To check direct PID Outputs:
 
-`
+```
 rostopic echo /control_effort/<var>
-`
+```
 
 Where &lt;var&gt; is x, y, z, roll, pitch, or yaw. To check final thruster allocations:
 
-`
+```
 rostopic echo /offboard_comms/ThrusterSpeeds
-`
+```
 
 ## Testing with Simulation
 
@@ -53,10 +53,10 @@ To get the initialize the simulation, follow the instructions in the simulation 
 
 Once the simulation is running, execute:
 
-`
+```
 rosrun controls test_state_publisher.py &
 roslaunch controls controls.launch mode:=sim
-`
+```
 
 test_state_publisher.py is where we specify the desired state of the robot. Alternatively, you can publish to any of the 3 desired state topics directly. The second command launches the entire controls node in simulation mode.
 
@@ -67,36 +67,34 @@ test_state_publisher.py is where we specify the desired state of the robot. Alte
 
 Desired State Topics:
 
-  - `controls/desired_pose_global`
+  - ```controls/desired_pose```
     + A point and quaternion representing the robot's desired global xyz position and rpy orientation
     + Type: geometry_msgs/Pose
-  - `controls/desired_twist_local`
-    + 2 vectors representing the robot's desired xyz and rpy velocities in the local frame
-    + Type: geometry_msgs/Twist
-  - `controls/desired_twist_global`
-    + 2 vectors representing the robot's desired xyz and rpy velocities in the global frame
+  - ```controls/desired_twist_power```
+    + A twist with values [-1,1] corresponding to relative linear or angular velocity. 0 values are interpreted as axes to stabilize on. For instance, a twist with values [1,0,0,0,0,0] will result in full speed in the local x-direction and stabilization along all other axes.
     + Type: geometry_msgs/Twist
 
 Current State Topics:
 
-  - `/state`
+  - ```/state``` 
     + The current state of the actual robot, in global position, orientation, linear velocity, and angular velocity
     + Type: nav_msgs/Odometry
-  - `/sim/pose`
+  - ```/sim/pose```
     + Current position and orientation of the robot in simulation
     + Type: geometry_msgs/PoseStamped
-  - `/sim/dvl`
+  - ```/sim/dvl```
     + Current linear and angular velocities of the robot in simulation
-    + Type: geometry_msgs/TwistStamped
+    + Type: geometry_msgs/TwistStamped 
+
 
 ### Publishing
 
 We can choose to publish to either of these topics:
 
-  - `/sim/move`
+  - ```/sim/move```
     + An array of 8 floats [-1,1] describing the allocation of the thrusters sent to the simulation
     + Type: std_msgs/Float32MultiArray
-  - `/offboard_comms/ThrusterSpeeds`
+  - ```/offboard/thruster_speeds```
     + An array of 8 8-bit integers [-128,127] describing the allocation of the thrusters sent to the arduino
     + Type: std_msgs/Int8MultiArray
 
@@ -106,6 +104,7 @@ Only the most recently updated Desired State topic will be used in movement. The
 
 The topic that is published to can be configured in controls.launch by passing in a parameter through the command line or a parent launch file.
 
-**Not yet, but soon**: When publishing local or global twists for desired state, every axis that is set to 0 will be controlled for by position PID rather than velocity PID to mitigate drift effects or other unwanted perturbations. Only those axes that have nonzero desires will be controlled for by velocity.
+When publishing powers to desired state, every axis that is set to 0 will be controlled for by position PID  to mitigate drift effects or other unwanted perturbations. Only those axes that have nonzero desires will be controlled for by powers.
 
+All power controls are relative; values of 1 represent moving at full speed in a direction and values of -1 represent moving at full speed in the opposite direction
 
--- src/controls/scripts/desired_state.py
+++ src/controls/scripts/desired_state.py
@@ -20,9 +20,8 @@ class DesiredStateHandler():
     POSE_TOPIC_PITCH = '/controls/state/pose/pitch'
     POSE_TOPIC_YAW = '/controls/state/pose/yaw'
 
-    DESIRED_LOCAL_TWIST_TOPIC = 'controls/desired_twist_local'
-    DESIRED_GLOBAL_TWIST_TOPIC = 'controls/desired_twist_global'
-    DESIRED_POSE_TOPIC = 'controls/desired_pose_global'
+    DESIRED_TWIST_POWER = 'controls/desired_twist_power'
+    DESIRED_POSE_TOPIC = 'controls/desired_pose'
 
     PUBLISHING_TOPIC_X_POS = 'controls/x_pos/setpoint'
     PUBLISHING_TOPIC_Y_POS = 'controls/y_pos/setpoint'
@@ -31,13 +30,6 @@ class DesiredStateHandler():
     PUBLISHING_TOPIC_PITCH_POS = 'controls/pitch_pos/setpoint'
     PUBLISHING_TOPIC_YAW_POS = 'controls/yaw_pos/setpoint'
 
-    PUBLISHING_TOPIC_X_VEL = 'controls/x_vel/setpoint'
-    PUBLISHING_TOPIC_Y_VEL = 'controls/y_vel/setpoint'
-    PUBLISHING_TOPIC_Z_VEL = 'controls/z_vel/setpoint'
-    PUBLISHING_TOPIC_ROLL_VEL = 'controls/roll_vel/setpoint'
-    PUBLISHING_TOPIC_PITCH_VEL = 'controls/pitch_vel/setpoint'
-    PUBLISHING_TOPIC_YAW_VEL = 'controls/yaw_vel/setpoint'
-
     PUBLISHING_TOPIC_ENABLE_X_POS = 'controls/enable/x_pos'
     PUBLISHING_TOPIC_ENABLE_Y_POS = 'controls/enable/y_pos'
     PUBLISHING_TOPIC_ENABLE_Z_POS = 'controls/enable/z_pos'
@@ -45,12 +37,19 @@ class DesiredStateHandler():
     PUBLISHING_TOPIC_ENABLE_PITCH_POS = 'controls/enable/pitch_pos'
     PUBLISHING_TOPIC_ENABLE_YAW_POS = 'controls/enable/yaw_pos'
 
-    PUBLISHING_TOPIC_ENABLE_X_VEL = 'controls/enable/x_vel'
-    PUBLISHING_TOPIC_ENABLE_Y_VEL = 'controls/enable/y_vel'
-    PUBLISHING_TOPIC_ENABLE_Z_VEL = 'controls/enable/z_vel'
-    PUBLISHING_TOPIC_ENABLE_ROLL_VEL = 'controls/enable/roll_vel'
-    PUBLISHING_TOPIC_ENABLE_PITCH_VEL = 'controls/enable/pitch_vel'
-    PUBLISHING_TOPIC_ENABLE_YAW_VEL = 'controls/enable/yaw_vel'
+    PUBLISHING_TOPIC_X_EFFORT = '/control_effort/x'
+    PUBLISHING_TOPIC_Y_EFFORT = '/control_effort/y'
+    PUBLISHING_TOPIC_Z_EFFORT = '/control_effort/z'
+    PUBLISHING_TOPIC_ROLL_EFFORT = '/control_effort/roll'
+    PUBLISHING_TOPIC_PITCH_EFFORT = '/control_effort/pitch'
+    PUBLISHING_TOPIC_YAW_EFFORT = '/control_effort/yaw'
+
+    x_hold = 0
+    y_hold = 0
+    z_hold = 0
+    roll_hold = 0
+    pitch_hold = 0
+    yaw_hold = 0
 
     x = 0
     y = 0
@@ -59,11 +58,9 @@ class DesiredStateHandler():
     pitch = 0
     yaw = 0
 
-    adjoint = np.zeros((6, 6))
-
     pose = None
-    local_twist = None
-    global_twist = None
+    powers = None
+    last_powers = None
 
     def __init__(self):
         rospy.Subscriber(self.POSE_TOPIC_X, Float64, self.receive_x)
@@ -80,13 +77,6 @@ class DesiredStateHandler():
         self._pub_roll_pos = rospy.Publisher(self.PUBLISHING_TOPIC_ROLL_POS, Float64, queue_size=3)
         self._pub_pitch_pos = rospy.Publisher(self.PUBLISHING_TOPIC_PITCH_POS, Float64, queue_size=3)
         self._pub_yaw_pos = rospy.Publisher(self.PUBLISHING_TOPIC_YAW_POS, Float64, queue_size=3)
-        #PID Velocity publishers
-        self._pub_x_vel = rospy.Publisher(self.PUBLISHING_TOPIC_X_VEL, Float64, queue_size=3)
-        self._pub_y_vel = rospy.Publisher(self.PUBLISHING_TOPIC_Y_VEL, Float64, queue_size=3)
-        self._pub_z_vel = rospy.Publisher(self.PUBLISHING_TOPIC_Z_VEL, Float64, queue_size=3)
-        self._pub_roll_vel = rospy.Publisher(self.PUBLISHING_TOPIC_ROLL_VEL, Float64, queue_size=3)
-        self._pub_pitch_vel = rospy.Publisher(self.PUBLISHING_TOPIC_PITCH_VEL, Float64, queue_size=3)
-        self._pub_yaw_vel = rospy.Publisher(self.PUBLISHING_TOPIC_YAW_VEL, Float64, queue_size=3)
 
         self._pub_x_pos_enable = rospy.Publisher(self.PUBLISHING_TOPIC_ENABLE_X_POS, Bool, queue_size=3)
         self._pub_y_pos_enable = rospy.Publisher(self.PUBLISHING_TOPIC_ENABLE_Y_POS, Bool, queue_size=3)
@@ -95,17 +85,15 @@ class DesiredStateHandler():
         self._pub_pitch_pos_enable = rospy.Publisher(self.PUBLISHING_TOPIC_ENABLE_PITCH_POS, Bool, queue_size=3)
         self._pub_yaw_pos_enable = rospy.Publisher(self.PUBLISHING_TOPIC_ENABLE_YAW_POS, Bool, queue_size=3)
 
-        self._pub_x_vel_enable = rospy.Publisher(self.PUBLISHING_TOPIC_ENABLE_X_VEL, Bool, queue_size=3)
-        self._pub_y_vel_enable = rospy.Publisher(self.PUBLISHING_TOPIC_ENABLE_Y_VEL, Bool, queue_size=3)
-        self._pub_z_vel_enable = rospy.Publisher(self.PUBLISHING_TOPIC_ENABLE_Z_VEL, Bool, queue_size=3)
-        self._pub_roll_vel_enable = rospy.Publisher(self.PUBLISHING_TOPIC_ENABLE_ROLL_VEL, Bool, queue_size=3)
-        self._pub_pitch_vel_enable = rospy.Publisher(self.PUBLISHING_TOPIC_ENABLE_PITCH_VEL, Bool, queue_size=3)
-        self._pub_yaw_vel_enable = rospy.Publisher(self.PUBLISHING_TOPIC_ENABLE_YAW_VEL, Bool, queue_size=3)
-
+        self._pub_x_effort = rospy.Publisher(self.PUBLISHING_TOPIC_X_EFFORT, Float64, queue_size=3)
+        self._pub_y_effort = rospy.Publisher(self.PUBLISHING_TOPIC_X_EFFORT, Float64, queue_size=3)
+        self._pub_z_effort = rospy.Publisher(self.PUBLISHING_TOPIC_X_EFFORT, Float64, queue_size=3)
+        self._pub_roll_effort = rospy.Publisher(self.PUBLISHING_TOPIC_X_EFFORT, Float64, queue_size=3)
+        self._pub_pitch_effort= rospy.Publisher(self.PUBLISHING_TOPIC_X_EFFORT, Float64, queue_size=3)
+        self._pub_yaw_effort = rospy.Publisher(self.PUBLISHING_TOPIC_X_EFFORT, Float64, queue_size=3)
 
         rospy.Subscriber(self.DESIRED_POSE_TOPIC, Pose, self.receive_pose)
-        rospy.Subscriber(self.DESIRED_LOCAL_TWIST_TOPIC, Twist, self.receive_local_twist)
-        rospy.Subscriber(self.DESIRED_GLOBAL_TWIST_TOPIC, Twist, self.receive_global_twist)
+        rospy.Subscriber(self.DESIRED_TWIST_POWER, Twist, self.receive_powers)
 
     def receive_x(self, x):
         self.x = x.data
@@ -128,24 +116,8 @@ class DesiredStateHandler():
     def receive_pose(self, pose):
         self.pose = pose
 
-    def receive_local_twist(self, twist):
-        self.local_twist = twist
-
-    def receive_global_twist(self, twist):
-        self.global_twist = twist
-
-    def update_transformation(self):
-        # Create rotation matrix from local frame to global frame
-        r = euler_matrix(-self.roll, -self.pitch, -self.yaw)
-        
-        # Create translation vector from local frame to global frame
-        p = np.array([-self.x, -self.y, -self.z])
-
-        # Create transformation matrix
-        t = RpToTrans(r[:3,:3], p)
-
-        # Create adjoint matrix for twist transformation
-        self.adjoint = Adjoint(t)
+    def receive_powers(self, twist):
+        self.powers = twist
 
     def run(self):
 
@@ -153,17 +125,15 @@ class DesiredStateHandler():
         rate = rospy.Rate(10)  # 10 Hz
 
         while not rospy.is_shutdown():
-            if not self.pose and not self.local_twist and not self.global_twist:
+            if not self.pose and not self.powers:
                 # TODO: If it's been too long, stop PID and warn (timeout)
-
                 continue
-            if (self.pose and self.local_twist) or (self.pose and self.global_twist) or (self.local_twist and self.global_twist):
-                # Make this if statement prettier
+
+            if self.pose and self.powers:
                 # TODO: More than one seen in one update cycle, so warn and continue
                 continue
 
-            # Now we have either pose XOR local_twist XOR global_twist
-
+            # Now we have either pose XOR powers
             if self.pose:
                 self._pub_x_pos_enable.publish(True)
                 self._pub_y_pos_enable.publish(True)
@@ -171,14 +141,6 @@ class DesiredStateHandler():
                 self._pub_roll_pos_enable.publish(True)
                 self._pub_pitch_pos_enable.publish(True)
                 self._pub_yaw_pos_enable.publish(True)
-
-                self._pub_x_vel_enable.publish(False)
-                self._pub_y_vel_enable.publish(False)
-                self._pub_z_vel_enable.publish(False)
-                self._pub_roll_vel_enable.publish(False)
-                self._pub_pitch_vel_enable.publish(False)
-                self._pub_yaw_vel_enable.publish(False)
-
 
                 x = self.pose.position.x
                 y = self.pose.position.y
@@ -199,70 +161,57 @@ class DesiredStateHandler():
 
                 self.pose = None
 
-            elif self.local_twist:
-                self._pub_x_pos_enable.publish(False)
-                self._pub_y_pos_enable.publish(False)
-                self._pub_z_pos_enable.publish(False)
-                self._pub_roll_pos_enable.publish(False)
-                self._pub_pitch_pos_enable.publish(False)
-                self._pub_yaw_pos_enable.publish(False)
+            elif self.powers:
 
-                self._pub_x_vel_enable.publish(True)
-                self._pub_y_vel_enable.publish(True)
-                self._pub_z_vel_enable.publish(True)
-                self._pub_roll_vel_enable.publish(True)
-                self._pub_pitch_vel_enable.publish(True)
-                self._pub_yaw_vel_enable.publish(True)
+                if self.powers!=self.last_powers:
 
-                x_vel = self.local_twist.linear.x
-                y_vel = self.local_twist.linear.y
-                z_vel = self.local_twist.linear.z
-                roll_vel = self.local_twist.angular.x
-                pitch_vel = self.local_twist.angular.y
-                yaw_vel = self.local_twist.angular.z
+                    #Enable all PID Loops
+                    self._pub_x_pos_enable.publish(True)
+                    self._pub_y_pos_enable.publish(True)
+                    self._pub_z_pos_enable.publish(True)
+                    self._pub_roll_pos_enable.publish(True)
+                    self._pub_pitch_pos_enable.publish(True)
+                    self._pub_yaw_pos_enable.publish(True)
 
-                self._pub_x_vel.publish(x_vel)
-                self._pub_y_vel.publish(y_vel)
-                self._pub_z_vel.publish(z_vel)
-                self._pub_roll_vel.publish(roll_vel)
-                self._pub_pitch_vel.publish(pitch_vel)
-                self._pub_yaw_vel.publish(yaw_vel)
+                    #Hold Position on the current state
+                    self.x_hold = self.x
+                    self.y_hold = self.y
+                    self.z_hold = self.z
+                    self.roll_hold = self.roll
+                    self.pitch_hold = self.pitch
+                    self.yaw_hold = self.yaw
+
+                    self.last_powers = self.powers
+
+                #Nonzero entries bypass PID
+                if self.powers.linear.x != 0:
+                    self._pub_x_pos_enable.publish(False)
+                    self._pub_x_effort.publish(self.powers.linear.x)
+                if self.powers.linear.y != 0:
+                    self._pub_y_pos_enable.publish(False)
+                    self._pub_y_effort.publish(self.powers.linear.y)
+                if self.powers.linear.z != 0:
+                    self._pub_z_pos_enable.publish(False)
+                    self._pub_z_effort.publish(self.powers.linear.z)
+                if self.powers.angular.x != 0:
+                    self._pub_roll_pos_enable.publish(False)
+                    self._pub_roll_effort.publish(self.powers.angular.x)
+                if self.powers.angular.y != 0:
+                    self._pub_pitch_pos_enable.publish(False)
+                    self._pub_pitch_effort.publish(self.powers.angular.y)
+                if self.powers.angular.z != 0:
+                    self._pub_yaw_pos_enable.publish(False)
+                    self._pub_yaw_effort.publish(self.powers.angular.z)
+
+                #Publish current state to the desired state for PID
+                self._pub_x_pos.publish(self.x_hold)
+                self._pub_y_pos.publish(self.y_hold)
+                self._pub_z_pos.publish(self.z_hold)
+                self._pub_roll_pos.publish(self.roll_hold)
+                self._pub_pitch_pos.publish(self.pitch_hold)
+                self._pub_yaw_pos.publish(self.yaw_hold)
 
                 self.local_twist = None
-
-            elif self.global_twist:
-                self._pub_x_pos_enable.publish(False)
-                self._pub_y_pos_enable.publish(False)
-                self._pub_z_pos_enable.publish(False)
-                self._pub_roll_pos_enable.publish(False)
-                self._pub_pitch_pos_enable.publish(False)
-                self._pub_yaw_pos_enable.publish(False)
-
-                self._pub_x_vel_enable.publish(True)
-                self._pub_y_vel_enable.publish(True)
-                self._pub_z_vel_enable.publish(True)
-                self._pub_roll_vel_enable.publish(True)
-                self._pub_pitch_vel_enable.publish(True)
-                self._pub_yaw_vel_enable.publish(True)
-
-                # Perform necessary transformations from global to local frame then publish
-                self.update_transformation()
-                g = np.array([self.global_twist.linear.x,
-                              self.global_twist.linear.y,
-                              self.global_twist.linear.z,
-                              self.global_twist.angular.x,
-                              self.global_twist.angular.y,
-                              self.global_twist.angular.z])
-                local = np.matmul(self.adjoint, g)
-
-                self._pub_x_vel.publish(local[0])
-                self._pub_y_vel.publish(local[1])
-                self._pub_z_vel.publish(local[2])
-                self._pub_roll_vel.publish(local[3])
-                self._pub_pitch_vel.publish(local[4])
-                self._pub_yaw_vel.publish(local[5])
-
-                self.global_twist = None
 
             rate.sleep()
 
