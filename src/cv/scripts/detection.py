dc2f09efaa2c50bec55d0886e66a9b8314700988
Alan Bi <alan.bi@duke.edu>
2020-02-15 18:11:35 -0500

    Get working object detection on dummy video feed

--- src/cv/scripts/detection.py
+++ src/cv/scripts/detection.py
@@ -1,31 +1,57 @@
 #!/usr/bin/env python
 
 import rospy
+import os
 from cv.msg import Object
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from detecto.core import Model
 
 
 class Detector:
 
     NODE_NAME = 'cv'
-    GATE_TOPIC = '/cv/gate'
+    GATE_TOPIC = '/cv/buoy'
     IMAGE_TOPIC = '/test_images/image'
 
+    BUOY_CLASSES = ['alien', 'bat', 'witch', 'brick', '_start_gate', '_start_tick', '_']
+
+    # Load in models and other misc. setup work
     def __init__(self):
-        self.gate_publisher = rospy.Publisher(self.GATE_TOPIC, Object, queue_size=10)
+        self.buoy_publisher = rospy.Publisher(self.GATE_TOPIC, Object, queue_size=10)
+        self.bridge = CvBridge()
 
+        path = os.path.dirname(__file__)
+        buoy_file = os.path.join(path, '../models/buoy.pth')
+        self.buoy_model = Model.load(buoy_file, self.BUOY_CLASSES)
+
+    # Camera subscriber callback; publishes predictions for each frame
     def detect(self, img_msg):
-        object_msg = Object()
-        object_msg.xmin = img_msg.height
+        image = self.bridge.imgmsg_to_cv2(img_msg, 'rgb8')
 
-        self.gate_publisher.publish(object_msg)
+        preds = self.buoy_model.predict_top(image)
 
+        for label, box, score in zip(*preds):
+            object_msg = Object()
+
+            object_msg.label = label
+            object_msg.score = score
+
+            object_msg.xmin = box[0].item()
+            object_msg.ymin = box[1].item()
+            object_msg.xmax = box[2].item()
+            object_msg.ymax = box[3].item()
+
+            self.buoy_publisher.publish(object_msg)
+
+    # Initialize node and set up Subscriber to generate and
+    # publish predictions at every camera frame
     def run(self):
         rospy.init_node(self.NODE_NAME)
         rospy.Subscriber(self.IMAGE_TOPIC, Image, self.detect)
 
         while not rospy.is_shutdown():
-            pass
+            pass  # There's gotta be a better way to do this
 
 
 if __name__ == '__main__':
--- src/cv/scripts/test_images.py
+++ src/cv/scripts/test_images.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
 
 import rospy
+import cv2
+import os
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
 
+# Mock the camera by publishing the same image to a topic
 class DummyImagePublisher:
 
     NODE_NAME = 'test_images'
     IMAGE_TOPIC = '/test_images/image'
 
+    # Read in the dummy image and other misc. setup work
     def __init__(self):
-        # image_topic_name = '{}{}'.format(self.NODE_NAME, self.IMAGE_TOPIC)
         self.image_publisher = rospy.Publisher(self.IMAGE_TOPIC, Image, queue_size=10)
 
-        self.image_msg = Image()
-        self.image_msg.height = 100
+        path = os.path.dirname(__file__)
+        image = cv2.imread(os.path.join(path, '../assets/buoy.jpg'), cv2.IMREAD_COLOR)
+        bridge = CvBridge()
 
+        self.image_msg = bridge.cv2_to_imgmsg(image, 'bgr8')
+
+    # Publish dummy image to topic every 5 seconds
     def run(self):
         rospy.init_node(self.NODE_NAME)
 
-        loop_rate = rospy.Rate(1)
+        loop_rate = rospy.Rate(0.2)
         while not rospy.is_shutdown():
             self.image_publisher.publish(self.image_msg)
             loop_rate.sleep()
