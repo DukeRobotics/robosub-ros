388f1cdc63961feb6361d6c3b8c2c2a87299a973
Shaan Gondalia <shaan.gondalia@duke.edu>
2020-02-18 20:00:12 -0500

    Fixed config file

--- catkin_ws/src/controls/scripts/thruster.py
+++ catkin_ws/src/controls/scripts/thruster.py
@@ -17,7 +17,10 @@ class Thruster():
         self.rpy = rpy
         
         to_rad = lambda x: x * math.pi / 180  # convert degrees to radians
-        q = quaternion_from_euler(to_rad(rpy[0]), to_rad(rpy[1]), to_rad(rpy[2])) # create a quaternion from euler rpy
+        q = quaternion_from_euler(to_rad(rpy[0]), to_rad(rpy[1]), to_rad(rpy[2]), 'sxyz') # create a quaternion from euler rpy
+        # sxyz denotes a static (extrinsic) frame of reference.
+        # https://github.com/matthew-brett/transforms3d/blob/master/transforms3d/euler.py
+        # https://en.wikipedia.org/wiki/Euler_angles
 
         self.force_hat = quad_vec_mult(q, [1, 0, 0]) # vector representing force of thruster in x,y,z directions
         
