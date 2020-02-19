9472c80eba857e16a2c7923a321d01d5e87c370c
Samuel Rabinowitz <5happy1@users.noreply.github.com>
2020-02-18 23:58:51 -0500

    Updates post-Saturday pool test (#166)
    * Fixed config file
    
    * update config section of readme, delete unnecessary files
    
    * [untested] updated state republisher to new simplified input topics, updated 'mode' argument to 'sim'
    
    * add in functionality to flip thrusters in controls
    
    Co-authored-by: ShaanGondalia <55925366+ShaanGondalia@users.noreply.github.com>

--- catkin_ws/src/controls/scripts/state_republisher.py
+++ catkin_ws/src/controls/scripts/state_republisher.py
@@ -2,17 +2,13 @@
 
 import rospy
 from std_msgs.msg import Float64
-from geometry_msgs.msg import PoseStamped, TwistStamped, Quaternion
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 
 from drc_math import quad_vec_mult
 
 class StateRepublisher():
-    SIMULATION_POSE_TOPIC = '/sim/pose'
-    SIMULATION_TWIST_TOPIC = '/sim/dvl'
-
-    ROBOT_ODOMETRY_TOPIC = '/state'
+    STATE_TOPIC = '/state'
 
     PUBLISHING_POSE_TOPIC_X = '/controls/state/pose/x'
     PUBLISHING_POSE_TOPIC_Y = '/controls/state/pose/y'
@@ -29,7 +25,6 @@ class StateRepublisher():
     PUBLISHING_TWIST_TOPIC_YAW = '/controls/state/twist/yaw'
 
     def __init__(self):
-
         self._pub_pose_x = rospy.Publisher(self.PUBLISHING_POSE_TOPIC_X, Float64, queue_size=3)
         self._pub_pose_y = rospy.Publisher(self.PUBLISHING_POSE_TOPIC_Y, Float64, queue_size=3)
         self._pub_pose_z = rospy.Publisher(self.PUBLISHING_POSE_TOPIC_Z, Float64, queue_size=3)
@@ -45,41 +40,8 @@ class StateRepublisher():
         self._pub_twist_yaw = rospy.Publisher(self.PUBLISHING_TWIST_TOPIC_YAW, Float64, queue_size=3)
 
         rospy.init_node('state_republisher')
-
-        self.mode = rospy.get_param('~/state_republisher/mode')  # robot or sim, default to robot
-        if self.mode == 'sim':
-            rospy.Subscriber(self.SIMULATION_POSE_TOPIC, PoseStamped, self.receive_pose_stamped)
-            rospy.Subscriber(self.SIMULATION_TWIST_TOPIC, TwistStamped, self.receive_twist_stamped)
-        else:
-            rospy.Subscriber(self.ROBOT_ODOMETRY_TOPIC, Odometry, self.receive_odometry)
-
+        rospy.Subscriber(self.STATE_TOPIC, Odometry, self.receive_odometry)
         rospy.spin()
-
-
-    def receive_pose_stamped(self, pose_stamped):
-        # xyz Position
-        self._pub_pose_x.publish(pose_stamped.pose.position.x)
-        self._pub_pose_y.publish(pose_stamped.pose.position.y)
-        self._pub_pose_z.publish(pose_stamped.pose.position.z)
-        # rpy Orientation
-        roll, pitch, yaw = euler_from_quaternion([pose_stamped.pose.orientation.x,
-                                                  pose_stamped.pose.orientation.y,
-                                                  pose_stamped.pose.orientation.z,
-                                                  pose_stamped.pose.orientation.w])
-        self._pub_pose_roll.publish(roll)
-        self._pub_pose_pitch.publish(pitch)
-        self._pub_pose_yaw.publish(yaw)
-
-
-    def receive_twist_stamped(self, twist_stamped):
-        # Linear Velocity
-        self._pub_twist_x.publish(twist_stamped.twist.linear.x)
-        self._pub_twist_y.publish(twist_stamped.twist.linear.y)
-        self._pub_twist_z.publish(twist_stamped.twist.linear.z)
-        # Angular Velocity
-        self._pub_twist_roll.publish(twist_stamped.twist.angular.x)
-        self._pub_twist_pitch.publish(twist_stamped.twist.angular.y)
-        self._pub_twist_yaw.publish(twist_stamped.twist.angular.z)
 
     def receive_odometry(self, odometry):
         # xyz Position
--- catkin_ws/src/controls/scripts/thruster_controls.py
+++ catkin_ws/src/controls/scripts/thruster_controls.py
@@ -25,10 +25,10 @@ class ThrusterController():
     enabled = False
 
     def __init__(self):
-        self.mode = rospy.get_param('~/thruster_controls/mode')  # robot or sim, default to robot
-        if self.mode == 'robot':
+        self.sim = rospy.get_param('~/thruster_controls/sim')
+        if self.sim == 'false':
             self.pub = rospy.Publisher(self.ROBOT_PUB_TOPIC, ThrusterSpeeds, queue_size=3)
-        elif self.mode == 'sim':
+        elif self.sim == 'true':
             self.pub = rospy.Publisher(self.SIM_PUB_TOPIC, Float32MultiArray, queue_size=3)
         else:
             # TODO: alert that unrecognized mode destination has been set
@@ -50,10 +50,10 @@ class ThrusterController():
 
     def handle_enable_controls(self, req):
         self.enabled = req.data
-        return {'success':True,'message':'Successfully set enabled to '+str(req.data)}
+        return {'success': True, 'message': 'Successfully set enabled to ' + str(req.data)}
 
     def update_thruster_allocs(self):
-        self.t_allocs = self.tm.calc_thruster_allocs(self.pid_outputs)
+        self.t_allocs = self.tm.calc_t_allocs(self.pid_outputs)
 
     def _on_x(self, x):
         self.pid_outputs[0] = x.data
@@ -84,25 +84,22 @@ class ThrusterController():
         rate = rospy.Rate(10)  # 10 Hz
 
         while not rospy.is_shutdown():
-            #rospy.loginfo(f32_t_allocs)
             if not self.enabled:
                 # If not enabled, publish all 0s.
-                if self.mode == 'robot':
+                if self.sim == 'false':
                     i8_t_allocs = ThrusterSpeeds()
                     i8_t_allocs.speeds = np.zeros(8)
                     self.pub.publish(i8_t_allocs)
-                elif self.mode == 'sim':
-                    #self.t_allocs[4:8] = 0
+                elif self.sim == 'true':
                     f32_t_allocs = Float32MultiArray()
                     f32_t_allocs.data = np.zeros(8)
                     self.pub.publish(f32_t_allocs)
             if self.enabled:
-                if self.mode == 'robot':
+                if self.sim == 'false':
                     i8_t_allocs = ThrusterSpeeds()
                     i8_t_allocs.speeds = (self.t_allocs * 127).astype(int)
                     self.pub.publish(i8_t_allocs)
-                elif self.mode == 'sim':
-                    #self.t_allocs[4:8] = 0
+                elif self.sim == 'true':
                     f32_t_allocs = Float32MultiArray()
                     f32_t_allocs.data = self.t_allocs
                     self.pub.publish(f32_t_allocs)
