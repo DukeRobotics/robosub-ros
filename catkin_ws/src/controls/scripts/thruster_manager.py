d584c23525392b1968c0f3fcab246881055dd772
ShaanGondalia <55925366+ShaanGondalia@users.noreply.github.com>
2020-02-22 12:19:00 -0500

    Thruster Orientation Debugging (#172)
    * Fixed config file
    
    * update config section of readme, delete unnecessary files
    
    * [untested] updated state republisher to new simplified input topics, updated 'mode' argument to 'sim'
    
    * add in functionality to flip thrusters in controls
    
    * update thruster flipping in config file to match robot as of now
    
    * delete unecessary file
    
    * clarify flipping wording in readme
    
    * Debugging, Typos
    
    * Debugging
    
    * Merge Conflicts
    
    * Debugging
    
    Co-authored-by: Samuel Rabinowitz <5happy1@users.noreply.github.com>

--- catkin_ws/src/controls/scripts/thruster_manager.py
+++ catkin_ws/src/controls/scripts/thruster_manager.py
@@ -44,7 +44,8 @@ class ThrusterManager():
         t_allocs[np.abs(t_allocs) < 0.001] = 0
 
         # Flip thrusters that are marked as such
-        for i in range(t_allocs):
+        
+        for i in range(len(t_allocs)):
             if self.thrusters[i].flipped:
                 t_allocs[i] = -t_allocs[i]
 
