ff44a5e6300c18dab39ea426cb60ebb45367e0db
echang264 <eric.t.chang@duke.edu>
2020-02-27 09:14:43 -0500

    move init_node s.t. it's called bf transforms

--- catkin_ws/src/motion_planning/scripts/task_runner.py
+++ catkin_ws/src/motion_planning/scripts/task_runner.py
@@ -7,14 +7,13 @@ from task_state import TaskState
 
 class TaskRunner:
 
-    RATE = 30 # Hz
+    RATE = 15 # Hz
 
     def __init__(self):
+        rospy.init_node("task_planning")
         self.competition_task = CompetitionTask()
 
     def start(self):
-        rospy.init_node("task_planning")
-
         self.rate = rospy.Rate(self.RATE)
 
         while not self.competition_task.finished:
