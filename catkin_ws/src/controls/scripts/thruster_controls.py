Fixed Scaling

catkin_ws/src/controls/scripts/thruster_controls.py
@@ -56,13 +56,6 @@ class ThrusterController():
         # Calculate thruster allocations
         self.t_allocs = self.tm.calc_t_allocs(self.pid_outputs)
 
-        # Scale thruster alloc max to PID max
-        pid_max = np.max(np.absolute(self.pid_outputs))
-        t_alloc_max = np.max(np.absolute(self.t_allocs))
-
-        # Multiply each thruster allocation by scaling ratio
-        self.t_allocs *= pid_max / t_alloc_max
-
     def _on_x(self, x):
         self.pid_outputs[0] = x.data
         self.update_thruster_allocs()
@@ -104,6 +97,14 @@ class ThrusterController():
                     self.pub.publish(f32_t_allocs)
 
             if self.enabled:
+                # Scale thruster alloc max to PID max
+                t_alloc_max = float(np.max(np.absolute(self.t_allocs)))
+                pid_max = float(np.max(np.absolute(self.pid_outputs)))
+
+                if(t_alloc_max != 0):
+                    # Multiply each thruster allocation by scaling ratio
+                    self.t_allocs *= pid_max / t_alloc_max
+
                 if self.sim == False:
                     i8_t_allocs = ThrusterSpeeds()
                     i8_t_allocs.speeds = (self.t_allocs * 127).astype(int)