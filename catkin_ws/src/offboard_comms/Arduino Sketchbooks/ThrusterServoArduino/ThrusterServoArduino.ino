6332dfedd8eba00d129eee0908574e50d4f688d8
Muthu Arivoli <ma381@duke.edu>
2020-03-04 16:54:17 -0500

    reduces memory that node handler uses and implements servo code

--- catkin_ws/src/offboard_comms/Arduino Sketchbooks/ThrusterServoArduino/ThrusterServoArduino.ino
+++ catkin_ws/src/offboard_comms/Arduino Sketchbooks/ThrusterServoArduino/ThrusterServoArduino.ino
@@ -10,15 +10,15 @@ Adafruit_PWMServoDriver pwm_multiplexer(0x40);
 
 #define NUM_THRUSTERS 8
 #define THRUSTER_TIMEOUT_MS 500
-#define NUM_SERVO 2
+#define NUM_SERVO 8
 
-uint64_t last_cmd_ms_ts;
+unsigned long last_cmd_ms_ts;
 
 int8_t thruster_speeds[NUM_THRUSTERS];
-//uint16_t servo_angle[NUM_SERVO];
+uint16_t servo_angle[NUM_SERVO];
 
 MultiplexedBasicESC *thrusters[NUM_THRUSTERS];
-//MultiplexedServo *servos[NUM_SERVO];
+MultiplexedServo *servos[NUM_SERVO];
 
 // reusing ESC library code
 void thruster_speeds_callback(const offboard_comms::ThrusterSpeeds &ts_msg){
@@ -27,29 +27,30 @@ void thruster_speeds_callback(const offboard_comms::ThrusterSpeeds &ts_msg){
     last_cmd_ms_ts = millis();
 }
 
-/*void servo_control_callback(const offboard_comms::ServoControl &sc_msg){
+void servo_control_callback(const offboard_comms::ServoControl &sc_msg){
     //copy the contents of the angle message to the local array
     memcpy(servo_angle, sc_msg.speeds, sizeof(servo_angle));
-}*/
+}
 
-ros::NodeHandle nh;
+//Sets node handle to have 2 subscribers, 0 publishers, and 150 bytes for input and output buffer
+ros::NodeHandle_<ArduinoHardware,2,0,150,150> nh;  
 ros::Subscriber<offboard_comms::ThrusterSpeeds> ts_sub("/offboard/thruster_speeds", &thruster_speeds_callback);
-//ros::Subscriber<offboard_comms::ServoControl> sc_sub("/offboard/servo_control", &servo_control_callback);
+ros::Subscriber<offboard_comms::ServoControl> sc_sub("/offboard/servo_control", &servo_control_callback);
 
 void setup(){
-  Serial.begin(57600);
+    Serial.begin(115200);
     nh.initNode();
     nh.subscribe(ts_sub);
-    //nh.subscribe(sc_sub);
+    nh.subscribe(sc_sub);
     pwm_multiplexer.begin();
     for (int i = 0; i < NUM_THRUSTERS; ++i){
         thrusters[i] = new MultiplexedBasicESC(&pwm_multiplexer, i);
         thrusters[i]->initialise();
     }
-    /*for (int i = 0; i < NUM_SERVO; ++i){
-        servos[i] = new MultiplexedServo(&pwm_multiplexer, i+8);
+    for (int i = 0; i < NUM_SERVO; ++i){
+        servos[i] = new MultiplexedServo(&pwm_multiplexer, i + NUM_THRUSTERS);
         servos[i]->initialise();
-    }*/
+    }
     // Wait for motors to fully initialise
     delay(2000);
     last_cmd_ms_ts = millis();
@@ -60,11 +61,10 @@ void loop(){
     if (last_cmd_ms_ts + THRUSTER_TIMEOUT_MS < millis())
         memset(thruster_speeds, 0, sizeof(thruster_speeds));
     for (int i = 0; i < NUM_THRUSTERS; ++i){
-      Serial.print(thruster_speeds[7]);
         thrusters[i]->run(thruster_speeds[i]);
     }
-    /*for (int i = 0; i < NUM_SERVO; ++i){
+    for (int i = 0; i < NUM_SERVO; ++i){
         servos[i]->run(servo_angle[i]);
-    }*/
+    }
     nh.spinOnce();
 }
