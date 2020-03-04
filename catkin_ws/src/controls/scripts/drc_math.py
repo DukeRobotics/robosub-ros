034d2a97a31554e66c72671065cbf24c73b22768
ShaanGondalia <55925366+ShaanGondalia@users.noreply.github.com>
2020-03-03 19:52:35 -0500

    Converting Global PID Outputs to Local Frame (#182)
    * Initial Commit
    
    * remove unnecessary space
    
    * Fixed function name and math
    
    Co-authored-by: Samuel Rabinowitz <5happy1@users.noreply.github.com>

--- catkin_ws/src/controls/scripts/drc_math.py
+++ catkin_ws/src/controls/scripts/drc_math.py
@@ -3,11 +3,10 @@
 import numpy as np
 from tf.transformations import unit_vector, quaternion_multiply, quaternion_conjugate
 
-def quad_vec_mult(q1, v1):
+def quat_vec_mult(q1, v1):
     """Rotate vector v1 by quaternion q1, and return the resulting vector.
     From https://answers.ros.org/question/196149/how-to-rotate-vector-by-quaternion-in-python/
     """
-    v1 = unit_vector(v1)
     q2 = list(v1)
     q2.append(0.0)
     return quaternion_multiply(
