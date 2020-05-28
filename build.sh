4832da3e23b9f88329e3c3a4ce328c3f13bc25b0
unknown <ma381@duke.edu>
2020-05-28 17:16:55 -0400

    update build script and add automated build test

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
 # A convenience script used to build our code. Takes one argument that specifies the workspace to build.
+
+set -e
 
 source /opt/ros/melodic/setup.bash
 
-cd core/catkin_ws || exit
+cd core/catkin_ws
 catkin build
 source devel/setup.bash
 cd ../..
 
-cd "$1"/catkin_ws || exit
+cd "$1"/catkin_ws
 catkin build
 source devel/setup.bash
