25e7cd9e72d9a8e306d5314a279e458df5e46346
Muthu Arivoli <ma381@duke.edu>
2020-05-29 00:52:26 -0400

    add help message and fix build script to return on exit from sourcing

--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 # A convenience script used to build our code. Takes one argument that specifies the workspace to build.
 
-set -e
+# If sourcing file then return instead of exiting shell
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    trap "trap - ERR;return 1" ERR
+else
+    trap "trap - ERR;exit 1" ERR
+fi
+
+# Print help message on invalid argument
+if [[ "$1" != "onboard" ]] && [[ "$1" != "landside" ]]; then
+    echo "ERROR. Invalid argument or no argument specified. Please specify either onboard or landside, as in:"
+    echo "source ./build.sh onboard"
+    echo "source ./build.sh landside"
+    false
+fi
 
 source /opt/ros/melodic/setup.bash
 
@@ -13,3 +26,6 @@ cd ../..
 cd "$1"/catkin_ws
 catkin build
 source devel/setup.bash
+
+# All commands successful, release trap on err
+trap - ERR
