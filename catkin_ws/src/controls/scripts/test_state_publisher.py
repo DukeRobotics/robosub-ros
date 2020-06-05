ceafbb610d94e7b32287c45c1c3697a7ad3bd3e3
PEP 8 Speaks <pep8speaks@gmail.com>
2020-06-05 08:25:40 +0530

    Fix PEP 8 errors in catkin_ws/src/controls/scripts/test_state_publisher.py

--- catkin_ws/src/controls/scripts/test_state_publisher.py
+++ catkin_ws/src/controls/scripts/test_state_publisher.py
@@ -5,6 +5,7 @@ from std_msgs.msg import Float64
 from geometry_msgs.msg import Pose, Twist
 from nav_msgs.msg import Odometry
 
+
 class TestStatePublisher():
     PUBLISHING_TOPIC_DESIRED_POSE = 'controls/desired_pose'
     PUBLISHING_TOPIC_DESIRED_POWERS = 'controls/desired_twist_power'
@@ -17,7 +18,7 @@ class TestStatePublisher():
         self._pub_desired_powers = rospy.Publisher(self.PUBLISHING_TOPIC_DESIRED_POWERS, Twist, queue_size=3)
         self._pub_current_state = rospy.Publisher(self.PUBLISHING_TOPIC_CURRENT_STATE, Odometry, queue_size=3)
 
-        #These values correspond to the desired pose of the robot
+        # These values correspond to the desired pose of the robot
         self.desired_pose = Pose()
         self.desired_pose.position.x = 1
         self.desired_pose.position.y = 1
@@ -27,7 +28,7 @@ class TestStatePublisher():
         self.desired_pose.orientation.z = 0
         self.desired_pose.orientation.w = 1
 
-        #These values correspond to the desired powers for the robot
+        # These values correspond to the desired powers for the robot
         self.desired_powers = Twist()
         self.desired_powers.linear.x = 1
         self.desired_powers.linear.y = 1
@@ -50,7 +51,7 @@ class TestStatePublisher():
         self.current_state.twist.twist.angular.x = 0
         self.current_state.twist.twist.angular.y = 0
         self.current_state.twist.twist.angular.z = 0
-        
+
         self.current_state.header.frame_id = 'odom'
         self.current_state.header.stamp = rospy.Time()
 
@@ -58,21 +59,22 @@ class TestStatePublisher():
         rate = rospy.Rate(15)
         while not rospy.is_shutdown():
             self._pub_desired_pose.publish(self.desired_pose)
-            #self._pub_current_state.publish(self.current_state)
+            # self._pub_current_state.publish(self.current_state)
             rate.sleep()
 
     def publish_desired_powers(self):
-        
+
         rate = rospy.Rate(15)
         while not rospy.is_shutdown():
             self._pub_desired_powers.publish(self.desired_powers)
-            #self._pub_current_state.publish(self.current_state)
+            # self._pub_current_state.publish(self.current_state)
             rate.sleep()
 
 
 def main():
     TestStatePublisher().publish_desired_pose()
-    #TestStatePublisher().publish_desired_powers()
+    # TestStatePublisher().publish_desired_powers()
+
 
 if __name__ == '__main__':
     main()
