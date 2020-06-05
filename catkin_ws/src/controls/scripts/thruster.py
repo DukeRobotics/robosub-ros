9d915a49b0fe031c967dc7d651a52275793746e6
PEP 8 Speaks <pep8speaks@gmail.com>
2020-06-05 08:25:41 +0530

    Fix PEP 8 errors in catkin_ws/src/controls/scripts/thruster.py

--- catkin_ws/src/controls/scripts/thruster.py
+++ catkin_ws/src/controls/scripts/thruster.py
@@ -4,7 +4,8 @@ import rospy
 import numpy as np
 from tf.transformations import quaternion_from_euler
 import math
-from drc_utils import quat_vec_mult 
+from drc_utils import quat_vec_mult
+
 
 class Thruster():
 
@@ -16,14 +17,15 @@ class Thruster():
         self.pos = pos
         self.rpy = rpy
         self.flipped = flipped
-        
-        to_rad = lambda x: x * math.pi / 180  # convert degrees to radians
 
-        q = quaternion_from_euler(to_rad(rpy[0]), to_rad(rpy[1]), to_rad(rpy[2]), 'sxyz') # create a quaternion from euler rpy
+        def to_rad(x): return x * math.pi / 180  # convert degrees to radians
+
+        q = quaternion_from_euler(to_rad(rpy[0]), to_rad(rpy[1]), to_rad(rpy[2]),
+                                  'sxyz')  # create a quaternion from euler rpy
         # sxyz denotes a static (extrinsic) frame of reference.
         # https://github.com/matthew-brett/transforms3d/blob/master/transforms3d/euler.py
         # https://en.wikipedia.org/wiki/Euler_angles
 
-        self.force_hat = quat_vec_mult(q, [1, 0, 0]) # vector representing force of thruster in x,y,z directions
-        
-        self.torque = np.cross(self.pos, self.force_hat) # vector representing torque generated from thruster
+        self.force_hat = quat_vec_mult(q, [1, 0, 0])  # vector representing force of thruster in x,y,z directions
+
+        self.torque = np.cross(self.pos, self.force_hat)  # vector representing torque generated from thruster
