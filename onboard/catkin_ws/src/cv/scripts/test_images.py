20767672152405ebbd51e52457ff8107e5fab963
Alan Bi <alan.bi@duke.edu>
2020-06-25 18:04:18 -0700

    Re-add test_images and fix cmake file

---
+++ onboard/catkin_ws/src/cv/scripts/test_images.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+import os
+from custom_msgs.srv import ToggleModel
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+
+# Mock the camera by publishing the same image to a topic
+class DummyImagePublisher:
+
+    NODE_NAME = 'test_images'
+    # IMAGE_TOPIC = '/test_images/image'
+    IMAGE_TOPIC = '/camera/{CAMERA_GOES_HERE}'
+
+    # Read in the dummy image and other misc. setup work
+    def __init__(self):
+        self.image_publisher = rospy.Publisher(self.IMAGE_TOPIC, Image, queue_size=10)
+
+        path = os.path.dirname(__file__)
+        image = cv2.imread(os.path.join(path, '../assets/buoy.jpg'), cv2.IMREAD_COLOR)
+        bridge = CvBridge()
+
+        self.image_msg = bridge.cv2_to_imgmsg(image, 'bgr8')
+
+    # Publish dummy image to topic every few seconds
+    def run(self):
+        rospy.init_node(self.NODE_NAME)
+
+        # Testing toggle_model service
+        service_name = 'toggle_model_{CAMERA_GOES_HERE}'
+        rospy.wait_for_service(service_name)
+        toggle_model = rospy.ServiceProxy(service_name, ToggleModel)
+
+        loop_rate = rospy.Rate(1)
+        model_enabled = True
+
+        count = 0
+        while not rospy.is_shutdown():
+            self.image_publisher.publish(self.image_msg)
+
+            # Testing toggle_model
+            if count % 30 == 0:
+                toggle_model('buoy', model_enabled)
+                # toggle_model('test', not model_enabled)
+                model_enabled = not model_enabled
+
+            count += 1
+            loop_rate.sleep()
+
+
+if __name__ == '__main__':
+    DummyImagePublisher().run()
