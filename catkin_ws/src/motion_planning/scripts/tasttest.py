4b5e13f31fe0336bb7f2c8a0b4d6c7fb085f8b53
echang264 <eric.t.chang@duke.edu>
2020-09-12 14:20:09 -0400

    clean up directory

--- catkin_ws/src/motion_planning/scripts/tasttest.py
+++
@@ -1,8 +0,0 @@
-import dependency_injector.providers as providers
-from combination_tasks import ListTask
-
-task_provider = providers.Singleton(ListTask)
-task_provider2 = providers.Singleton(ListTask)
-
-print(task_provider([]))
-print(task_provider2([]))
