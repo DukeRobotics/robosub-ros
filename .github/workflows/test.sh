ca316fd76eb682e683fef317fc8404c83142a143
Muthu Arivoli <ma381@duke.edu>
2020-09-13 03:44:38 -0400

    attempt to fix test

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -2,4 +2,9 @@
 
 set -ex
 
-docker-compose run --entrypoint /bin/bash "${SERVICE_NAME}" -c "cd ~/dev/robosub-ros && ./build.sh ${SERVICE_NAME}"
+docker-compose run --entrypoint /bin/bash "${SERVICE_NAME}" -c cd ~/dev && \
+                                ls -R && \
+                                git clone https://github.com/DukeRobotics/robosub-ros.git && \
+                                cd robosub-ros && \
+                                git checkout ${github.sha} && \
+                                ./build.sh ${SERVICE_NAME}
