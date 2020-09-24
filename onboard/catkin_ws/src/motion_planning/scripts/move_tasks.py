7471683f6d1c14a1f6c29a73bd7574cf1c71c265
echang264 <44076526+echang264@users.noreply.github.com>
2020-09-24 04:07:29 -0400

    improve readibility
    Co-authored-by: Muthu Arivoli <ma381@duke.edu>

--- onboard/catkin_ws/src/motion_planning/scripts/move_tasks.py
+++ onboard/catkin_ws/src/motion_planning/scripts/move_tasks.py
@@ -35,8 +35,9 @@ class MoveToPoseLocalTask(MoveToPoseGlobalTask):
 
     def _on_task_run(self):
         self.publish_desired_pose_global(self.transformed_pose)
-        if task_utils.at_pose(self.state.pose.pose, self.transformed_pose) and \
-                task_utils.at_vel(self.state.twist.twist, self.desired_twist):
+        at_desired_pose = task_utils.at_pose(self.state.pose.pose, self.transformed_pose)
+        at_desired_vel = task_utils.at_vel(self.state.twist.twist, self.desired_twist)
+        if at_desired_pose and at_desired_vel:
             self.finish()
 
 
