dd89e4a707f81ced5a642739cb62735bc85d123f
echang264 <eric.t.chang@duke.edu>
2020-09-26 12:56:36 -0400

    add gate task skeleton files

---
+++ onboard/catkin_ws/src/motion_planning/scripts/gate_task.py
@@ -0,0 +1,34 @@
+class GateTask(Task):
+    def __init__(self):
+        super(GateTask, self).__init__(*args, **kwargs)
+
+    def _on_task_run(self):
+        listtask1 = ListTask([
+            LeaderFollowerTask(IsThereAGateTask, RotateTask(...)),
+            select_smaller_box_of_seen_boxes_task,
+            find_normal_vector_from_middle_of_smaller_box_task,
+            move_to_and_align_with_normal_vector_task,
+            LeaderFollowerTask(within_tolerance_of_normal_vector_task & DistanceToGateTask, ListTask([move_to_gate_task, calculate_normal_task, calculate_dist_task]))
+            ])
+        task1 = LeaderFollowerTask(DistanceToGateTask, listtask1)
+        task2 = ListTask([CalculateNormalTask, AlignWithNormalTask, MoveTask])
+
+        ListTask of listtask1, task1, task2
+
+
+class DistanceToGateTask(Task):
+    def __init__(self, threshold):
+        super...
+
+    def _on_task_run(self):
+        if(distance < threshold):
+            self.finish()
+
+
+class IsThereAGateTask(Task):
+    def __init__(self):
+        super...
+
+    def _on_task_run(self):
+        if data_from_cv:
+            self.finish()
