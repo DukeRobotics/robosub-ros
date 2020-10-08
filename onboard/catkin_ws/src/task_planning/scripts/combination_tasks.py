8fba9ba6d33dd53a1a30fdb4d1ec6378827f5054
AndrewCouncil <andrew.p.council@gmail.com>
2020-10-07 20:14:42 -0400

    Implement DepSimulTask

--- onboard/catkin_ws/src/task_planning/scripts/combination_tasks.py
+++ onboard/catkin_ws/src/task_planning/scripts/combination_tasks.py
@@ -21,11 +21,11 @@ class ListTask(Task):
             self.tasks[self.curr_index].run()
 
 
-class SimulTask(Task):
-    """Run a list of tasks simulataneously"""
+class IndSimulTask(Task):
+    """Run a list of tasks simulataneously, exits when all tasks finished"""
 
     def __init__(self, tasks, *args, **kwargs):
-        super(SimulTask, self).__init__(*args, **kwargs)
+        super(IndSimulTask, self).__init__(*args, **kwargs)
 
         self.tasks = tasks
 
@@ -37,6 +37,27 @@ class SimulTask(Task):
                 self.all_finished = False
 
         if self.all_finished:
+            self.finish()
+
+
+class DepSimulTask(Task):
+    """Run a list of tasks simulataneously, exits when any task finished"""
+
+    def __init__(self, tasks, *args, **kwargs):
+        super(DepSimulTask, self).__init__(*args, **kwargs)
+
+        self.tasks = tasks
+
+    def _on_task_run(self):
+        self.any_finished = False
+        for task in self.tasks:
+            task.run()
+            if task.finished:
+                self.any_finished = True
+
+        if self.any_finished:
+            for task in self.tasks:
+                task.finish
             self.finish()
 
 
