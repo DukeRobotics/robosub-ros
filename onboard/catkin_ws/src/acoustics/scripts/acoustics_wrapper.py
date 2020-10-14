c051c6b15e1cab53b07262b52fee9060a4b98063
Brandon K Bae <brandon.bae@duke.edu>
2020-10-14 19:29:46 -0400

    did changes according to comments

--- onboard/catkin_ws/src/acoustics/scripts/acoustics_wrapper.py
+++ onboard/catkin_ws/src/acoustics/scripts/acoustics_wrapper.py
@@ -2,33 +2,37 @@
 
 import rospy
 import actionlib
-from custom_msgs.msg import AcousticsGuessGoal, AcousticsGuessAction, AcousticsProcessingGoal, AcousticsProcessingAction, AcousticsWrapperMessageAction, AcousticsWrapperMessageFeedback, AcousticsWrapperMessageResult
-import sys
-import os
+from custom_msgs.msg import AcousticsGuessGoal, AcousticsGuessAction, AcousticsProcessingGoal, AcousticsProcessingAction, AcousticsWrapperAction, AcousticsWrapperFeedback, AcousticsWrapperResult
 
 
-class ServerClientThing:
+
+class AcousticsWrapper:
 
     NODE_NAME = "acoustics_wrapper"
     ACTION_NAME = "call_guess_processing"
+    
+    GUESS_SAMPLE_FREQ = 625000
+    PROCESSING_SAMPLE_FREQ = 625000;
+    IF_DOUBLE = True;
+    PROCESSING_VERSION = 0;
+
 
     def __init__(self):
         rospy.init_node(self.NODE_NAME)
         self.client_guess = actionlib.SimpleActionClient('guess_acoustics', AcousticsGuessAction)  # name might actually be action name
         self.client_processing = actionlib.SimpleActionClient('process_acoustics', AcousticsProcessingAction)
-        self.server = actionlib.SimpleActionServer(self.ACTION_NAME, AcousticsWrapperMessageAction, self.execute, False)
+        self.server = actionlib.SimpleActionServer(self.ACTION_NAME, AcousticsWrapperAction, self.execute, False)
         self.server.start()
         rospy.spin()
 
-
     def publish_feedback(self, stage):
-        feedback = AcousticsWrapperMessageFeedback()
+        feedback = AcousticsWrapperFeedback()
         feedback.curr_stage = stage;
         feedback.total_stage = 2
         self.server.publish_feedback(feedback)
 
     def publish_result(self, processing_result):
-        result = AcousticsWrapperMessageResult()
+        result = AcousticsWrapperResult()
         result.hz_angle = processing_result.hz_angle
         self.server.set_succeeded(result)
 
@@ -36,7 +40,7 @@ class ServerClientThing:
         self.client_guess.wait_for_server()
         goal_client_guess = AcousticsGuessGoal()
         goal_client_guess.filename = this_goal.filename
-        goal_client_guess.samp_f = 625000
+        goal_client_guess.samp_f = self.GUESS_SAMPLE_FREQ
         goal_client_guess.tar_f = this_goal.tar_f
         self.client_guess.send_goal(goal_client_guess)
         self.client_guess.wait_for_result()
@@ -46,11 +50,11 @@ class ServerClientThing:
         self.client_processing.wait_for_server()
         goal_client_processing = AcousticsProcessingGoal()
         goal_client_processing.filename = this_goal.filename
-        goal_client_processing.if_double = True
-        goal_client_processing.version = 0
-        goal_client_processing.samp_f = 625000
+        goal_client_processing.if_double = self.IF_DOUBLE
+        goal_client_processing.version = self.PROCESSING_VERSION
+        goal_client_processing.samp_f = self.PROCESSING_SAMPLE_FREQ
         goal_client_processing.tar_f = this_goal.tar_f
-        goal_client_processing.guess = guess;
+        goal_client_processing.guess = guess
         self.client_processing.send_goal(goal_client_processing)
         self.client_processing.wait_for_result()
         self.publish_feedback(2)
@@ -60,4 +64,4 @@ class ServerClientThing:
 
 
 if __name__ == '__main__':
-    ServerClientThing()
+    AcousticsWrapper()
