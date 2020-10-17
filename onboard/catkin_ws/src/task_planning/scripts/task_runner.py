6a97ec35417485930bf2dcc4470d0e8d7d1ca058
Andy Zhou <andy.zhou@duke.edu>
2020-10-17 13:00:40 -0500

    Create local variable rate

--- onboard/catkin_ws/src/task_planning/scripts/task_runner.py
+++ onboard/catkin_ws/src/task_planning/scripts/task_runner.py
@@ -6,18 +6,18 @@ from competition_task import CompetitionTask
 
 
 class TaskRunner:
+    RATE = 15 #Hz
 
     def __init__(self):
         rospy.init_node("task_planning")
         self.competition_task = CompetitionTask()
 
     def start(self):
-        rate = 15 #Hz
-        self.rate = rospy.Rate(rate)
+        rate = rospy.Rate(self.RATE)
 
         while not self.competition_task.finished:
             self.competition_task.run()
-            self.rate.sleep()
+            rate.sleep()
 
 
 TaskRunner().start()
