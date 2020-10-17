48ab982eaca1621d607735d51fea130f2d053523
Andy Zhou <andy.zhou@duke.edu>
2020-10-17 11:32:19 -0500

    Change RATE to local variable

--- onboard/catkin_ws/src/task_planning/scripts/task_runner.py
+++ onboard/catkin_ws/src/task_planning/scripts/task_runner.py
@@ -6,14 +6,14 @@ from competition_task import CompetitionTask
 
 
 class TaskRunner:
-    RATE = 15  # Hz
 
     def __init__(self):
         rospy.init_node("task_planning")
         self.competition_task = CompetitionTask()
 
     def start(self):
-        self.rate = rospy.Rate(self.RATE)
+        rate = 15 #Hz
+        self.rate = rospy.Rate(rate)
 
         while not self.competition_task.finished:
             self.competition_task.run()
