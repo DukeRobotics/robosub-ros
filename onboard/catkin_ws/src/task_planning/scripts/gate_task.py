e68a2d82f9a02f8b64226799d830d65b53d48fd7
echang264 <eric.t.chang@duke.edu>
2020-11-11 19:11:27 -0500

    simplify gate algo, improve gate helper tasks

--- onboard/catkin_ws/src/task_planning/scripts/gate_task.py
+++ onboard/catkin_ws/src/task_planning/scripts/gate_task.py
@@ -18,12 +18,10 @@ class GateTask(Task):
             [DistanceToGateTask(self.DIST_THRESH), ListTask([IsThereAGateTask()], -1)])
 
         self.rotate_to_gate = LeaderFollowerTask(IsThereAGateTask(), AllocatePowerTask(0, 0, 0, 0, 0, self.power))
-        self.move_along_x_y = MoveToGateTask()
-        self.gate_path_condition = OnGatePathTask()  # finishes if fall off path or lose camera data
-        self.move_forward = LeaderFollowerTask(self.gate_path_condition, AllocatePowerTask(self.power, 0, 0, 0, 0, 0))
-        self.gate_sequence = ListTask([self.rotate_to_gate,
-                                       self.move_along_x_y,
-                                       self.move_forward], 100)
+        self.move_to_gate = LeaderFollowerTask(IsThereAGateTask(finish_with_data=False), MoveToGateTask())
+        # self.gate_path_condition = OnGatePathTask()  # finishes if fall off path or lose camera data
+        # self.move_forward = LeaderFollowerTask(self.gate_path_condition, AllocatePowerTask(self.power, 0, 0, 0, 0, 0))
+        self.gate_sequence = ListTask([self.rotate_to_gate, self.move_to_gate], 100)
         self.move_through_gate = AllocatePowerTask(self.power, 0, 0, 0, 0, 0)
         self.do_gate_magic = LeaderFollowerTask(self.gate_search_condition,
                                                 ListTask[self.gate_sequence, self.move_through_gate])
@@ -34,14 +32,16 @@ class GateTask(Task):
             self.finish()
 
 
-class MoveToGateTask(MoveToPoseGlobalTask):  # this will be local
+class MoveToGateTask(Task):  # this can be generalized to dynamic point, relies on gate data already accounting for single bad frames
     def __init__(self):
-        super(MoveToGateTask, self).__init__(0, 0, 0, 0, 0, 0)
+        super(MoveToGateTask, self).__init__()
 
-    def _on_task_start(self):
-        self.gate_vector = self.gate_data
-        self.desired_pose.position = self.gate_vector
-        #  super(MoveToGateTask, self)._on_task_start()  # transforms pose
+    def _on_task_run(self):
+        self.global_pose = task_utils.transform('base_link', 'odom', self.gate_data)
+        self.publish_desired_pose_global(self.global_pose)
+        at_desired_pose_vel = task_utils.stopped_at_pose(self.state.pose.pose, self.transformed_pose, self.state.twist.twist)
+        if at_desired_pose_vel:
+            self.finish()
 
 
 class DistanceToGateTask(Task):
@@ -57,15 +57,19 @@ class DistanceToGateTask(Task):
             self.finish()
 
 
-class IsThereAGateTask(Task):  # finishes if cannot see a gate
+class IsThereAGateTask(Task):  # finishes if there is gate data that is NOT None
     def __init__(self):
-        super(IsThereAGateTask, self).__init__()
+        super(IsThereAGateTask, self).__init__(finish_with_data=True)
 
     def _on_task_run(self):
-        if (self.gate_data.x is not None) and (self.gate_data.y is not None) and (self.gate_data.z is not None):
-            self.finish()
+        if finish_with_data:
+            if (self.gate_data.x is not None) and (self.gate_data.y is not None) and (self.gate_data.z is not None):
+                self.finish()
+        else:
+            if (self.gate_data.x is None) or (self.gate_data.y is None) or (self.gate_data.z is None):
+                self.finish()
 
-
+"""
 class OnGatePathTask(Task):
     def __init__(self):
         super(OnGatePathTask, self).__init__()
@@ -78,5 +82,5 @@ class OnGatePathTask(Task):
         desired_pose.position.y = self.gate_data.y
         if not task_utils.at_pose(self.state.pose.pose, desired_pose):
             self.finish()
-
+"""
 # write some function that checks if gate data is bad
