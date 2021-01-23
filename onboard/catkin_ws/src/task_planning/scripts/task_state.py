9bcb113e1a62722c944fe5c2bcc1295f596b8c24
Andy Zhou <andy.zhou@duke.edu>
2021-01-23 13:01:57 -0600

    Create gate and gate_tick listeners

--- onboard/catkin_ws/src/task_planning/scripts/task_state.py
+++ onboard/catkin_ws/src/task_planning/scripts/task_state.py
@@ -2,6 +2,7 @@ import rospy
 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose, Twist, Vector3
+from custom_msgs.msg import CVObject
 
 
 class TaskState:
@@ -9,7 +10,8 @@ class TaskState:
     DESIRED_POSE_TOPIC = 'controls/desired_pose'
     DESIRED_TWIST_POWER_TOPIC = 'controls/desired_power'
     DESIRED_TWIST_VELOCITY_TOPIC = 'controls/desired_twist'
-    CV_GATE_DATA_TOPIC = 'cv/gate_data'
+    CV_GATE_DATA_TOPIC = '/cv/_start_gate/left'
+    CV_GATE_TICK_DATA_TOPIC = '/cv/_start_tick/left'
 
     def __init__(self):
         self.state_listener = rospy.Subscriber(self.STATE_TOPIC, Odometry, self._on_receive_state)
@@ -17,8 +19,11 @@ class TaskState:
         self.desired_twist_power_publisher = rospy.Publisher(self.DESIRED_TWIST_POWER_TOPIC, Twist, queue_size=5)
         self.desired_twist_velocity_publisher = rospy.Publisher(self.DESIRED_TWIST_VELOCITY_TOPIC, Twist, queue_size=5)
         self.state = None
-        self.cv_gate_data_listener = rospy.Subscriber(self.CV_GATE_DATA_TOPIC, Vector3, self._on_receive_gate_data)
+        self.cv_gate_data_listener = rospy.Subscriber(self.CV_GATE_DATA_TOPIC, CVObject, self._on_receive_gate_data)
+        self.cv_gate_tick_data_listener = rospy.Subscriber(self.CV_GATE_TICK_DATA_TOPIC, CVObject,
+                                                           self._on_receive_gate_tick_data)
         self.gate_data = None
+        self.gate_tick_data = None
 
     def _on_receive_state(self, state):
         """Receive the state, update initial_state if it is empty
@@ -27,3 +32,6 @@ class TaskState:
 
     def _on_receive_gate_data(self, gate_data):
         self.gate_data = gate_data
+
+    def _on_receive_gate_tick_data(self, gate_tick_data):
+        self.gate_tick_data = gate_tick_data
