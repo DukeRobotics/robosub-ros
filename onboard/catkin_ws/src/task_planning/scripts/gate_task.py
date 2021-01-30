24ea06f68167ddca5fdfecaa6d0afdc00eaccf92
echang264 <eric.t.chang@duke.edu>
2021-01-30 11:06:19 -0500

    delete comments

--- onboard/catkin_ws/src/task_planning/scripts/gate_task.py
+++ onboard/catkin_ws/src/task_planning/scripts/gate_task.py
@@ -29,22 +29,6 @@ class GateTask(Task):
             self.finish()
 
 
-"""
-
-        self.gate_search_condition = IndSimulTask(
-            [DistanceToGateTask(self.DIST_THRESH), ListTask([IsThereAGateTask()], -1)])
-
-        self.rotate_to_gate = LeaderFollowerTask(IsThereAGateTask(), AllocateVelocityTask(0, 0, 0, 0, 0, self.velocity))
-        self.move_to_gate = LeaderFollowerTask(IsThereAGateTask(finish_with_data=False), MoveToGateTask())
-        # self.gate_path_condition = OnGatePathTask()  # finishes if fall off path or lose camera data
-        # self.move_forward = LeaderFollowerTask(self.gate_path_condition, AllocatePowerTask(self.power, 0, 0, 0, 0, 0))
-        self.gate_sequence = ListTask([self.rotate_to_gate, self.move_to_gate], 100)
-        self.move_through_gate = AllocateVelocityTask(self.velocity, 0, 0, 0, 0, 0)
-        self.do_gate_magic = LeaderFollowerTask(self.gate_search_condition,
-                                                ListTask[self.gate_sequence, self.move_through_gate])
-"""
-
-
     def scrutinize_gate(self, gate_data, gate_tick_data):
         """Finds the distance from the gate to each of the four edges of the frame
 
@@ -84,7 +68,6 @@ class GateTask(Task):
         return res
 
 
-
 class NearGateTask(Task):
     def __init__(self, threshold):
         super(NearGateTask, self).__init__()
@@ -112,59 +95,3 @@ class GateCenteredTask(Task):
             else:
                 if gate_info["offset_h"] > threshold:
                     self.finish()
-
-"""
-class IsThereAGateTask(Task):  # finishes if there is gate data that is NOT None
-    def __init__(self):
-        super(IsThereAGateTask, self).__init__()
-
-    def _on_task_run(self):
-        gate_info = scrutinize_gate(self.gate_data, self.gate_tick_data)
-        if gate_info:
-            self.finish()
-
-
-
-
-
-
-
-
-class MoveToGateTask(Task):  # this can be generalized to dynamic point, relies on gate data already accounting for single bad frames
-    def __init__(self):
-        super(MoveToGateTask, self).__init__()
-
-    def _on_task_run(self):
-        self.global_pose = task_utils.transform('base_link', 'odom', self.gate_data)
-        self.publish_desired_pose_global(self.global_pose)
-        at_desired_pose_vel = task_utils.stopped_at_pose(self.state.pose.pose, self.transformed_pose, self.state.twist.twist)
-        if at_desired_pose_vel:
-            self.finish()
-
-
-class DistanceToGateTask(Task):
-    def __init__(self, threshold):
-        super(DistanceToGateTask, self).__init__()
-        self.threshold = threshold
-
-    def _on_task_start(self):
-        self.distance_to_gate = np.linalg.norm(self.gate_data.x, self.gate_data.y, self.gate_data.z)
-
-    def _on_task_run(self):
-        if self.distance_to_gate < self.threshold:
-            self.finish()
-
-
-class OnGatePathTask(Task):
-    def __init__(self):
-        super(OnGatePathTask, self).__init__()
-
-    def _on_task_run(self):
-        if self.gate_data.x is None:  # if gate data is bad finish
-            self.finish()
-        desired_pose = self.state.pose.pose
-        desired_pose.position.x = self.gate_data.x
-        desired_pose.position.y = self.gate_data.y
-        if not task_utils.at_pose(self.state.pose.pose, desired_pose):
-            self.finish()
-"""
