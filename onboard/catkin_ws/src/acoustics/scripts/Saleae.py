7d37be1896b08b268308c38c74e664ebf081e6bc
Muthu Arivoli <ma381@duke.edu>
2021-02-16 19:24:08 -0500

    spelling changes

---
+++ onboard/catkin_ws/src/acoustics/scripts/Saleae.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+from custom_msgs.msg import AcousticsGuessGoal, AcousticsGuessAction, \
+    AcousticsProcessingGoal, AcousticsProcessingAction, \
+    SaleaeAction, SaleaeFeedback, SaleaeResult, SaleaeGoal
+import saleae
+import sys
+import os
+import resource_retriever as rr
+
+
+
+class Saeleae:
+
+    NODE_NAME = "saleae"
+    ACTION_NAME = "call_saleae"
+    IP_ADDRESS = 'localhost'
+    PORT = 10429
+    CAPTURE_COUNT = 1
+    CAPTURE_DURATION = 1
+    HYDROPHONE_SET_1 = [0, 3]
+    HYDROPHONE_SET_2 = [4, 7]
+
+    def __init__(self):
+        rospy.init_node(self.NODE_NAME)
+        self.server = actionlib.SimpleActionServer(self.ACTION_NAME, SaleaeAction, self.execute, False)
+        self.server.start()
+        rospy.spin()
+
+    def publish_feedback(self, stage):
+        feedback = SaleaeFeedback()
+        feedback.curr_stage = stage
+        feedback.total_stage = 2
+        self.server.publish_feedback(feedback)
+
+    def publish_result(self, return_file):
+        result = SaleaeResult()
+        result.return_file_name = return_file
+        self.server.set_succeeded(result)
+
+    def validate_path(self, path):
+        if path is not None:
+            if not os.path.dirname(path):
+                rospy.logerr('the specified ' + ' directory does not exist or is invalid')
+                rospy.logerr('you specified: ' + path)
+                exit(1)
+
+    def execute(self, goal):
+        rospy.loginfo("a")
+        s = saleae.Saleae(self.IP_ADDRESS, self.PORT)
+        goal_name = goal.save_path
+        file_name = "/root/dev/robosub-ros/onboard/catkin_ws/src/acoustics/data/" + goal_name + "{0}.{1}"
+        # "/root/dev/robosub-ros/onboard/catkin_ws/src/acoustics/data/{0}.{1}"
+        s.set_capture_seconds(self.CAPTURE_DURATION)
+        save_path = ""
+        self.publish_feedback(1)
+        if goal.hydrophone_set == 1:
+            for i in range(self.CAPTURE_COUNT):
+               # s.capture_to_file(file_name.format(i, "logicdata"))
+                save_path = file_name.format(i, "csv")
+                print('exporting data to ' + save_path)
+                s.export_data2(save_path, analog_channels=[0, 1, 2, 3])
+        if goal.hydrophone_set == 2:
+            for i in range(self.CAPTURE_COUNT):
+                s.capture_to_file(file_name.format(i, "logicdata"))
+                save_path = file_name.format(i, "csv")
+                print('exporting data to ' + save_path)
+                s.export_data2(save_path, analog_channels=[4, 5, 6, 7])
+        self.publish_feedback(2)
+        self.publish_result(save_path)
+
+if __name__ == '__main__':
+    Saleae()
+
--- onboard/catkin_ws/src/acoustics/scripts/acoustics_wrapper.py
+++ onboard/catkin_ws/src/acoustics/scripts/acoustics_wrapper.py
@@ -4,7 +4,7 @@ import rospy
 import actionlib
 from custom_msgs.msg import AcousticsGuessGoal, AcousticsGuessAction, \
     AcousticsProcessingGoal, AcousticsProcessingAction, \
-    SaeleaeGoal, SaeleaeAction, \
+    SaleaeGoal, SaleaeAction, \
     AcousticsWrapperAction, AcousticsWrapperFeedback, AcousticsWrapperResult
 from datetime import datetime
 import sys
@@ -26,7 +26,7 @@ class AcousticsWrapper:
     def __init__(self):
         rospy.init_node(self.NODE_NAME)
         #
-        self.client_sampling = actionlib.SimpleActionClient('call_saeleae', SaeleaeAction)
+        self.client_sampling = actionlib.SimpleActionClient('call_saleae', SaleaeAction)
         #
         self.client_guess = actionlib.SimpleActionClient('guess_acoustics', AcousticsGuessAction)
         self.client_processing = actionlib.SimpleActionClient('process_acoustics', AcousticsProcessingAction)
@@ -37,7 +37,7 @@ class AcousticsWrapper:
     def publish_feedback(self, stage):
         feedback = AcousticsWrapperFeedback()
         feedback.curr_stage = stage;
-        feedback.total_stage = 2
+        feedback.total_stage = 4
         self.server.publish_feedback(feedback)
 
     def publish_result(self, processing_result):
@@ -78,40 +78,41 @@ class AcousticsWrapper:
 
     def execute(self, this_goal):
         time_now = datetime.now()
-        guess_data = self.create_sample_file("guess", time_now)
-        processing_data = self.create_sample_file("processing", time_now)
-
+        guess_export_name = "Date:"+time_now.strftime("%m_%d_%Y;%H.%M.%S") + ";guess"
+        processing_export_name = "Date:"+time_now.strftime("%m_%d_%Y;%H.%M.%S") + ";processing"
         #
         self.client_sampling.wait_for_server()
-        goal_client_sampling = SaeleaeGoal()
+        goal_client_sampling = SaleaeGoal()
         goal_client_sampling.hydrophone_set = 1
-        goal_client_sampling.save_path = guess_data
+        goal_client_sampling.save_path = guess_export_name
         goal_client_sampling.capture_directory = ''
         self.client_sampling.send_goal(goal_client_sampling)
         self.client_sampling.wait_for_result()
         guess_file_name = self.client_sampling.get_result().return_file_name
+        rospy.loginfo(guess_file_name)
         self.publish_feedback(1)
         #
 
-        guess_file = 'matlab_custom_cheap_hydrophone_-2_7_4_(1).csv'
+        guess_file = guess_export_name + ".csv"
         guess = self.get_guess(this_goal, guess_file)
         self.publish_feedback(2)
 
         #
         self.client_sampling.wait_for_server()
-        goal_client_sampling = SaeleaeGoal()
+        goal_client_sampling = SaleaeGoal()
         goal_client_sampling.hydrophone_set = 2
-        goal_client_sampling.save_path = processing_data
+        goal_client_sampling.save_path = processing_export_name
         goal_client_sampling.capture_directory = ''
         self.client_sampling.send_goal(goal_client_sampling)
         self.client_sampling.wait_for_result()
         processing_file_name = self.client_sampling.get_result().return_file_name
-        self.publish_feedback(1)
+        rospy.loginfo(processing_file_name)
+        self.publish_feedback(3)
         #
 
-        processing_file = 'matlab_custom_cheap_hydrophone_-2_7_4_(1).csv'
+        processing_file = 'matlab_custom_cheap_hydrophone_-2_7_4_(1).csv' #processing_export_name + ".csv"
         processing_result = self.get_processing_angle(this_goal, processing_file, guess)
-        self.publish_feedback(2)
+        self.publish_feedback(4)
 
         self.publish_result(processing_result)
 
