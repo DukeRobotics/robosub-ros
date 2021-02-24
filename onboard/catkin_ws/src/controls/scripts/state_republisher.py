77e11f9cbd4e7c6fa3eb44d4003fd3f196f9ddec
Eric Jiang <edj9@duke.edu>
2021-02-23 19:36:47 -0500

    Publish local state

--- onboard/catkin_ws/src/controls/scripts/state_republisher.py
+++ onboard/catkin_ws/src/controls/scripts/state_republisher.py
@@ -9,6 +9,7 @@ from tf import TransformListener
 
 class StateRepublisher:
     STATE_TOPIC = '/state'
+    LOCAL_STATE_TOPIC = '/state_local'
 
     def __init__(self):
         rospy.init_node('state_republisher')
@@ -20,27 +21,34 @@ class StateRepublisher:
             self._pub_pose[d] = rospy.Publisher(utils.get_pose_topic(d), Float64, queue_size=3)
             self._pub_twist[d] = rospy.Publisher(utils.get_twist_topic(d), Float64, queue_size=3)
 
+        self._pub_local_state = rospy.Publisher(self.LOCAL_STATE_TOPIC, Odometry, queue_size=3)
+
         rospy.init_node('state_republisher')
         rospy.Subscriber(self.STATE_TOPIC, Odometry, self.receive_odometry)
         rospy.spin()
 
     def receive_odometry(self, odometry):
         if 'base_link' in self.listener.getFrameStrings():
+            local_pose = utils.transform_pose(self.listener, 'odom', 'base_link', odometry.pose.pose)
+
             utils.publish_data_dictionary(
                 self._pub_pose,
                 utils.get_axes(),
-                utils.parse_pose(
-                    utils.transform_pose(self.listener, 'odom', 'base_link', odometry.pose.pose)
-                )
+                utils.parse_pose(local_pose)
             )
 
+            local_twist = utils.transform_twist(self.listener, 'odom', 'base_link', odometry.twist.twist)
             utils.publish_data_dictionary(
                 self._pub_twist,
                 utils.get_axes(),
-                utils.parse_twist(
-                    utils.transform_twist(self.listener, 'odom', 'base_link', odometry.twist.twist)
-                )
+                utils.parse_twist(local_twist)
             )
+
+            local_state = Odometry()
+            local_state.header.frame_id = 'base_link'
+            local_state.pose.pose = local_pose
+            local_state.twist.twist = local_twist
+            self._pub_local_state.publish(local_state)
 
 
 def main():
