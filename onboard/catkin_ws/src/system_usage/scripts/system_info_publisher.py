3f7a6308ce9002294e6fc15d4a8b50ca5224c224
kyy2 <57043438+kyy2@users.noreply.github.com>
2021-03-13 13:37:49 -0500

    Removed whitespace from empty lines

--- onboard/catkin_ws/src/system_usage/scripts/system_info_publisher.py
+++ onboard/catkin_ws/src/system_usage/scripts/system_info_publisher.py
@@ -9,7 +9,7 @@ from custom_msgs.msg import Memory, SystemUsage
 class SystemInfoPublisher:
     TOPIC_NAME = 'system/usage'
     NODE_NAME = 'system_usage_publisher'
-    
+
     def __init__(self):
         self._pub = rospy.Publisher(self.TOPIC_NAME, SystemUsage, queue_size=10)
         self._current_msg = SystemUsage()
@@ -63,7 +63,7 @@ class SystemInfoPublisher:
             self._current_msg.header.frame_id = "system_usage_link"
             self._pub.publish(self._current_msg)
 
-            
+
 if __name__ == '__main__':
     try:
         SystemInfoPublisher().run()
