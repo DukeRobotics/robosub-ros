3b09caf3c2250d373814c7f57a2f481a216e2c88
Muthu Arivoli <ma381@duke.edu>
2021-03-23 01:22:53 -0400

    disable shellcheck

--- scripts/format-code.sh
+++ scripts/format-code.sh
@@ -3,6 +3,7 @@
 # A script that automatically formats code to the standard.
 
 # Run python formatter
+# shellcheck disable=SC2035
 autopep8 -r -i --aggressive --aggressive --max-line-length 120 \
     --exclude **/sim.py,**/simConst.py \
     core/catkin_ws/src \
