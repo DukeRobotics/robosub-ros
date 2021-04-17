c38166dc30d4dbcc78c9ee36598317c853058194
Brandon K Bae <bkunjun@gmail.com>
2021-04-17 13:30:04 -0400

    working arduino upload code

--- onboard/catkin_ws/src/system_utils/scripts/arduino_upload.py
+++ onboard/catkin_ws/src/system_utils/scripts/arduino_upload.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import shlex
 import rospy
 import actionlib
 import subprocess
+import shutil
+import os
 from custom_msgs.msg import UploadArduinoAction, UploadArduinoFeedback, UploadArduinoResult
 
 class Saleae:
@@ -28,17 +31,22 @@ class Saleae:
         self.server.set_succeeded(result)
 
     def execute(self, goal):
-        subprocess.check_call("export PATH=\"~/dev/robosub-ros/test:$PATH\"")
-        subprocess.check_call("rosrun rosserial_arduino make_libraries.py .")
-        subprocess.check_call("zip ros_lib.zip ros_lib")
-        subprocess.check_call("arduino-cli") #assuming arduino cli is part of path variables
-        subprocess.check_call("export ARDUINO_LIBRARY_ENABLE_UNSAFE_INSTALL=true")
-        subprocess.check_call("arduino-cli lib install --zip-path ros_lib.zip")
-        subprocess.check_call("rm -r ros_lib")
-        subprocess.check_call("rm ros_lib.zip")
-        subprocess.check_call("arduino-cli core install arduino:avr") #for the robot use arduino:samd to download the arduino nano core
-        subprocess.check_call("arduino-cli compile -b arduino:avr:mega ../data/" + goal.filename)
-        subprocess.check_call("arduino-cli upload -b arduino:avr:mega -p " + goal.port + " ../data/" + goal.filename)
+        current_dir = os.getcwd()
+        print(current_dir)
+        subprocess.check_call("cd ~/dev/robosub-ros/onboard/catkin_ws/src/system_utils/scripts", shell=True)
+        ##subprocess.check_call("export PATH=\"/root/dev/robosub-ros/test:$PATH\"")
+        subprocess.check_call("rosrun rosserial_arduino make_libraries.py ~/dev/robosub-ros/onboard/catkin_ws/src/system_utils/scripts", shell=True)
+        os.chdir('/root/dev/robosub-ros/onboard/catkin_ws/src/system_utils/scripts')
+        current_dir = os.getcwd()
+        print(current_dir)
+        shutil.make_archive('/root/dev/robosub-ros/onboard/catkin_ws/src/system_utils/scripts/ros_lib', 'zip', '/root/dev/robosub-ros/onboard/catkin_ws/src/system_utils/scripts', 'ros_lib')
+        subprocess.check_call('arduino-cli') #assuming arduino cli is part of path variables
+        ##subprocess.check_call("export ARDUINO_LIBRARY_ENABLE_UNSAFE_INSTALL=true", shell=True)
+        subprocess.check_call("arduino-cli lib install --zip-path /root/dev/robosub-ros/onboard/catkin_ws/src/system_utils/scripts/ros_lib.zip", shell = True)
+        subprocess.check_call("rm ros_lib.zip", shell = True)
+        subprocess.check_call("arduino-cli core install arduino:avr", shell = True) #for the robot use arduino:samd to download the arduino nano core
+        subprocess.check_call("arduino-cli compile -b arduino:avr:nano ../data/" + goal.filename, shell = True)
+        subprocess.check_call("arduino-cli upload -b arduino:avr:mega -p " + goal.port + " ../data/" + goal.filename, shell = True)
         self.publish_result()
 
 
