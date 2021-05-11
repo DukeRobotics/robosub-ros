7a456f6bf5a89bdf7bfd00dad20050231629aa24
Muthu Arivoli <ma381@duke.edu>
2021-05-10 21:22:31 -0400

    Automatically launch Saleae Logic with Saleae ROS node (#278)
    * Symlink saleae logic to /usr/local/bin, let python-saleae automatically launch logic, and rename saleae to saleae_interface to avoid naming conflicts
    
    * Fix space
    
    * lint

--- onboard/catkin_ws/src/acoustics/scripts/saleae_interface.py
+++ onboard/catkin_ws/src/acoustics/scripts/saleae_interface.py
@@ -7,14 +7,14 @@ import pandas as pd
 import saleae
 import resource_retriever as rr
 from datetime import datetime
+import os
 
 
 class Saleae:
 
     NODE_NAME = "saleae"
     ACTION_NAME = "call_saleae"
-    IP_ADDRESS = "localhost"
-    PORT = 10429
+    SALEAE_SETTINGS = '/root/docker-build/Logic/Settings/settings.xml'
     CAPTURE_DURATION = 2
     HYDROPHONE_SET = {HydrophoneSet.GUESS: [0, 1, 2, 3], HydrophoneSet.PROCESS: [4, 5, 6, 7]}
     FILE_EXTENSIONS = {HydrophoneSet.GUESS: "_guess", HydrophoneSet.PROCESS: "_processing"}
@@ -22,8 +22,15 @@ class Saleae:
     def __init__(self):
         rospy.init_node(self.NODE_NAME)
         self.server = actionlib.SimpleActionServer(self.ACTION_NAME, SaleaeAction, self.execute, False)
+
+        # Delete the saleae settings file to get around this issue:
+        # https://github.com/saleae/SaleaeSocketApi/issues/14
+        if os.path.exists(self.SALEAE_SETTINGS):
+            os.remove(self.SALEAE_SETTINGS)
+        self.saleae = saleae.Saleae(args='-disablepopups -socket')
+        rospy.loginfo("Saleae started")
+
         self.server.start()
-        self.saleae = saleae.Saleae(self.IP_ADDRESS, self.PORT)
         rospy.spin()
 
     def publish_feedback(self, stage, total_stages, msg):
@@ -48,13 +55,14 @@ class Saleae:
 
         time_now = datetime.now()
         export_name = "date_" + time_now.strftime("%m_%d_%Y_%H_%M_%S") + self.FILE_EXTENSIONS[goal.hydrophone_set.type]
-        package_path = 'package://acoustics/data/' + export_name + '_({1}).csv'
+        package_path = 'package://acoustics/data/' + export_name + '_({0}).csv'
         save_paths = []
 
         for i in range(goal.capture_count):
             self.publish_feedback(i + 1, goal.capture_count + 1, "Starting capture {}".format(i))
             save_paths.append(package_path.format(i))
             save_path = rr.get_filename(package_path.format(i), use_protocol=False)
+            rospy.loginfo("Path: " + save_path)
             self.saleae.export_data2(save_path, analog_channels=self.HYDROPHONE_SET[goal.hydrophone_set.type])
             self.format_csv(save_path)
 
