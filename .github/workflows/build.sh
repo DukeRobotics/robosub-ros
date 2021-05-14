92149fd60cbeb54fe46d95dfb2bc4aa542ee8ace
Muthu Arivoli <ma381@duke.edu>
2021-05-13 23:59:24 -0400

    try using already built wheel

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -12,27 +12,8 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce=5:19.03.15~3-0~ubuntu-bionic docker-ce-cli=5:19.03.15~3-0~ubuntu-bionic containerd.io
 
-curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
-  sudo apt-key add -
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
-  sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
-sudo apt-get update
-sudo apt-get install nvidia-container-runtime
-
-# Use experimental Docker with nvidia support
-sudo tee /etc/docker/daemon.json <<EOF
-{
-    "experimental": true,
-    "runtimes": {
-        "nvidia": {
-            "path": "nvidia-container-runtime",
-            "runtimeArgs": []
-        }
-    },
-    "default-runtime": "nvidia"
-}
-EOF
+# Use experimental Docker
+sudo jq -n '{experimental: true}' | sudo tee /etc/docker/daemon.json > /dev/null
 sudo systemctl restart docker
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
--- docker/onboard/linux/arm64/TorchInstaller.sh
+++ docker/onboard/linux/arm64/TorchInstaller.sh
@@ -9,9 +9,4 @@ pip install --no-cache-dir future torch-1.4.0-cp27-cp27mu-linux_aarch64.whl
 rm torch-1.4.0-cp27-cp27mu-linux_aarch64.whl
 
 # Install torchvision
-git clone --branch v0.5.0 https://github.com/pytorch/vision torchvision
-cd torchvision
-export BUILD_VERSION=0.5.0 
-python setup.py install
-cd ../
-rm -rf torchvision
+pip install --no-cache-dir torchvision-0.5.0-cp27-cp27mu-linux_aarch64.whl
