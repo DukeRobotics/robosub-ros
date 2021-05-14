3fcdbf91bbab03f24ef2e7771e93059dac4569c2
Muthu Arivoli <ma381@duke.edu>
2021-05-13 21:25:13 -0400

    lint

--- docker/onboard/linux/arm64/TorchInstaller.sh
+++ docker/onboard/linux/arm64/TorchInstaller.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Install Pytorch for ARM using nvidia's providied package for the jetson
 wget https://nvidia.box.com/shared/static/1v2cc4ro6zvsbu0p8h6qcuaqco1qcsif.whl -O torch-1.4.0-cp27-cp27mu-linux_aarch64.whl
 apt-get update && apt-get install -y --no-install-recommends libopenblas-base libopenmpi-dev 
