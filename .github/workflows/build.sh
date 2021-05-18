73417f4ea556e12fbe28fa287078e659bf2c81f3
Muthu Arivoli <ma381@duke.edu>
2021-05-18 19:54:37 -0400

    fix docker build

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -20,4 +20,4 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 # Build image
 cd docker/"${SERVICE_NAME}"
-docker build --build-arg BASE_IMAGE="${BASE_IMAGE}" --build-arg TARGETPLATFORM="${TARGETPLATFORM}" --platform "${TARGETPLATFORM}" -t "${IMAGE_NAME}" .
+docker build --build-arg BASE_IMAGE="${BASE_IMAGE}" --build-arg TARGETPLATFORM="${TARGETPLATFORM}" --build-arg CUDA=${CUDA} --platform "${TARGETPLATFORM}" -t "${IMAGE_NAME}" .
