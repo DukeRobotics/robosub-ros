cb9c37381619b541534d249d3728d16730a745ac
Brandon K Bae <bkunjun@gmail.com>
2021-06-19 11:53:49 -0700

    updated build.sh itself

--- build.sh
+++ build.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
-# A convenience script used to build our code.
-# Takes one argument that specifies the workspace to build when used to build.
-# When used to clean, argument 1 should be clean, and argument 2, if present is the workspace to clean.
-# If argument 2 is omitted when cleaning, then all workspaces will be cleaned.
+# A convenience script used to build our code within the workplace you are in .
+# When used to clean, argument 1 should be clean, and argument 2, if present should be "all" specifying to clean all workspaces.
+# If argument 2 is omitted when cleaning, then the current workspace will be cleaned.
 
 set -e
 
 # Handle cleaning
 if [[ "$1" == "clean" ]]; then
-  if [[ -z "$2" ]] || [[ "$2" == "onboard" ]]; then
+  if [[ "$2" == "all" ]]; then
     cd onboard/catkin_ws
     catkin clean -y
     cd ../..
-  fi
-
-  if [[ -z "$2" ]] || [[ "$2" == "landside" ]]; then
     cd landside/catkin_ws
     catkin clean -y
     cd ../..
-  fi
-
-  if [[ -z "$2" ]]; then
     cd core/catkin_ws
     catkin clean -y
     cd ../..
+    echo "clean all successful"
+  fi
+  if [[ -z "$2" ]]; then
+    cd "${COMPUTER_TYPE}"/catkin_ws
+    catkin clean -y
+    cd ../..
+    echo "clean ${COMPUTER_TYPE} successful"
   fi
   exit 0
-fi
-
-# Print help message on invalid argument
-if [[ "$1" != "onboard" ]] && [[ "$1" != "landside" ]]; then
-    echo "ERROR. Invalid argument or no argument specified. Please specify either onboard or landside, as in:"
-    echo "./build.sh onboard"
-    echo "./build.sh landside"
-    exit 1
 fi
 
 # shellcheck disable=SC1091
@@ -45,11 +37,11 @@ catkin build
 source devel/setup.bash
 cd ../..
 
-cd "$1"/catkin_ws
+cd "${COMPUTER_TYPE}"/catkin_ws
 catkin build
 # shellcheck disable=SC1091
 source devel/setup.bash
 cd ../..
 
 echo "If you did not source this scipt, please run"
-echo "source ${1}/catkin_ws/devel/setup.bash"
+echo "source "${COMPUTER_TYPE}"/catkin_ws/devel/setup.bash"
