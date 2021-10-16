39f4fb22a045b718b89201a80e79f1a8e408f3a4
Maverick Chung <mc608@duke.edu>
2021-10-16 13:55:35 -0400

    filled out buoyancy functions in object code

--- landside/catkin_ws/src/simulation/personal/scenes/object_code.lua
+++ landside/catkin_ws/src/simulation/personal/scenes/object_code.lua
@@ -1,9 +1,92 @@
 require "math"
 
 function extsysCall_init()
+    hr = sim.getObjectAssociatedWithScript(sim.handle_self)
+    buoyancyEnabled = 0
+    dragType = 1 -- 0 for linear, 1 for quadratic
+    dragCoef = 1
+    anchorPoints = {}
 end
 
 function extsysCall_actuation()
+    if buoyancyEnabled then
+        applyBuoyancy()
+    end
+end
+
+function applyBuoyancy()
+    pos = sim.getObjectPosition(hr, -1)
+
+    res, xsizemin = sim.getObjectFloatParameter(hr, 15)
+    res, ysizemin = sim.getObjectFloatParameter(hr, 16)
+    res, zsizemin = sim.getObjectFloatParameter(hr, 17)
+    res, xsizemax = sim.getObjectFloatParameter(hr, 18)
+    res, ysizemax = sim.getObjectFloatParameter(hr, 19)
+    res, zsizemax = sim.getObjectFloatParameter(hr, 20)
+    xsize = xsizemax - xsizemin
+    ysize = ysizemax - ysizemin
+    zsize = zsizemax - zsizemin
+
+    grav = sim.getArrayParameter(sim.arrayparam_gravity)
+    pos[3] = pos[3] - zsize / 2 --fudge due to inconsistency with relative measurements (?)
+    if zsize <= (waterlevel - pos[3]) then
+        subdepth = zsize
+    else
+        subdepth = (waterlevel - pos[3])
+    end
+    fbuoy = xsize * ysize * subdepth * (-1) * grav[3] * p
+    if pos[3] > waterlevel then
+        fbuoy = 0
+        subdepth = 0
+    end
+
+    transform = sim.getObjectMatrix(hr, -1)
+    res = sim.invertMatrix(transform)
+    relbuoy = sim.multiplyVector(transform, { 0, 0, fbuoy })
+
+    v, angv = sim.getVelocity(hr)
+    dragforcelin = {calc_dragforcelin(v[1], ysize, subdepth),
+                    calc_dragforcelin(v[2], xsize, subdepth),
+                    calc_dragforcelin(v[3], xsize, ysize)}
+    if pos[3] > waterlevel then
+        dragforcelin[3] = 0
+    end
+    dragforceang = {calc_dragforceang(angv[1], ysize, xsize),
+                    calc_dragforceang(angv[2], ysize, xsize),
+                    calc_dragforceang(angv[3], ysize, subdepth)}
+
+    sim.addForceAndTorque(hr, dragforcelin, dragforceang)
+    sim.addForce(hr, {0,0,0}, relbuoy) -- 0,0,0 is center of buoyancy, here
+end
+
+function get_sign(x)
+    if (x > 0) then
+        return 1
+    elseif (x < 0) then
+        return -1
+    else
+        return 0
+    end
+end
+
+function calc_dragforcelin(linvel, length, depth)
+    if dragType then
+        return -p * math.abs(linvel ^ 2) * get_sign(linvel) * dragCoef * length * depth
+    end
+    return -p * math.abs(linvel) * get_sign(linvel) * dragCoef * length * depth
+end
+
+function calc_dragforceang(angvel, length, depth)
+    --if quadratic:
+    -- -p * angvelocity * angvelocity * x * y * y * y * dragCoef / 12
+    -- if linear
+    -- -p * angvelocity * x * y * y * dragCoef / 4
+    angdragfudgecoef = 1 -- 0.05
+    if dragType then
+        return -p * math.abs(angvel ^ 2) * get_sign(angvel) * dragCoef * length ^ 3 * depth / 12 * angdragfudgecoef
+    end
+    return -p * math.abs(angvel ^ 1) * get_sign(angvel) * dragCoef * length ^ 2 * depth / 4 * angdragfudgecoef
+
 end
 
 -- Enables or disables simulation of drag and buoyancy for this object.
@@ -12,7 +95,8 @@ end
 --   and 0 otherwise
 -- Outputs:
 --   None
-function applyBuoyancyDrag(inInts, inFloats, inString, inBuffer)
+function enableBuoyancyDrag(inInts, inFloats, inString, inBuffer)
+    buoyancyEnabled = inInts[1]
     return {},{},{},''
 end
 
@@ -21,17 +105,19 @@ end
 --   inFloats: Array whose first value is the new drag coefficient.
 -- Outputs:
 --   None
-function setDragCoefficient(inInts, inFloats, inString, inBuffer)
+function setdragCoefficient(inInts, inFloats, inString, inBuffer)
+    dragCoef = inFloats[1]
     return {},{},{},''
 end
 
 -- Sets the type of drag for this object to linear or quadratic.
 -- Params:
---   inInts: Array whose first value 0 if linear and greater than 0 
+--   inInts: Array whose first value 0 if linear and nonzero
 --   if quadratic
 -- Outputs:
 --   None
 function setDragType(inInts, inFloats, inString, inBuffer)
+    dragType = inInts[1]
     return {},{},{},''
 end
 
@@ -41,6 +127,7 @@ end
 -- Outputs:
 --   None
 function setMass(inInts, inFloats, inString, inBuffer)
+    sim.setShapeMass(handle, inFloats[1])
     return {},{},{},''
 end
 
