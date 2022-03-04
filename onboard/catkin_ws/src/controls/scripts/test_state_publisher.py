ccadcd58c237d799f289fecccfc33e12c89e9dcc
nickchak21 <nc165@duke.edu>
2022-03-03 19:34:54 -0500

    add local pose

--- onboard/catkin_ws/src/controls/scripts/test_state_publisher.py
+++ onboard/catkin_ws/src/controls/scripts/test_state_publisher.py
@@ -3,7 +3,8 @@
 import rospy
 from geometry_msgs.msg import Pose, Twist
 from nav_msgs.msg import Odometry
-
+import controls_utils
+from tf import TransformListener
 
 class TestStatePublisher:
     PUBLISHING_TOPIC_DESIRED_POSE = 'controls/desired_pose'
@@ -13,6 +14,7 @@ class TestStatePublisher:
 
     def __init__(self):
         rospy.init_node('test_state_publisher')
+        self.listener = TransformListener()
 
         self._pub_desired_pose = rospy.Publisher(self.PUBLISHING_TOPIC_DESIRED_POSE, Pose, queue_size=3)
         self._pub_desired_twist = rospy.Publisher(self.PUBLISHING_TOPIC_DESIRED_TWIST, Twist, queue_size=3)
@@ -20,14 +22,25 @@ class TestStatePublisher:
         self._pub_current_state = rospy.Publisher(self.PUBLISHING_TOPIC_CURRENT_STATE, Odometry, queue_size=3)
 
         # These values correspond to the desired global pose of the robot
-        self.desired_pose = Pose()
-        self.desired_pose.position.x = 1
-        self.desired_pose.position.y = 0
-        self.desired_pose.position.z = 0
-        self.desired_pose.orientation.x = 0
-        self.desired_pose.orientation.y = 0
-        self.desired_pose.orientation.z = 0
-        self.desired_pose.orientation.w = 1
+        self.desired_pose_global = Pose()
+        self.desired_pose_global.position.x = 1
+        self.desired_pose_global.position.y = 0
+        self.desired_pose_global.position.z = 0
+        self.desired_pose_global.orientation.x = 0
+        self.desired_pose_global.orientation.y = 0
+        self.desired_pose_global.orientation.z = 0
+        self.desired_pose_global.orientation.w = 1
+
+        # These values correspond to the desired local pose of the robot
+        self.desired_pose_local = Pose()
+        self.desired_pose_local.position.x = 1
+        self.desired_pose_local.position.y = 0
+        self.desired_pose_local.position.z = 0
+        self.desired_pose_local.orientation.x = 0
+        self.desired_pose_local.orientation.y = 0
+        self.desired_pose_local.orientation.z = 0
+        self.desired_pose_local.orientation.w = 1
+        self.desired_pose_transformed = controls_utils.transform_pose(self.listener, "base_link", "odom", self.desired_pose_local)
 
         # These values correspond to the desired global twist for the robot
         # Max linear z speed is ~ -0.26 -- ignore (for different mass)
@@ -66,10 +79,17 @@ class TestStatePublisher:
         self.current_state.header.frame_id = 'odom'
         self.current_state.header.stamp = rospy.Time()
 
-    def publish_desired_pose(self):
+    def publish_desired_pose_global(self):
         rate = rospy.Rate(15)
         while not rospy.is_shutdown():
-            self._pub_desired_pose.publish(self.desired_pose)
+            self._pub_desired_pose.publish(self.desired_pose_global)
+            # self._pub_current_state.publish(self.current_state)
+            rate.sleep()
+
+    def publish_desired_pose_local(self):
+        rate = rospy.Rate(15)
+        while not rospy.is_shutdown():
+            self._pub_desired_pose.publish(self.desired_pose_transformed)
             # self._pub_current_state.publish(self.current_state)
             rate.sleep()
 
@@ -89,7 +109,8 @@ class TestStatePublisher:
 
 
 def main():
-    TestStatePublisher().publish_desired_pose()
+    # TestStatePublisher().publish_desired_pose_global()
+    TestStatePublisher().publish_desired_pose_local()
     # TestStatePublisher().publish_desired_twist()
     # TestStatePublisher().publish_desired_power()
 
