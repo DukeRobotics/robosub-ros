b38192c932d455407918362ee969fb4fa673d68d
AndrewCouncil <andrew.p.council@gmail.com>
2022-07-30 12:28:00 -0400

    select tasks to run based on boolean values

--- onboard/catkin_ws/src/task_planning/scripts/task_runner.py
+++ onboard/catkin_ws/src/task_planning/scripts/task_runner.py
@@ -2,7 +2,7 @@
 
 import rospy
 import smach.StateMachine
-from gate_task import create_gate_task_sm
+from gate_task import create_simple_gate_task_sm, create_simplest_gate_task_sm
 
 
 class TaskRunner:
@@ -10,16 +10,69 @@ class TaskRunner:
 
     def __init__(self):
         rospy.init_node("task_planning")
+        # SET THESE TO CURRENT WORKING STATE OF ROBOT
+        self.YAW_WORKING = True
+        self.CV_GATE_WORKING = True
+        self.CV_BUOY_WORKING = True
+        self.ACOUSTICS_WORKING = False
 
     def start(self):
         sm_top = smach.StateMachine(outcomes=['task_runner_succeeded', 'task_runner_failed'])
 
         with sm_top:
-            sm_gate = create_gate_task_sm()
-            smach.StateMachine.add('GATE_STATE', sm_gate,
-                                   transitions={
-                                       'gate_task_succeeded': 'task_runner_succeeded',
-                                       'gate_task_failed': 'task_runner_failed'})
+            # Determine gate task to run based on state of CV
+            if self.CV_GATE_WORKING:
+                sm_gate = create_simple_gate_task_sm(1)
+            else:
+                sm_gate = create_simplest_gate_task_sm(1)
+            
+            # Determine the next task to run based on state of CV and acoustics
+            if self.CV_BUOY_WORKING:
+                # If the bouy is working, move to buoy task
+                next_step = 'BUOY_TASK'
+            elif self.ACOUSTICS_WORKING:
+                # If the acoustics are working, do octagon with acoustics
+                next_step = 'ACOUSTICS_TASK'
+            else:
+                # If neither is working, do octagon without acoustics
+                next_step = 'EYEBALL_OCTAGON_TASK'
+            
+            # Add the gate task selected earlier to the top state machine
+            smach.StateMachine.add('GATE_TASK', sm_gate,
+                                    transitions={
+                                        'succeeded': next_step,
+                                        'failed': 'task_runner_failed'
+                                    })
+            
+            # If the buoy is working, add the buoy task
+            if self.CV_BUOY_WORKING:
+                sm_buoy = create_buoy_task_sm()
+                if self.ACOUSTICS_WORKING:
+                    next_step = 'ACOUSTICS_TASK'
+                else:
+                    next_step = 'task_runner_succeeded'
+                smach.StateMachine.add('BUOY_TASK', sm_buoy,
+                                        transitions={
+                                            'succeeded': next_step,
+                                            'failed': 'task_runner_failed'
+                                        })
+            
+            # If the acoustics are working, add the acoustics task
+            if self.ACOUSTICS_WORKING:
+                sm_acoustics = create_acoustics_task_sm()
+                smach.StateMachine.add('ACOUSTICS_TASK', sm_acoustics,
+                                        transitions={
+                                            'succeeded': 'task_runner_succeeded',
+                                            'failed': 'task_runner_failed'
+                                        })
+            
+            # If neither are working, just eyeball to octagon and then end
+            elif self.CV_BUOY_WORKING == False:
+                smach.StateMachine.add('EYEBALL_OCTAGON_TASK', create_eyeball_octagon_task_sm(),
+                                        transitions={
+                                            'succeeded': 'task_runner_succeeded',
+                                            'failed': 'task_runner_failed'
+                                        })
 
 
 def main():
