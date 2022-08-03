5fcb3939fb57dc60bdcc46e2851898e999983066
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-08-03 21:10:15 +0400

    Document thruster config
    Added docstrings for thruster and thruster_manager explaining how thruster objects are created and how thruster allocations are calculated.

--- onboard/catkin_ws/src/controls/scripts/thruster.py
+++ onboard/catkin_ws/src/controls/scripts/thruster.py
@@ -1,10 +1,24 @@
+#!/usr/bin/env python3
+
 import numpy as np
 from tf.transformations import quaternion_from_euler
 from controls_utils import quat_vec_mult
 
 
 class Thruster:
-
+    """Describes a thruster in terms of position and orientation. Calculates the
+    force and torque that the thruster applies when spun.
+    
+    Attributes:
+        flipped: Whether the thruster is reversed, meaning power has to be negated
+        force_hat: The unit force vector (N) applied by the thruster in x, y, z 
+        MAX_NEG_POW: The maximum negative power that can be sent to the thruster
+        MAX_POS_POW: The maximum positive power that can be sent to the thruster
+        pos: The x, y, z position of the thruster (m) relative to the robot's center of mass
+        rpy: The euler angle orientation of the thruster (degrees) relative to the robot's frame of reference
+        torque: The torque vector (N m) applied by the thruster
+    """
+    
     # Constants for scaling thruster speeds to ROS thruster controller
     MAX_NEG_POW = -128
     MAX_POS_POW = 127
@@ -21,5 +35,4 @@ class Thruster:
         # https://en.wikipedia.org/wiki/Euler_angles
 
         self.force_hat = quat_vec_mult(q, [1, 0, 0])  # unit vector representing force of thruster in x,y,z directions
-
         self.torque = np.cross(self.pos, self.force_hat)  # vector representing torque generated from thruster
