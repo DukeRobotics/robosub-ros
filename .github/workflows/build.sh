8c40ec698cd048d33718fc8e38267a56e67d03c8
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-09-07 15:55:55 -0400

    Cache images in GitHub

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -11,5 +11,4 @@ cd docker/"${SERVICE_NAME}"
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --name multiarch --driver docker-container --use --bootstrap
 docker buildx inspect --bootstrap
-docker buildx build --build-arg BASE_IMAGE="${BASE_IMAGE}" --build-arg TARGETPLATFORM="${TARGETPLATFORM}" --build-arg CUDA="${CUDA}" --platform="${TARGETPLATFORM}" --load -t"${IMAGE_NAME}" .
-docker images
+docker buildx build --build-arg BASE_IMAGE="${BASE_IMAGE}" --build-arg TARGETPLATFORM="${TARGETPLATFORM}" --build-arg CUDA="${CUDA}" --platform="${TARGETPLATFORM}" --cache-to=type=gha --cache-from=type=gha --load -t"${IMAGE_NAME}" .
