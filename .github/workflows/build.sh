5bce4b13a12e474c5d84b00614d955a0927c74e0
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-09-08 13:47:12 -0400

    Switch to docker builder

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,14 +2,9 @@
 
 set -ex
 
-# Use experimental Docker
-sudo apt-get update
 sudo systemctl restart docker
 
 # Build image
 cd docker/"${SERVICE_NAME}"
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker buildx create --name multiarch --driver docker-container --use --bootstrap
-docker buildx inspect --bootstrap
 docker buildx build --build-arg BASE_IMAGE="${BASE_IMAGE}" --build-arg TARGETPLATFORM="${TARGETPLATFORM}" --build-arg CUDA="${CUDA}" --platform="${TARGETPLATFORM}" --load -t"${IMAGE_NAME}" .
-docker images
