8d7b35fcebb01b02575de2eecd7f9741999aa608
ShaanGondalia <shaan.gondalia@duke.edu>
2022-09-09 09:49:14 -0400

    Migrate CI to docker buildx  (#339)
    * Bump ubuntu version for workflows
    
    * Migrate build workflow to buildx
    
    * Cache images in GitHub
    
    * Revert "Cache images in GitHub"
    
    This reverts commit 8c40ec698cd048d33718fc8e38267a56e67d03c8.
    
    * Switch to docker builder
    
    * Address comments

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,22 +2,6 @@
 
 set -ex
 
-sudo apt-get update
-sudo apt-get remove docker docker-engine docker.io containerd runc
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install docker-ce=5:19.03.15~3-0~ubuntu-bionic docker-ce-cli=5:19.03.15~3-0~ubuntu-bionic containerd.io
-
-# Use experimental Docker
-sudo jq -n '{experimental: true}' | sudo tee /etc/docker/daemon.json > /dev/null
-sudo systemctl restart docker
-
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-
-# Build image
 cd docker/"${SERVICE_NAME}"
-docker build --build-arg BASE_IMAGE="${BASE_IMAGE}" --build-arg TARGETPLATFORM="${TARGETPLATFORM}" --build-arg CUDA="${CUDA}" --platform "${TARGETPLATFORM}" -t "${IMAGE_NAME}" .
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx build --build-arg BASE_IMAGE="${BASE_IMAGE}" --build-arg TARGETPLATFORM="${TARGETPLATFORM}" --build-arg CUDA="${CUDA}" --platform="${TARGETPLATFORM}" --load -t "${IMAGE_NAME}" .
