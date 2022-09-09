8713784a5a817a955c01bf4abbdc736c3e9201be
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-09-09 09:02:03 -0400

    Address comments

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-sudo systemctl restart docker
-
-# Build image
 cd docker/"${SERVICE_NAME}"
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker buildx build --build-arg BASE_IMAGE="${BASE_IMAGE}" --build-arg TARGETPLATFORM="${TARGETPLATFORM}" --build-arg CUDA="${CUDA}" --platform="${TARGETPLATFORM}" --load -t"${IMAGE_NAME}" .
+docker buildx build --build-arg BASE_IMAGE="${BASE_IMAGE}" --build-arg TARGETPLATFORM="${TARGETPLATFORM}" --build-arg CUDA="${CUDA}" --platform="${TARGETPLATFORM}" --load -t "${IMAGE_NAME}" .
