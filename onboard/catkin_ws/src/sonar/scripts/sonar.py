996c7b76bd8c867a8c680ab24fca6b43be694504
tylerfeldman321 <tylerfeldman321@gmail.com>
2022-09-30 17:55:40 -0400

    Add docstrings and TODOs

--- onboard/catkin_ws/src/sonar/scripts/sonar.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar.py
@@ -3,7 +3,7 @@
 from brping import Ping360
 import rospy
 
-SERIAL_PORT_NAME = "COM4"
+SERIAL_PORT_NAME = "COM4"  # TODO determine what port this is for the robot.
 BAUD_RATE = 115200  # hz
 SAMPLE_PERIOD_TICK_DURATION = 25e-9  # s
 SPEED_OF_SOUND_IN_WATER = 1480  # m/s
@@ -12,21 +12,45 @@ NODE_NAME = "sonar"
 
 
 class Sonar:
+    """Class to interface with the Sonar device.
+    """
+
     def __init__(self, serial_port_name=SERIAL_PORT_NAME, baud_rate=BAUD_RATE):
         self.ping360 = Ping360()
         self.ping360.connect_serial(serial_port_name, baud_rate)
-        self.sample_period = 80  # TODO: double check this
+        self.sample_period = 80  # TODO: get the sample period from the device
 
         rospy.init_node(NODE_NAME)
 
     def is_initialized(self):
+        """Checks if the sonar device is initialized.
+
+        Returns:
+            bool: True if the sonar device is initialized
+        """
         return self.ping360.initialize()
 
     def request_data_at_angle(self, angle_in_gradians):
+        """Set sonar device to provided angle and retrieve data.
+
+        Args:
+            angle_in_gradians (float): Angle 
+
+        Returns:
+            dictionary: Response from the device. See https://docs.bluerobotics.com/ping-protocol/pingmessage-ping360/#get for information about the dictionary response.
+        """
         response = self.ping360.transmitAngle(angle_in_gradians)
         return response
 
     def get_distance_of_sample(self, sample_index):
+        """Get the distance in meters of a sample given its index in the data array returned from the device.
+
+        Args:
+            sample_index (int): Index of the sample in the data array.
+
+        Returns:
+            float: Distance in meters of the sample from the sonar device.
+        """
         distance = (self.sample_period * SAMPLE_PERIOD_TICK_DURATION) * sample_index * SPEED_OF_SOUND_IN_WATER / 2.0
         return distance
 
