3ef15671566bdcefe976df6457a09d4685dbba36
Brokemia <37253216+Brokemia@users.noreply.github.com>
2022-10-12 19:18:59 -0400

    Migrate potentially useful code back to test_state_publisher.py

--- onboard/catkin_ws/src/controls/scripts/test_state_publisher.py
+++ onboard/catkin_ws/src/controls/scripts/test_state_publisher.py
@@ -2,6 +2,7 @@
 
 import rospy
 from geometry_msgs.msg import Pose, Twist
+from std_msgs.msg import Float64
 from nav_msgs.msg import Odometry
 import controls_utils
 from tf import TransformListener
@@ -21,6 +22,15 @@ class TestStatePublisher:
     def __init__(self):
         rospy.init_node('test_state_publisher')
         self.listener = TransformListener()
+        rospy.Subscriber("/controls/x_pos/setpoint", Float64, self._on_receive_data_x)
+        rospy.Subscriber("/controls/y_pos/setpoint", Float64, self._on_receive_data_y)
+        rospy.Subscriber("/controls/z_pos/setpoint", Float64, self._on_receive_data_z)
+        rospy.Subscriber("/controls/yaw_pos/setpoint", Float64, self._on_receive_data_yaw)
+
+        self.current_setpoint = [100.0, 100.0, 100.0]  # x,y,z
+        self.MOVE_OFFSET_CONSTANT = 1
+        self.current_yaw = 100.0
+        self.MOVE_OFFSET_CONSTANT_ANGULAR = 0.2
 
         sleep(1)
 
@@ -115,6 +125,113 @@ class TestStatePublisher:
             # self._pub_current_state.publish(self.current_state)
             rate.sleep()
 
+    def move_to_pos_and_stop(self, x, y, z):
+        self.desired_pose_local.position.x = x
+        self.desired_pose_local.position.y = y
+        self.desired_pose_local.position.z = z
+        # self.desired_pose_local.orientation.x = 0
+        # self.desired_pose_local.orientation.y = 0
+        # self.desired_pose_local.orientation.z = 0
+        # self.desired_pose_local.orientation.w = 1
+
+        self.recalculate_local_pose()
+
+        rate = rospy.Rate(15)
+
+        delay = 0
+        while not rospy.is_shutdown():
+            delay += 1
+            self._pub_desired_pose.publish(self.desired_pose_transformed)
+
+            if delay > 30:
+                # print(self.current_setpoint)
+                # if abs(self.current_setpoint[0]) <= self.MOVE_OFFSET_CONSTANT and
+                # abs(self.current_setpoint[1]) <= self.MOVE_OFFSET_CONSTANT and
+                # abs(self.current_setpoint[2]) <= self.MOVE_OFFSET_CONSTANT:
+                if abs(self.current_setpoint[0]) <= self.MOVE_OFFSET_CONSTANT and abs(
+                        self.current_setpoint[1]) <= self.MOVE_OFFSET_CONSTANT:
+                    # print("Done with loop")
+                    break
+            rate.sleep()
+        # print("Finished")
+
+    def move_to_yaw_and_stop(self, x, y, z, w):
+        # self.desired_pose_local.position.x = 0
+        # self.desired_pose_local.position.y = 0
+        self.desired_pose_local.position.z = -0.1
+        self.desired_pose_local.orientation.x = x
+        self.desired_pose_local.orientation.y = y
+        self.desired_pose_local.orientation.z = z
+        self.desired_pose_local.orientation.w = w
+
+        self.recalculate_local_pose()
+
+        rate = rospy.Rate(15)
+
+        delay = 0
+        while not rospy.is_shutdown():
+            delay += 1
+            self._pub_desired_pose.publish(self.desired_pose_transformed)
+
+            if delay > 30:
+                if abs(self.current_yaw) <= self.MOVE_OFFSET_CONSTANT_ANGULAR and abs(
+                        self.current_yaw) <= self.MOVE_OFFSET_CONSTANT_ANGULAR:
+                    break
+            rate.sleep()
+
+    # Point the robot forward in the global frame
+    def global_face_forward(self):
+        # Set desired local pose to current postion
+        self.desired_pose_local.position.x = 0
+        self.desired_pose_local.position.y = 0
+        self.desired_pose_local.position.z = 0
+        self.desired_pose_local.orientation.x = 0
+        self.desired_pose_local.orientation.y = 0
+        self.desired_pose_local.orientation.z = 0
+        self.desired_pose_local.orientation.w = 1
+
+        # Convert to global pose
+        self.recalculate_local_pose()
+
+        # Set global pose to point forward relative to start orientation
+        self.desired_pose_transformed.orientation.x = self.global_start_pose.orientation.x
+        self.desired_pose_transformed.orientation.y = self.global_start_pose.orientation.y
+        self.desired_pose_transformed.orientation.z = self.global_start_pose.orientation.z
+        self.desired_pose_transformed.orientation.w = self.global_start_pose.orientation.w
+
+        rate = rospy.Rate(15)
+
+        # DELAY BASED STOPPING PROTOCOL
+        # tot = 0
+        # duration = 60_000
+        # while tot < duration and not rospy.is_shutdown():
+        #     self._pub_desired_pose.publish(self.desired_pose_transformed)
+        #     tot += 15
+        #     rate.sleep()
+
+        # SET POINT BASED STOPPING PROTOCOL
+        delay = 0
+        while not rospy.is_shutdown():
+            delay += 1
+            self._pub_desired_pose.publish(self.desired_pose_transformed)
+
+            if delay > 30:
+                if abs(self.current_yaw) <= self.MOVE_OFFSET_CONSTANT_ANGULAR and abs(
+                        self.current_yaw) <= self.MOVE_OFFSET_CONSTANT_ANGULAR:
+                    break
+            rate.sleep()
+
+    def _on_receive_data_x(self, data):
+        self.current_setpoint[0] = data.data
+
+    def _on_receive_data_y(self, data):
+        self.current_setpoint[1] = data.data
+
+    def _on_receive_data_z(self, data):
+        self.current_setpoint[2] = data.data
+
+    def _on_receive_data_yaw(self, data):
+        self.current_yaw = data.data
 
 def main():
     # TestStatePublisher().publish_desired_pose_global()
