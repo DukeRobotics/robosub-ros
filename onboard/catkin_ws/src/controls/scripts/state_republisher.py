6403fa96b907207c74e60186b1a92b05d6fe5282
ShaanGondalia <shaan.gondalia@duke.edu>
2022-10-17 13:15:09 -0400

    Refactor Controls (#320)
    * Document thruster config
    
    Added docstrings for thruster and thruster_manager explaining how thruster objects are created and how thruster allocations are calculated.
    
    * Refactor and document thruster scaling
    
    * Document state republisher
    
    * Split PID publishing and desired state logic
    
    * Refactor utility methods
    
    Reduce the boilerplate for some utility methods and move some unique methods to specific scripts.
    
    * Document desired state and pid manager
    
    * Refactor and document launch files
    
    Documented controls.launch, adding explanation for static transform hack. Reformatted velocity.launch to follow the same format as position.launch. Marked some TODOs in pid launch files that require some further testing.
    
    * Document utility methods
    
    * Update controls documentation
    
    * Add safety check TODO
    
    * Auto-format code to pep8 standard
    
    * Fix bugs and typos
    
    * Remove unused imports
    
    * Move rate instantiation outside of loop
    
    * Fix typo

--- onboard/catkin_ws/src/controls/scripts/state_republisher.py
+++ onboard/catkin_ws/src/controls/scripts/state_republisher.py
@@ -8,40 +8,51 @@ from tf import TransformListener
 
 
 class StateRepublisher:
+    """Handles re-publishing of monolithic state topic (Odom) to separate topics. Also handles conversion from global
+    to local reference frame (controls operates solely in the local frame). PID loops require individual state topics
+    to compare to set-points.
+
+    Attributes:
+        listener: The ROS transformation listener used to transform from global to local reference frame
+    """
+
     STATE_TOPIC = '/state'
     LOCAL_STATE_TOPIC = '/state_local'
 
     def __init__(self):
+        """Initializes pub/sub config used to transform and isolate state topics."""
         rospy.init_node('state_republisher')
         self._pub_pose = {}
         self._pub_twist = {}
         self.listener = TransformListener()
 
         for d in utils.get_axes():
-            self._pub_pose[d] = rospy.Publisher(utils.get_pose_topic(d), Float64, queue_size=3)
-            self._pub_twist[d] = rospy.Publisher(utils.get_twist_topic(d), Float64, queue_size=3)
+            self._pub_pose[d] = rospy.Publisher(self._get_pose_topic(d), Float64, queue_size=3)
+            self._pub_twist[d] = rospy.Publisher(self._get_twist_topic(d), Float64, queue_size=3)
 
         self._pub_local_state = rospy.Publisher(self.LOCAL_STATE_TOPIC, Odometry, queue_size=3)
 
-        rospy.Subscriber(self.STATE_TOPIC, Odometry, self.receive_odometry)
+        rospy.Subscriber(self.STATE_TOPIC, Odometry, self._receive_odometry)
         rospy.spin()
 
-    def receive_odometry(self, odometry):
+    def _get_pose_topic(self, axis):
+        return '/controls/state/pose/' + axis
+
+    def _get_twist_topic(self, axis):
+        return '/controls/state/twist/' + axis
+
+    def _receive_odometry(self, odometry):
+        """Callback that handles re-publishing of state topics and transformation to local frame.
+
+        Args:
+            odometry: The Odom message holding the state of the robot in the global frame of reference
+        """
         if 'base_link' in self.listener.getFrameStrings():
             local_pose = utils.transform_pose(self.listener, 'odom', 'base_link', odometry.pose.pose)
-
-            utils.publish_data_dictionary(
-                self._pub_pose,
-                utils.get_axes(),
-                utils.parse_pose(local_pose)
-            )
+            utils.publish_data_dictionary(self._pub_pose, utils.parse_pose(local_pose))
 
             local_twist = utils.transform_twist(self.listener, 'odom', 'base_link', odometry.twist.twist)
-            utils.publish_data_dictionary(
-                self._pub_twist,
-                utils.get_axes(),
-                utils.parse_twist(local_twist)
-            )
+            utils.publish_data_dictionary(self._pub_twist, utils.parse_twist(local_twist))
 
             local_state = Odometry()
             local_state.header.frame_id = 'base_link'
--- onboard/catkin_ws/src/controls/scripts/thruster.py
+++ onboard/catkin_ws/src/controls/scripts/thruster.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python3
+
 import numpy as np
 from tf.transformations import quaternion_from_euler
 from controls_utils import quat_vec_mult
 
 
 class Thruster:
+    """Describes a thruster in terms of position and orientation. Calculates the
+    force and torque that the thruster applies when spun.
 
-    # Constants for scaling thruster speeds to ROS thruster controller
-    MAX_NEG_POW = -128
-    MAX_POS_POW = 127
+    Attributes:
+        flipped: Whether the thruster is reversed, meaning power has to be negated. This is related to how
+            the thrusters were wired to the ESCs and is part of the robot config
+        force_hat: The unit force vector (N) applied by the thruster in x, y, z
+        pos: The x, y, z position of the thruster (m) relative to the robot's center of mass
+        rpy: The euler angle orientation of the thruster (degrees) relative to the robot's frame of reference
+        torque: The torque vector (N m) applied by the thruster
+    """
 
     def __init__(self, pos, rpy, flipped):
         self.pos = pos
@@ -21,5 +30,4 @@ class Thruster:
         # https://en.wikipedia.org/wiki/Euler_angles
 
         self.force_hat = quat_vec_mult(q, [1, 0, 0])  # unit vector representing force of thruster in x,y,z directions
-
         self.torque = np.cross(self.pos, self.force_hat)  # vector representing torque generated from thruster
