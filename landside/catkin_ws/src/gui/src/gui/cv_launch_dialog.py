0ce9cf53f8114a4924823c5771bc322ef20ebf07
Vedarsh Shah <vedarsh.shah@gmail.com>
2022-10-19 18:57:46 -0400

    Duplicated systems plugin and launch dialog to initialize cv gui

---
+++ landside/catkin_ws/src/gui/src/gui/cv_launch_dialog.py
@@ -0,0 +1,81 @@
+import os
+import glob
+
+from python_qt_binding import loadUi
+from python_qt_binding.QtWidgets import QDialog
+from python_qt_binding.QtCore import pyqtSignal
+
+import rospy
+import resource_retriever as rr
+
+from custom_msgs.srv import StartLaunch
+
+
+class CVLaunchDialog(QDialog):
+
+    ROOT_PATH = '/root/dev/robosub-ros/onboard/catkin_ws/src'
+
+    node_launched = pyqtSignal(int, str, str, str, name='nodeLaunched')
+
+    def __init__(self):
+        super(CVLaunchDialog, self).__init__()
+
+        ui_file = rr.get_filename('package://gui/resource/CVLaunchDialog.ui', use_protocol=False)
+        loadUi(ui_file, self)
+
+        self.reset()
+
+        self.package_name_box.activated.connect(self.package_name_selected)
+        self.node_name_box.activated.connect(self.node_name_selected)
+        self.accept_button.clicked.connect(self.click_ok)
+        self.cancel_button.clicked.connect(self.reject)
+
+    def reset(self):
+        package_list = glob.glob(os.path.join(self.ROOT_PATH, '*/'))
+        package_names = [''] + [str(os.path.split(f[:-1])[1]) for f in package_list]
+        self.package_name_box.clear()
+        self.package_name_box.addItems(package_names)
+        self.package_name_box.setEnabled(True)
+        self.node_name_box.setEnabled(False)
+        self.accept_button.setEnabled(False)
+        self.cancel_button.setEnabled(True)
+
+    def get_launchables(self, package_name):
+        package_dir = os.path.join(self.ROOT_PATH, package_name)
+        launch_files = glob.glob(os.path.join(package_dir, 'launch', '*.launch'))
+        script_files = glob.glob(os.path.join(package_dir, 'scripts', '*.py'))
+        executables = [f for f in script_files if os.access(f, os.X_OK)]
+        return [''] + launch_files + executables
+
+    def package_name_selected(self, item_index):
+        self.node_name_box.clear()
+
+        if item_index == 0:
+            self.node_name_box.setEnabled(False)
+            return
+
+        selected_package = self.package_name_box.itemText(item_index)
+        launchables = self.get_launchables(selected_package)
+        launchable_names = [os.path.split(f)[1] for f in launchables]
+        self.node_name_box.addItems(launchable_names)
+        self.node_name_box.setEnabled(True)
+
+    def node_name_selected(self, item_index):
+        if item_index == 0:
+            self.accept_button.setEnabled(False)
+        else:
+            self.accept_button.setEnabled(True)
+
+    def click_ok(self):
+        package = self.package_name_box.currentText()
+        node = self.node_name_box.currentText()
+        args = self.args_input.text().split(' ')
+        start_launch = rospy.ServiceProxy('start_node', StartLaunch)
+        try:
+            resp = start_launch(package, node, args, node.endswith('.launch'))
+            self.node_launched.emit(resp.pid, package, node, self.args_input.text())
+        except rospy.ServiceException as exc:
+            rospy.logerr(f'Service did not process request: {str(exc)}')
+            self.reject()
+        else:
+            self.accept()
---
+++ landside/catkin_ws/src/gui/src/gui/cv_launch_widget.py
@@ -0,0 +1,144 @@
+from python_qt_binding import loadUi
+from python_qt_binding.QtWidgets import QWidget, QAbstractItemView, QTableWidgetItem
+from python_qt_binding.QtCore import QTimer
+import python_qt_binding.QtCore as QtCore
+
+import rospy
+import resource_retriever as rr
+import rosservice
+
+from gui.cv_launch_dialog import CVLaunchDialog
+from custom_msgs.msg import SystemUsage
+from custom_msgs.srv import StartLaunch, StopLaunch
+
+
+class CVLaunchWidget(QWidget):
+
+    LAUNCHFILES = {'motion': 'motion.launch',
+                   'state': 'state.launch',
+                   'tasks': 'tasks.launch'}
+
+    def __init__(self):
+        super(CVLaunchWidget, self).__init__()
+
+        ui_file = rr.get_filename('package://gui/resource/CVLaunchWidget.ui', use_protocol=False)
+        loadUi(ui_file, self)
+
+        self.simulation = False
+        self.simulation_check_box.setChecked(False)
+        self.simulation_check_box.stateChanged.connect(self.simulation_changed)
+
+        self.cpu_value.setDigitCount(4)
+        self.cpu_value.display(0)
+        self.ram_value.setDigitCount(4)
+        self.ram_value.display(0)
+        self.system_sub = rospy.Subscriber('/system/usage', SystemUsage, self.update_cpu_ram)
+
+        self.last_system_time = rospy.Time.now() - rospy.Duration(5)
+        self.system_sub_timer = QTimer(self)
+        self.system_sub_timer.timeout.connect(self.check_system_sub)
+        self.system_sub_timer.start(100)
+
+        self.current_launch = None
+        self.execute_buttons = {"motion": self.motion_button,
+                                "state": self.state_button,
+                                "tasks": self.tasks_button}
+        self.execute_rows = {"motion": -1,
+                             "state": -1,
+                             "tasks": -1}
+
+        self.motion_button.setText(f'Start {self.LAUNCHFILES["motion"]}')
+        self.motion_button.clicked.connect(lambda: self.execute_button_clicked('motion'))
+
+        self.state_button.setText(f'Start {self.LAUNCHFILES["state"]}')
+        self.state_button.clicked.connect(lambda: self.execute_button_clicked('state'))
+
+        self.tasks_button.setText(f'Start {self.LAUNCHFILES["tasks"]}')
+        self.tasks_button.clicked.connect(lambda: self.execute_button_clicked('tasks'))
+
+        self.table_widget.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        self.launch_dialog = CVLaunchDialog()
+        self.launch_dialog.setObjectName('CVLaunchDialog')
+        self.launch_dialog.reset()
+        self.launch_dialog.node_launched.connect(self.append_to_table)
+
+        self.launch_dialog_button.clicked.connect(self.launch_node_dialog)
+
+        self.remote_launch_timer = QTimer(self)
+        self.remote_launch_timer.timeout.connect(self.check_remote_launch)
+        self.remote_launch_timer.start(100)
+
+        rospy.loginfo('CV Launch Widget successfully initialized')
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Delete:
+            items = self.table_widget.selectedItems()
+            for key in self.execute_rows:
+                if items and self.execute_rows[key] == items[0].row():
+                    self.execute_button_clicked(key)
+                    items = None
+            if items and items[0].row() != 0:
+                self.delete_launch(items[0].row())
+        super(CVLaunchWidget, self).keyPressEvent(event)
+
+    def check_remote_launch(self):
+        enabled = '/start_node' in rosservice.get_service_list()
+        self.launch_dialog_button.setEnabled(enabled)
+        self.table_widget.setEnabled(enabled)
+        if enabled and self.current_launch is None:
+            for key in self.execute_buttons:
+                self.execute_buttons[key].setEnabled(True)
+        elif enabled and self.current_launch is not None:
+            for key in self.execute_buttons:
+                self.execute_buttons[key].setEnabled(key == self.current_launch)
+        else:
+            for key in self.execute_buttons:
+                self.execute_buttons[key].setEnabled(False)
+
+    def check_system_sub(self):
+        self.system_usage_box.setEnabled(rospy.Time.now() - self.last_system_time < rospy.Duration(2))
+
+    def simulation_changed(self, val):
+        self.simulation = self.simulation_check_box.isChecked()
+
+    def update_cpu_ram(self, system_usage):
+        self.last_system_time = rospy.Time.now()
+        self.cpu_value.display(system_usage.cpu_percent)
+        self.ram_value.display(system_usage.ram.used)
+
+    def launch_node_dialog(self):
+        self.launch_dialog.reset()
+        self.launch_dialog.open()
+
+    def launch_file(self, launchfile):
+        args = ['sim:=true'] if self.simulation else ['sim:=false']
+        start_launch = rospy.ServiceProxy('start_node', StartLaunch)
+        resp = start_launch('execute', launchfile, args, True)
+        return self.append_to_table(resp.pid, 'execute', launchfile, args[0])
+
+    def delete_launch(self, row_value):
+        stop_launch = rospy.ServiceProxy('stop_node', StopLaunch)
+        resp = stop_launch(int(self.table_widget.item(row_value, 0).text()))
+        self.table_widget.removeRow(row_value)
+        return resp.success
+
+    def append_to_table(self, pid, package, name, args):
+        self.table_widget.insertRow(self.table_widget.rowCount())
+        self.table_widget.setItem(self.table_widget.rowCount() - 1, 0, QTableWidgetItem(str(pid)))
+        self.table_widget.setItem(self.table_widget.rowCount() - 1, 1, QTableWidgetItem(package))
+        self.table_widget.setItem(self.table_widget.rowCount() - 1, 2, QTableWidgetItem(name))
+        self.table_widget.setItem(self.table_widget.rowCount() - 1, 3, QTableWidgetItem(args))
+        return self.table_widget.rowCount() - 1
+
+    def execute_button_clicked(self, button):
+        start_launch = self.execute_buttons[button].text() == f'Start {self.LAUNCHFILES[button]}'
+        new_button_text = 'Stop ' if start_launch else 'Start '
+        self.execute_buttons[button].setText(new_button_text + self.LAUNCHFILES[button])
+        if start_launch:
+            self.current_launch = button
+            self.execute_rows[button] = self.launch_file(self.LAUNCHFILES[button])
+        else:
+            self.current_launch = None
+            self.delete_launch(self.execute_rows[button])
+            self.execute_rows[button] = -1
