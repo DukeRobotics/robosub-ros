dc303123e5d68caa2cc44692b0cc5f1058b163a2
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-10-26 19:21:19 -0400

    Clean up installs

--- landside/ros2_ws/src/sub_test/setup.py
+++ landside/ros2_ws/src/sub_test/setup.py
@@ -20,7 +20,6 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = pub_test.test_publisher:main',
             'listener = sub_test.test_subscriber:main',
         ],
     },
