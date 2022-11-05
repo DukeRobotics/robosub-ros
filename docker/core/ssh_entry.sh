73657caea23cd627422af5047d4dcb447baa985d
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-11-05 15:34:51 -0400

    Revert some utilities

--- docker/core/ssh_entry.sh
+++ docker/core/ssh_entry.sh
@@ -2,3 +2,7 @@
 # shellcheck disable=SC1090,SC1091
 
 source /opt/ros/humble/setup.bash
+
+# We know this directory exists, since it was created in the Dockerfile
+# shellcheck disable=SC2164
+cd /root/dev/robosub-ros
