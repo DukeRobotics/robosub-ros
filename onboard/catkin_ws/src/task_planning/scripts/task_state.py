fae1d13370b3cd8097685e31977381a0418ba025
Brokemia <37253216+Brokemia@users.noreply.github.com>
2022-11-16 21:02:09 -0500

    Very rough conversion of move_tasks to use actions

--- onboard/catkin_ws/src/task_planning/scripts/task_state.py
+++ onboard/catkin_ws/src/task_planning/scripts/task_state.py
@@ -1,15 +1,16 @@
 import rospy
+import actionlib
 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose, Twist
-from custom_msgs.msg import CVObject
+from custom_msgs.msg import CVObject, ControlsDesiredPoseAction, ControlsDesiredTwistAction, ControlsDesiredPowerAction
 
 
 class TaskState:
     STATE_TOPIC = '/state'
-    DESIRED_POSE_TOPIC = 'controls/desired_pose'
-    DESIRED_TWIST_POWER_TOPIC = 'controls/desired_power'
-    DESIRED_TWIST_VELOCITY_TOPIC = 'controls/desired_twist'
+    DESIRED_POSE_ACTION = 'controls/desired_pose'
+    DESIRED_TWIST_POWER_ACTION = 'controls/desired_power'
+    DESIRED_TWIST_VELOCITY_ACTION = 'controls/desired_twist'
     CV_DATA_TOPICS = ["/cv/simulation/bin/left",
                       "/cv/simulation/bootleggerbuoy/left",
                       "/cv/simulation/gate/left",
@@ -22,9 +23,9 @@ class TaskState:
 
     def __init__(self):
         self.state_listener = rospy.Subscriber(self.STATE_TOPIC, Odometry, self._on_receive_state)
-        self.desired_pose_global_publisher = rospy.Publisher(self.DESIRED_POSE_TOPIC, Pose, queue_size=5)
-        self.desired_twist_power_publisher = rospy.Publisher(self.DESIRED_TWIST_POWER_TOPIC, Twist, queue_size=5)
-        self.desired_twist_velocity_publisher = rospy.Publisher(self.DESIRED_TWIST_VELOCITY_TOPIC, Twist, queue_size=5)
+        self.desired_pose_global_client = actionlib.SimpleActionClient(self.DESIRED_POSE_ACTION, ControlsDesiredPoseAction)
+        self.desired_twist_velocity_client = actionlib.SimpleActionClient(self.DESIRED_TWIST_VELOCITY_ACTION, ControlsDesiredTwistAction)
+        self.desired_twist_power_client = actionlib.SimpleActionClient(self.DESIRED_TWIST_POWER_ACTION, ControlsDesiredPowerAction)
         self.state = None
 
         self.cv_data = {}
@@ -32,6 +33,10 @@ class TaskState:
             name = topic.replace("/left", "").split("/")[-1]
             self.cv_data[name] = None
             rospy.Subscriber(topic, CVObject, self._on_receive_cv_data, name)
+
+        self.desired_pose_global_client.wait_for_server()
+        self.desired_twist_velocity_client.wait_for_server()
+        self.desired_twist_power_client.wait_for_server()
 
     def _on_receive_state(self, state):
         """Receive the state, update initial_state if it is empty
