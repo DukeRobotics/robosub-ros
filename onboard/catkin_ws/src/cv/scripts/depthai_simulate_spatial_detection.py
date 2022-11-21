7e3a773e047f6149529a7ba96d9645c1de0066a3
Tyler Feldman <tjf40@duke.edu>
2022-11-21 02:10:12 -0500

    Add functions to visualize the detections

--- onboard/catkin_ws/src/cv/scripts/depthai_simulate_spatial_detection.py
+++ onboard/catkin_ws/src/cv/scripts/depthai_simulate_spatial_detection.py
@@ -121,6 +121,23 @@ class DepthAISimulateSpatialDetection:
         }
 
 
+# nn data, being the bounding box locations, are in <0..1> range - they need to be normalized with frame width/height
+def frame_norm(frame, bbox):
+    normVals = np.full(len(bbox), frame.shape[0])
+    normVals[::2] = frame.shape[1]
+    return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+
+
+def display_frame(name, frame):
+    color = (255, 0, 0)
+    for detection in detections:
+        bbox = frame_norm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+        cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+        cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 2)
+    # Show the frame
+    cv2.imshow(name, frame)
+
+
 if __name__ == '__main__':
     d = DepthAISimulateSpatialDetection()
     out = d.detect_single_image(d.image)
