4e6eb4743f01bc266a62057835e8497e701bb383
Maxwell Lin <maxwell.lin@duke.edu>
2023-01-14 10:48:59 -0500

    Finish initial gui template

---
+++ landside/catkin_ws/src/gui/src/gui/camera_status_plugin.py
@@ -0,0 +1,26 @@
+from rqt_gui_py.plugin import Plugin
+
+from gui.camera_status_widget import CameraStatusWidget
+
+
+class CameraStatusPlugin(Plugin):
+
+    def __init__(self, context):
+        super(CameraStatusPlugin, self).__init__(context)
+        self.widget = CameraStatusWidget()
+        self.widget.setObjectName('CameraStatusPlugin')
+        self.widget.setWindowTitle('Camera Status Plugin')
+        if context.serial_number() > 1:
+            self.widget.setWindowTitle(self.widget.windowTitle() +
+                                       (' (%d)' % context.serial_number()))
+        context.add_widget(self.widget)
+
+    def shutdown_plugin(self):
+        self.widget.closeWidget()
+
+    def save_settings(self, plugin_settings, instance_settings):
+        pass
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        self.widget.default_pkg = instance_settings.value("default_pkg")
+        pass
--- landside/catkin_ws/src/gui/src/gui/camera_status_widget.py
+++ landside/catkin_ws/src/gui/src/gui/camera_status_widget.py
@@ -7,10 +7,36 @@ import resource_retriever as rr
 
 class CameraStatusWidget(QWidget):
 
-    def __init__(self, context):
+    def __init__(self):
         super(CameraStatusWidget, self).__init__()
 
         ui_file = rr.get_filename('package://gui/resource/CameraStatusWidget.ui', use_protocol=False)
         loadUi(ui_file, self)
 
         rospy.loginfo('Camera Status Widget successfully initialized')
+
+        self.background_colors = {
+            "green": "background-color: #8aff92",
+            "red": "background-color: #ff7878"
+        }
+
+        self.init_gui()
+
+    def init_gui(self):
+        mono_text_area = self.mono_camera_text_area
+        dai_text_area = self.dai_camera_text_area
+
+        mono_text_area.setText('<i style="color: red;">DISCONNECTED</i>')
+        dai_text_area.setText('<i style="color: red;">DISCONNECTED</i>')
+        
+        return
+
+    def populate_mono_camera_text_area(self):
+        # text_area = self.mono_camera_text_area
+
+        pass
+
+    def populate_dai_camera_text_area(self):
+        # text_area = self.mono_camera_text_area
+
+        pass
