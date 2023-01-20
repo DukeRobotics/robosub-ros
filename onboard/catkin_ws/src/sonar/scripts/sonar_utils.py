f3fd9ebf29e5f4d7e2444c4cf47efe0810884a83
Will-Denton <wildenton@outlook.com>
2023-01-20 13:07:14 -0500

    testing

--- onboard/catkin_ws/src/sonar/scripts/sonar_utils.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar_utils.py
@@ -1,6 +1,5 @@
 from geometry_msgs.msg import PoseStamped
 
-
 def transform_pose(listener, pose):
     """ Transform pose from base reference frame to target reference frame frame
 
@@ -17,4 +16,4 @@ def transform_pose(listener, pose):
     pose_stamped.pose = pose
     pose_stamped.header.frame_id = "sonar_link"
 
-    return listener.transformPose("odom", pose_stamped).pose
+    return listener.transformPose("corner_link", pose_stamped).pose
