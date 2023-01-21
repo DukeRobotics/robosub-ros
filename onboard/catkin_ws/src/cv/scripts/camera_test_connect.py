e3a664dbfba847e5a22ac49d0e65e1d34b7f6f4f
Vedarsh Shah <vedarsh.shah@gmail.com>
2023-01-21 11:58:22 -0500

    Develop ping_ip and camera_test_connect nodes to get camera status

---
+++ onboard/catkin_ws/src/cv/scripts/camera_test_connect.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import rospy
+import cv2
+import time
+import depthai as dai
+
+from custom_msgs.srv import ConnectDepthAICamera, ConnectUSBCamera
+import depthai_camera_connect
+
+
+class CameraStatus:
+    def __init__(self):
+        rospy.init_node('camera_status')
+
+    def run(self):
+        rospy.Service("connect_depthai_camera", ConnectDepthAICamera, self.connect_depthai)
+        rospy.Service("connect_usb_camera", ConnectUSBCamera, self.connect_usb)
+        rospy.spin()
+
+    def connect_depthai(self, _):
+        try:
+            depthai_camera_connect.connect(dai.Pipeline())
+            return {'success': True}
+
+        except Exception:
+            return {'success': False}
+
+    def connect_usb(self, req):
+        try:
+            # Connect to camera at specified channel
+            cap = cv2.VideoCapture(req.channel)
+
+            # Attempt to successfully read a frame for 5 seconds
+            time_end = time.time() + 5
+            while time.time() < time_end:
+                success, _ = cap.read()
+                if success:
+                    return {'success': True}
+
+            return {'success': False}
+
+        except Exception:
+            return {'success': False}
+
+
+if __name__ == '__main__':
+    CameraStatus().run()
---
+++ onboard/catkin_ws/src/cv/scripts/ping_ip.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import rospy
+import subprocess
+
+from diagnostic_msgs.msg import DiagnosticStatus, DiagnosticArray
+
+
+class PingIP:
+    def __init__(self):
+        rospy.init_node('ping_ip')
+
+        self.publisher = rospy.Publisher("ping_ip", DiagnosticArray, queue_size=10)
+        self.hostname = rospy.get_param("~hostname")
+        self.rate = rospy.get_param("~rate")
+
+    def ping(self):
+        loop_rate = rospy.Rate(self.rate)
+
+        while not rospy.is_shutdown():
+            proc = subprocess.Popen(["ping", "-c", "1", self.hostname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            stdout, _ = proc.communicate()
+            response = proc.returncode
+
+            msg_status = DiagnosticStatus()
+            msg_status.level = 0 if response == 0 else 2
+            msg_status.name = "ping"
+            msg_status.message = stdout.decode('ASCII')
+            msg_status.hardware_id = self.hostname
+
+            msg_array = DiagnosticArray()
+            msg_array.header.stamp = rospy.Time.now()
+            msg_array.status = [msg_status]
+
+            self.publisher.publish(msg_array)
+
+            loop_rate.sleep()
+
+
+if __name__ == '__main__':
+    PingIP().ping()
