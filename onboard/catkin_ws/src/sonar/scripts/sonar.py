e6b00e399fb68f3f2f23df90efb417680284cc34
Tyler Feldman <tylerfeldman321@gmail.com>
2023-01-21 15:53:21 -0500

    Change local test to false and comment init node in action server

--- onboard/catkin_ws/src/sonar/scripts/sonar.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar.py
@@ -9,11 +9,12 @@ import os
 from tf import TransformListener
 from time import sleep
 
+
 class Sonar:
     """Class to interface with the Sonar device.
     """
 
-    IF_LOCAL_TEST = False #only for local testing with the sonar script
+    IF_LOCAL_TEST = True  #only for local testing with the sonar script
 
     SERIAL_PORT_NAME = "/dev/ttyUSB2"  # PORT of the salea is ttyUSB2 for testing
     BAUD_RATE = 2000000  # hz
