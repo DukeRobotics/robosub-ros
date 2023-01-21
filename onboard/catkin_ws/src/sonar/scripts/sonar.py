876f2f5b7c15ccd4423b166d7cbd448ba950eb35
Will-Denton <wildenton@outlook.com>
2023-01-21 16:19:28 -0500

    testig

--- onboard/catkin_ws/src/sonar/scripts/sonar.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar.py
@@ -143,7 +143,7 @@ class Sonar:
         #mean_index = sum(max_byte_array, key=lambda tup: tup[0]) / len(max_byte_array) TODO fix mean angle code
         center_angle = (start_angle + end_angle) / 2
         print(f"{center_angle} {max_byte_array[0]}")
-        pose = self.to_robot_position(center_angle, max_byte_array[0]) #temp fix
+        pose = self.to_robot_position(center_angle, max_byte_array[0][0]) #temp fix
         return pose.position.x, pose.position.y
 
     def get_max_byte_in_sweep(self, start_angle, end_angle):
