c1f175508cde404f3bc724d8bc1107ffd9bb0c3f
Will-Denton <wildenton@outlook.com>
2023-01-21 14:53:18 -0500

    testing connection

--- onboard/catkin_ws/src/sonar/scripts/sonar_utils.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar_utils.py
@@ -14,6 +14,6 @@ def transform_pose(listener, pose):
     """
     pose_stamped = PoseStamped()
     pose_stamped.pose = pose
-    pose_stamped.header.frame_id = "sonar_link"
+    pose_stamped.header.frame_id = "base_link"
 
-    return listener.transformPose("corner_link", pose_stamped).pose
+    return listener.transformPose("odom", pose_stamped).pose
