aedae534d6bac3ddaebecdaf252b601c94ef242a
Will-Denton <wildenton@outlook.com>
2023-02-11 14:00:32 -0500

    Fixed message

--- onboard/catkin_ws/src/sonar/scripts/sonar_publisher.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar_publisher.py
@@ -18,14 +18,14 @@ class SonarPublisher:
         self._pub_request = rospy.Publisher(self.SONAR_RESPONSE_TOPIC, sweepGoal, queue_size=10)
 
     def on_request(self, request):
-        self.sonar.set_new_range(request.depth)
-        center_gradians = degrees_to_centered_gradians(request.center_degrees)
-        breadth_gradians = degrees_to_centered_gradians(request.breadth_degrees)
+        self.sonar.set_new_range(request.distance_of_scan)
+        # center_gradians = degrees_to_centered_gradians(request.center_degrees)
+        # breadth_gradians = degrees_to_centered_gradians(request.breadth_degrees)
 
-        left = max(center_gradians - breadth_gradians, 0)
-        right = min(center_gradians + breadth_gradians, 400)
+        # left = max(center_gradians - breadth_gradians, 0)
+        # right = min(center_gradians + breadth_gradians, 400)
 
-        sonar_xy_result = self.sonar.get_xy_of_object_in_sweep(left,right)
+        sonar_xy_result = self.sonar.get_xy_of_object_in_sweep(request.start_angle,request.end_angle)
 
         response = sweepResult()
         response.x_pos = sonar_xy_result[0]
