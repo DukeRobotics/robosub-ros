4d689c61d5c8cc57427286ce5bc61dadff8a53c5
Will-Denton <wildenton@outlook.com>
2023-02-11 16:02:42 -0500

    Added node to local test

--- onboard/catkin_ws/src/sonar/scripts/sonar_pool_test.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar_pool_test.py
@@ -1,6 +1,8 @@
 from sonar import Sonar
 from sonar_image_processing import *
+import rospy
 
 if __name__ == "__main__":
+    rospy.init_node("local_pool_test")
     sonar = Sonar(5) #need to test different # of samples - would also have to make 
     build_sonar_image(sonar, False, jpeg_save_path="/sampleData/testImgage", start_angle=197, end_angle=203)
