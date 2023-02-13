5b4257e21314ad1315acc4858cedaffda36aa50c
Vedarsh Shah <vedarsh.shah@gmail.com>
2023-02-13 17:02:00 -0500

    Added validation to rate argument and added timeout to ping command

--- onboard/catkin_ws/src/cv/scripts/ping_ip.py
+++ onboard/catkin_ws/src/cv/scripts/ping_ip.py
@@ -13,12 +13,18 @@ class PingIP:
         self.publisher = rospy.Publisher("ping_ip", DiagnosticArray, queue_size=10)
         self.hostname = rospy.get_param("~hostname")
         self.rate = rospy.get_param("~rate")
+        if not (isinstance(self.rate, int) and self.rate > 0):
+            raise ValueError("Rate must be a positive integer")
 
     def ping(self):
         loop_rate = rospy.Rate(self.rate)
 
         while not rospy.is_shutdown():
-            proc = subprocess.Popen(["ping", "-c", "1", self.hostname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            proc = subprocess.Popen(
+                ["ping", "-c", "1", "-W", str(self.rate), self.hostname],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
 
             stdout, _ = proc.communicate()
             response = proc.returncode
