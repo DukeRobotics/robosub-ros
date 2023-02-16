e06900a85c95e3b764d797281761322eddd927f2
vedarshshah <33162616+vedarshshah@users.noreply.github.com>
2023-02-15 21:49:11 -0500

    CV RQT Fixes (#393)
    * Added button to collapse table in launch widget
    
    * Separated System Usage Widget into its own plugin
    
    * Removed collapse button for table and added collapse button for launch dialog form
    
    * Added ability to show checkbox instead of line edit for boolean roslaunch arguments in Launch Dialog
    
    * Added doc JSON objects with type and help string
    
    * Updated System Usage Widget to use less horizontal space
    
    * Changed camera not found message level to error instead of info
    
    * Made test_images.py executable
    
    * Added doc attributes with JSON to launch file args
    
    * Added capability to validate argument input with regex, int, double, and show combo box and check box when appropriate
    
    * Added enviornment variables to dockerfile to fix libgl and QStandardPaths errors
    
    * Insert separator between launch and python files in Launch Dialog
    
    * Added checking for empty arguments in Launch Dialog
    
    * Fixed minimum index to insert separator in Launch Dialog
    
    * Updated gui readme to include system usage plugin
    
    * Switched from QRegExp to latest QRegularExpression
    
    * Added more type checking and try/except for JSON parsing failure in Launch Dialog
    
    * Added documentation for argument validation in Launch Dialog
    
    * Fixed CI issues with depthai and usb camera
    
    * Added accepted default checking to launch dialog
    
    * Modified launch dialog documentation to be centered on a table
    
    * Simplifying node_launched function in launch dialog
    
    * Replaced depthai model individual launch files with single launch file with arguments
    
    * Separated functionality into functions in launch dialog
    
    * Added doc JSON strings to all CV launch files
    
    * Removed unused launch file
    
    * Selecting a package name in launch dialog now clears the arguments
    
    * Added default to options list
    
    * Added system usage plugin to cv gui perspective
    
    * Added system usage plugin to task planning gui
    
    * Fixed CI issue with depthai service model
    
    * Modified test launch blocklist to include depthai_service_model and remove old models
    
    * Replaced python time loop with raspy.sleep in depthai camera connect
    
    Co-authored-by: Brokemia <37253216+Brokemia@users.noreply.github.com>
    
    * Added closing / to example arg tag in gui README
    
    * Added parentheses to clarify order of ops
    
    * Simplified boolean portion of interpret_type in launch_dialog
    
    Co-authored-by: Brokemia <37253216+Brokemia@users.noreply.github.com>
    
    * Removed spin from usb_camera and added it to block list
    
    * Removed unnecessary try/excepts and get operations in launch dialog
    
    * Replaced type with isinstance in launch dialog
    
    * Fixed linting issue
    
    * Refactored for loop to get index of first script file in launch dialog
    
    ---------
    
    Co-authored-by: Brokemia <37253216+Brokemia@users.noreply.github.com>

--- landside/catkin_ws/src/gui/src/gui/launch_dialog.py
+++ landside/catkin_ws/src/gui/src/gui/launch_dialog.py
@@ -1,9 +1,9 @@
 import os
 import glob
 
-from python_qt_binding import loadUi, QtWidgets
+from python_qt_binding import loadUi, QtWidgets, QtGui
 from python_qt_binding.QtWidgets import QDialog, QMessageBox
-from python_qt_binding.QtCore import pyqtSignal, pyqtProperty
+from python_qt_binding.QtCore import Qt, QRegularExpression, pyqtSignal, pyqtProperty
 
 import rospy
 import resource_retriever as rr
@@ -11,6 +11,7 @@ import resource_retriever as rr
 from custom_msgs.srv import StartLaunch
 
 import xml.etree.ElementTree as ET
+import json
 
 
 class LaunchDialog(QDialog):
@@ -33,6 +34,8 @@ class LaunchDialog(QDialog):
         self.package_name_box.activated.connect(self.package_name_selected)
         self.node_name_box.activated.connect(self.node_name_selected)
         self.accept_button.clicked.connect(self.click_ok)
+
+        self.collapse_button.clicked.connect(self.collapse)
 
         self.arg_form_rows = []
 
@@ -59,7 +62,7 @@ class LaunchDialog(QDialog):
         self.package_name_box.setEnabled(True)
         self.node_name_box.clear()
         self.node_name_box.setEnabled(False)
-        self.args_input.clear()
+        self.other_args_input.clear()
         self.accept_button.setEnabled(False)
 
     def get_launchables(self, package_name):
@@ -73,6 +76,8 @@ class LaunchDialog(QDialog):
             return []
 
     def package_name_selected(self, item_index):
+        self.clear_arg_form_rows()
+
         if item_index == 0:
             self.node_name_box.setEnabled(False)
             return
@@ -87,11 +92,24 @@ class LaunchDialog(QDialog):
         launchable_names = [os.path.split(f)[1] for f in launchables]
         self.node_name_box.addItems(launchable_names)
 
-    def node_name_selected(self, item_index):
+        index_of_first_script_file = None
+        for index, launchable_name in enumerate(launchable_names):
+            if launchable_name.endswith('.py'):
+                index_of_first_script_file = index
+                break
+
+        if index_of_first_script_file and (index_of_first_script_file > 1) and \
+           (index_of_first_script_file < len(launchable_names)):
+            self.node_name_box.insertSeparator(index_of_first_script_file)
+
+    def clear_arg_form_rows(self):
         for row in self.arg_form_rows:
             self.form_layout.removeRow(row['label'])
 
         self.arg_form_rows = []
+
+    def node_name_selected(self, item_index):
+        self.clear_arg_form_rows()
 
         if item_index == 0:
             self.accept_button.setEnabled(False)
@@ -102,49 +120,195 @@ class LaunchDialog(QDialog):
         selected_node = self.node_name_box.currentText()
         selected_node_file_type = selected_node.split(".")[1]
 
-        if selected_node_file_type == "launch":
-            package_dir = os.path.join(self.ROOT_PATH, self.package_name_box.currentText())
-            launch_file_path = os.path.join(package_dir, 'launch/' + self.node_name_box.currentText())
+        if selected_node_file_type != "launch":
+            return
 
-            tree = ET.parse(launch_file_path)
+        # Selected node file is a launch file
+        self.get_args_from_launch_file()
 
-            def traverse_tree(root):
-                for child in root:
-                    if child.tag == 'arg' and child.get('value') is None:
-                        self.arg_form_rows.append(child.attrib)
-                    traverse_tree(child)
+        for row, arg in enumerate(self.arg_form_rows):
+            arg['allow_empty'] = True
 
-            root = tree.getroot()
-            traverse_tree(root)
+            default_value = arg.get('default', '')
 
-            for row in range(len(self.arg_form_rows)):
-                arg = self.arg_form_rows[row]
-                if arg.get('default') is None:
-                    default_value = ''
+            label = QtWidgets.QLabel(arg['name'])
+
+            input = QtWidgets.QLineEdit()
+            input.setText(default_value)
+
+            toolTip = ""
+
+            if "doc" in arg:
+                doc_dict = {}
+                try:
+                    doc_dict = json.loads(arg["doc"])
+                except Exception:
+                    rospy.logwarn(f"Could not parse JSON doc string for argument `{arg['name']}` in "
+                                  f"`{selected_node}`. Defaulting to unrestricted string input.")
+
+                if "options" in doc_dict:
+                    input = self.interpret_options(selected_node, arg, doc_dict, input)
+
+                elif "regex" in doc_dict:
+                    input, toolTip = self.interpret_regex(selected_node, arg, doc_dict, input, toolTip)
+
+                elif "type" in doc_dict:
+                    input, toolTip = self.interpret_type(selected_node, arg, doc_dict, input, toolTip)
+
+                if "help" in doc_dict:
+                    if isinstance(doc_dict["help"], str) and doc_dict["help"].strip():
+                        if toolTip:
+                            toolTip += " | Help: "
+                        toolTip += doc_dict["help"]
+
+                if "allowEmpty" in doc_dict:
+                    if isinstance(doc_dict["allowEmpty"], bool):
+                        arg['allow_empty'] = doc_dict["allowEmpty"]
+                    else:
+                        rospy.logwarn(f"The property allowEmpty for argument `{arg['name']}` in `{selected_node}` "
+                                      f"is not a valid boolean. Defaulting to allow empty input.")
+
+            if toolTip:
+                label.setText(label.text() + " (?)")
+                label.setToolTip(toolTip)
+
+            # row inserted at position row+2, after the Package and Node Name rows
+            self.form_layout.insertRow(row + 2, label, input)
+
+            arg['label'] = label
+            arg['input'] = input
+
+    def get_args_from_launch_file(self):
+        package_dir = os.path.join(self.ROOT_PATH, self.package_name_box.currentText())
+        launch_file_path = os.path.join(package_dir, 'launch/' + self.node_name_box.currentText())
+
+        tree = ET.parse(launch_file_path)
+
+        def traverse_tree(root):
+            for child in root:
+                if child.tag == 'arg' and "value" not in child:
+                    self.arg_form_rows.append(child.attrib)
+                traverse_tree(child)
+
+        root = tree.getroot()
+        traverse_tree(root)
+
+    def interpret_options(self, selected_node, arg, doc_dict, input):
+        if (isinstance(doc_dict["options"], list) and doc_dict["options"] and
+                all(isinstance(e, str) for e in doc_dict["options"])):
+
+            input = QtWidgets.QComboBox()
+            input.addItems(doc_dict["options"])
+
+            if "default" in arg:
+                if arg["default"] in doc_dict["options"]:
+                    input.setCurrentText(arg["default"])
                 else:
-                    default_value = arg['default']
+                    input.insertItem(0, arg["default"])
+                    input.setCurrentIndex(0)
+                    rospy.loginfo(f"Default value for argument `{arg['name']}` in `{selected_node}` "
+                                  f"is not in options list. It has been added as the first option.")
 
-                label = QtWidgets.QLabel(arg['name'])
-                input = QtWidgets.QLineEdit()
-                input.setText(default_value)
+        else:
+            rospy.logwarn(f"Options list for argument `{arg['name']}` in `{selected_node}` is not a "
+                          f"list of strings. Defaulting to unrestricted string input.")
 
-                # row inserted at position row+2, after the Package and Node Name rows
-                self.form_layout.insertRow(row + 2, label, input)
+        return input
 
-                arg['label'] = label
-                arg['lineEdit'] = input
+    def interpret_regex(self, selected_node, arg, doc_dict, input, toolTip):
+        regex = QRegularExpression(doc_dict["regex"])
+        if regex.isValid():
+            regex_validator = QtGui.QRegularExpressionValidator(regex)
+            if "default" in arg:
+                if regex.match(arg["default"]).hasMatch() or arg["default"] == "":
+                    input.setValidator(regex_validator)
+                    toolTip += f"Regex: {doc_dict['regex']}"
+                else:
+                    rospy.logwarn(f"Default value for argument `{arg['name']}` in `{selected_node}` "
+                                  f"does not match regex. Defaulting to unrestricted string input.")
+            else:
+                input.setValidator(regex_validator)
+                toolTip += f"Regex: {doc_dict['regex']}"
+        else:
+            rospy.logwarn(f"Regex for argument `{arg['name']}` in `{selected_node}` is not valid. "
+                          f"Defaulting to unrestricted string input.")
+
+        return (input, toolTip)
+
+    def interpret_type(self, selected_node, arg, doc_dict, input, toolTip):
+        add_tooltip = True
+        if doc_dict["type"] == "bool":
+            if "default" not in arg or arg["default"].lower() == "false":
+                input = QtWidgets.QCheckBox()
+                input.setChecked(False)
+            elif arg["default"].lower() == "true":
+                input = QtWidgets.QCheckBox()
+                input.setChecked(True)
+            else:
+                rospy.logwarn(f"Default value for argument `{arg['name']}` in `{selected_node}` is "
+                              f"not a valid boolean. Defaulting to unrestricted string input.")
+                add_tooltip = False
+
+        elif doc_dict["type"] == "int":
+            int_validator = QtGui.QIntValidator()
+            if "default" in arg:
+                if (arg["default"] == "" or
+                        int_validator.validate(arg["default"], 0)[0] == QtGui.QValidator.Acceptable):
+                    input.setValidator(int_validator)
+                else:
+                    rospy.logwarn(f"Default value for argument `{arg['name']}` in `{selected_node}` is "
+                                  f"not a valid integer. Defaulting to unrestricted string input.")
+                    add_tooltip = False
+            else:
+                input.setValidator(int_validator)
+
+        elif doc_dict["type"] == "double":
+            double_validator = QtGui.QDoubleValidator()
+            if "default" in arg:
+                if (arg["default"] == "" or
+                        double_validator.validate(arg["default"], 0)[0] == QtGui.QValidator.Acceptable):
+                    input.setValidator(double_validator)
+                else:
+                    rospy.logwarn(f"Default value for argument `{arg['name']}` in `{selected_node}` is "
+                                  f"not a valid double. Defaulting to unrestricted string input.")
+                    add_tooltip = False
+            else:
+                input.setValidator(double_validator)
+
+        elif doc_dict["type"] == "str":
+            pass
+        else:
+            add_tooltip = False
+            rospy.logwarn(f"Type `{doc_dict['type']}` for argument `{arg['name']}` in `{selected_node}`"
+                          f" is not valid. Type must be `bool`, `int`, `double`, or `str`. Defaulting "
+                          f"to unrestricted string input.")
+
+        if add_tooltip:
+            toolTip += f"Type: {doc_dict['type']}"
+
+        return (input, toolTip)
 
     def click_ok(self):
         package = self.package_name_box.currentText()
         node = self.node_name_box.currentText()
-        args = self.args_input.text().split(' ') if self.args_input.text() != "" else []
+        args = []
 
         for row in self.arg_form_rows:
-            if row['lineEdit'].text() == "" and row.get('default') is None:
-                self.missing_argument_dialog(row['name'])
+            text = ""
+
+            if type(row['input']) is QtWidgets.QComboBox:
+                text = row['input'].currentText().strip()
+            elif type(row['input']) is QtWidgets.QLineEdit:
+                text = row['input'].text().strip()
+            elif type(row['input']) is QtWidgets.QCheckBox:
+                text = str(row['input'].isChecked())
+
+            if not text and not self.handle_empty_arg_value(row):
                 return
-            arg = row['name'] + ":=" + row['lineEdit'].text()
-            args.append(arg)
+
+            args.append(row['name'] + ":=" + text)
+
+        args.extend(self.other_args_input.text().split(' ') if self.other_args_input.text() != "" else [])
 
         start_launch = rospy.ServiceProxy('start_node', StartLaunch)
         try:
@@ -153,18 +317,50 @@ class LaunchDialog(QDialog):
         except rospy.ServiceException as exc:
             rospy.logerr(f'Service did not process request: {str(exc)}')
 
-    def missing_argument_dialog(self, missing_arg):
+    def handle_empty_arg_value(self, row):
+        if not row['allow_empty'] or "default" not in row:
+            self.argument_required_dialog(row['name'])
+            return False
+        elif row.get('default') != "":
+            return self.argument_default_dialog(row['name'], row['default'])
+        return True
+
+    def argument_default_dialog(self, default_arg, default_value):
+        msg = QMessageBox()
+
+        msg.setIcon(QMessageBox.Information)
+
+        msg.setWindowTitle("Warning")
+        msg.setText("Missing argument")
+        msg.setInformativeText(f"The value of {default_arg} will be the default value of `{default_value}`."
+                               f"Is that ok?")
+
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+
+        return msg.exec_() == QMessageBox.Ok
+
+    def argument_required_dialog(self, required_arg):
         msg = QMessageBox()
 
         msg.setIcon(QMessageBox.Warning)
 
         msg.setWindowTitle("Error")
-        msg.setText("Missing argument")
-        msg.setInformativeText("You must specify a value for " + missing_arg)
+        msg.setText("Required argument")
+        msg.setInformativeText("You must specify a value for " + required_arg)
 
         msg.setStandardButtons(QMessageBox.Close)
 
         msg.exec_()
+
+    def collapse(self):
+        if self.form.isHidden():
+            self.form.show()
+            self.accept_button.show()
+            self.collapse_button.setArrowType(Qt.UpArrow)
+        else:
+            self.form.hide()
+            self.accept_button.hide()
+            self.collapse_button.setArrowType(Qt.DownArrow)
 
     def reject(self):
         pass
