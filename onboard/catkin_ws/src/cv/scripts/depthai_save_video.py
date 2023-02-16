926dc3d0976b452e7e6eb6c9f91d69f089fbc514
Vedarsh Shah <vedarsh.shah@gmail.com>
2023-02-15 21:48:05 -0500

    Fixed video file paths and added comment describing how to convert encoded files to videos in depthai save video

--- onboard/catkin_ws/src/cv/scripts/depthai_save_video.py
+++ onboard/catkin_ws/src/cv/scripts/depthai_save_video.py
@@ -18,18 +18,18 @@ class DepthAIVideoSaver:
         self.save_rgb_video = rospy.get_param('~rgb_video')
         self.save_left = rospy.get_param('~left')
         self.save_right = rospy.get_param('~right')
-        self.rgb_video_file_name = rospy.get_param('~rgb_video_file_name')
-        self.left_file_name = rospy.get_param('~left_file_name')
-        self.right_file_name = rospy.get_param('~right_file_name')
+        self.rgb_video_file_name = os.path.join(os.path.dirname(__file__), rospy.get_param('~rgb_video_file_name'))
+        self.left_file_name = os.path.join(os.path.dirname(__file__), rospy.get_param('~left_file_name'))
+        self.right_file_name = os.path.join(os.path.dirname(__file__), rospy.get_param('~right_file_name'))
 
         if self.save_rgb_video and not self.check_file_writable(self.rgb_video_file_name):
-            raise ValueError(f'Cannot write to {self.rgb_video_file}')
+            raise ValueError(f'Cannot write to {self.rgb_video_file_name}')
 
         if self.save_left and not self.check_file_writable(self.left_file_name):
-            raise ValueError(f'Cannot write to {self.left_file}')
+            raise ValueError(f'Cannot write to {self.left_file_name}')
 
         if self.save_right and not self.check_file_writable(self.right_file_name):
-            raise ValueError(f'Cannot write to {self.right_file}')
+            raise ValueError(f'Cannot write to {self.right_file_name}')
 
         self.pipeline = dai.Pipeline()
         self.build_pipeline()
@@ -38,7 +38,6 @@ class DepthAIVideoSaver:
         """
         Check if a file can be created or overwritten.
         """
-
         if os.path.exists(filename):
             # path exists
             if os.path.isfile(filename):
@@ -109,46 +108,53 @@ class DepthAIVideoSaver:
         """
         Run the DepthAI pipeline, and save the video feeds to the specified files.
         """
-        rospy.spin()
-        # with depthai_camera_connect.connect(self.pipeline) as device:
-        #     if self.save_rgb_video:
-        #         veRgbOut = device.getOutputQueue(name='veRgbOut', maxSize=30, blocking=True)
-        #         rgb_video_file = open(self.rgb_video_file_name + ".h265", 'wb')
+        with depthai_camera_connect.connect(self.pipeline) as device:
+            if self.save_rgb_video:
+                veRgbOut = device.getOutputQueue(name='veRgbOut', maxSize=30, blocking=True)
+                rgb_video_file = open(self.rgb_video_file_name + ".h265", 'wb')
 
-        #     if self.save_left:
-        #         veLeftOut = device.getOutputQueue(name='veLeftOut', maxSize=30, blocking=True)
-        #         left_file = open(self.left_file_name + ".h264", 'wb')
+            if self.save_left:
+                veLeftOut = device.getOutputQueue(name='veLeftOut', maxSize=30, blocking=True)
+                left_file = open(self.left_file_name + ".h264", 'wb')
 
-        #     if self.save_right:
-        #         veRightOut = device.getOutputQueue(name='veRightOut', maxSize=30, blocking=True)
-        #         right_file = open(self.right_file_name + ".h264", 'wb')
+            if self.save_right:
+                veRightOut = device.getOutputQueue(name='veRightOut', maxSize=30, blocking=True)
+                right_file = open(self.right_file_name + ".h264", 'wb')
 
-        #     while not rospy.is_shutdown():
-        #         while self.save_rgb_video and veRgbOut.has() and not rospy.is_shutdown():
-        #             veRgbOut.get().getData().tofile(rgb_video_file)
+            while not rospy.is_shutdown():
+                while self.save_rgb_video and veRgbOut.has() and not rospy.is_shutdown():
+                    veRgbOut.get().getData().tofile(rgb_video_file)
 
-        #         while self.save_left and veLeftOut.has() and not rospy.is_shutdown():
-        #             veLeftOut.get().getData().tofile(left_file)
+                while self.save_left and veLeftOut.has() and not rospy.is_shutdown():
+                    veLeftOut.get().getData().tofile(left_file)
 
-        #         while self.save_right and veRightOut.has() and not rospy.is_shutdown():
-        #             veRightOut.get().getData().tofile(right_file)
+                while self.save_right and veRightOut.has() and not rospy.is_shutdown():
+                    veRightOut.get().getData().tofile(right_file)
 
-        #     if self.save_rgb_video:
-        #         rgb_video_file.close()
+            if self.save_rgb_video:
+                rgb_video_file.close()
 
-        #     if self.save_left:
-        #         left_file.close()
+            if self.save_left:
+                left_file.close()
 
-        #     if self.save_right:
-        #         right_file.close()
+            if self.save_right:
+                right_file.close()
+
+            # To convert the H.264/5 files to a playable videos, run the following command for each file:
+            # ffmpeg -framerate 25 -i <video_file_name>.h264/5 -c copy <video_file_name>.mp4
+            #
+            # QuickTime player will not be able to view the RGB video, but VLC will.
+            # The left and right videos can be viewed in QuickTime player.
+            #
+            # To make the RGB video viewable in QuickTime player, run the following command:
+            # fmpeg -framerate 25 -i rgb.h265 -vcodec libx264 -pix_fmt yuv420p rgb.mp4
+            #
+            # This command is quite slow as it converts the video encoding to be compatible with QuickTime player.
+            # It is generally preferrable to use VLC to view the RGB video generated by the first command.
 
 
 if __name__ == '__main__':
     try:
         DepthAIVideoSaver().run()
     except rospy.ROSInterruptException:
-        rospy.loginfo("To view the encoded data, convert the stream file (.h264/.h265) into a video file (.mp4), using commands below:")
-        cmd = "ffmpeg -framerate 25 -i {} -c copy {}"
-        rospy.loginfo(cmd.format("mono1.h264", "mono1.mp4"))
-        rospy.loginfo(cmd.format("mono2.h264", "mono2.mp4"))
-        rospy.loginfo(cmd.format("color.h265", "color.mp4"))
+        pass
