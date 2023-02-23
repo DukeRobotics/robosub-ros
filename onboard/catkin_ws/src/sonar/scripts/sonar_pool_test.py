d9611123756b1ed6d5c514a979dabe5fb7f8b9c0
Will-Denton <wildenton@outlook.com>
2023-02-22 20:35:07 -0500

    added cli

--- onboard/catkin_ws/src/sonar/scripts/sonar_pool_test.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar_pool_test.py
@@ -1,6 +1,7 @@
 from sonar import Sonar
 from sonar_image_processing import *
 import rospy
+import sys
 
 # COMMAND TO SETUP SERVER:
 # python -m http.server 
@@ -9,4 +10,4 @@ import rospy
 if __name__ == "__main__":
     rospy.init_node("local_pool_test")
     sonar = Sonar(5) #need to test different # of samples - would also have to make 
-    scan_and_build_sonar_image(sonar, False, jpeg_save_path="Sonar_Image.jpg", start_angle=197, end_angle=203)
+    scan_and_build_sonar_image(sonar, False, jpeg_save_path="Sonar_Image.jpg", start_angle=sys.argv[1], end_angle=sys.argv[2])
