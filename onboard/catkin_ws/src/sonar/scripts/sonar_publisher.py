e902a88a9f1cfd1d24feffb3834e4e2a9726a0bd
Will-Denton <wildenton@outlook.com>
2023-02-22 19:15:54 -0500

    Added method back

--- onboard/catkin_ws/src/sonar/scripts/sonar_publisher.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar_publisher.py
@@ -27,8 +27,8 @@ class SonarPublisher:
         # left = max(center_gradians - breadth_gradians, 0)
         # right = min(center_gradians + breadth_gradians, 400)
 
-        #sonar_xy_result = self.sonar.get_xy_of_object_in_sweep(request.start_angle,request.end_angle)
-        sonar_xy_result = (100, 200)
+        sonar_xy_result = self.sonar.get_xy_of_object_in_sweep(request.start_angle,request.end_angle)
+        #sonar_xy_result = (resp, 200)
 
         response = sweepResult()
         response.x_pos = sonar_xy_result[0]
