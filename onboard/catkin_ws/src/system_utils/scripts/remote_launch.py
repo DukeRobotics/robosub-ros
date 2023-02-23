cb1e5f1c26b15441b01091f9bf7a81aed21c51bb
Vedarsh Shah <vedarsh.shah@gmail.com>
2023-02-22 20:04:35 -0500

    Partially complete remote launch

--- onboard/catkin_ws/src/system_utils/scripts/remote_launch.py
+++ onboard/catkin_ws/src/system_utils/scripts/remote_launch.py
@@ -5,6 +5,8 @@ from custom_msgs.srv import StartLaunch, StopLaunch
 import subprocess
 import multiprocessing
 
+# TODO: Define a message type to publish when a node is launched and terminated and import it here
+
 
 class RemoteLaunchNode:
 
@@ -12,6 +14,9 @@ class RemoteLaunchNode:
         rospy.init_node('remote_launch')
 
         self.processes = {}
+        self.terminated_processes = {}
+
+        # TODO: Initialize a publisher to publish when node is launched and terminated using the message type defined above
 
         rospy.Service('start_node', StartLaunch, self.start_launch)
         rospy.Service('stop_node', StopLaunch, self.stop_launch)
@@ -19,24 +24,37 @@ class RemoteLaunchNode:
         rospy.spin()
 
     def start_launch(self, req):
-        process = multiprocessing.Process(target=self.launch_with_callback, args=(req,))
-        process.start()
-        process.join()
+        with multiprocessing.Manager() as manager:
+            shared_dict = manager.dict()
+            process = multiprocessing.Process(target=self.launch_with_callback, args=(req, shared_dict))
+            process.start()
 
-    def launch_with_callback(self, req):
+            while 'pid' not in shared_dict:
+                continue
+
+            # TODO: Publish node launched, including the pid, package, file, and arguments
+
+            return {'pid': shared_dict['pid']}
+
+    def launch_with_callback(self, req, shared_dict):
         exe = 'roslaunch' if req.is_launch_file else 'rosrun'
         rospy.loginfo(f'Executing {exe} {req.package} {req.file} {req.args}')
         if req.args == ['']:  # No arguments provided
             proc = subprocess.Popen([exe, req.package, req.file])
         else:
             proc = subprocess.Popen([exe, req.package, req.file] + req.args)
+        shared_dict['pid'] = proc.pid
         self.processes[int(proc.pid)] = proc
         proc.wait()
+        self.terminated_processes.append(proc.pid)
         rospy.loginfo(f'Terminating {int(proc.pid)} {req.package} {req.file} {req.args}')
+        # TODO: Publish node terminated, including the pid, package, file, and arguments
         return
-        # return {'pid': int(proc.pid)}
 
     def stop_launch(self, req):
+        if req.pid in self.terminated_processes:
+            return {'success': True}
+
         if req.pid not in self.processes:
             return {'success': False}
 
@@ -45,10 +63,8 @@ class RemoteLaunchNode:
             self.processes[req.pid].wait()
             self.processes.pop(req.pid)
             return {'success': True}
-        return {'success': False}
 
-    # def log_status(self, req):
-    #     rospy.loginfo(f'Terminating {req.pid} {req.package} {req.file} {req.args}')
+        return {'success': False}
 
 
 if __name__ == '__main__':
