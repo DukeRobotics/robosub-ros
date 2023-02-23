4ab90b42b0ae98f4e3a0a82a26f548a7b0875b6e
Will-Denton <wildenton@outlook.com>
2023-02-22 20:25:31 -0500

    image

--- onboard/catkin_ws/src/sonar/scripts/sonar_pool_test.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar_pool_test.py
@@ -5,4 +5,4 @@ import rospy
 if __name__ == "__main__":
     rospy.init_node("local_pool_test")
     sonar = Sonar(5) #need to test different # of samples - would also have to make 
-    scan_and_build_sonar_image(sonar, False, jpeg_save_path="/sampleData/testImgage", start_angle=197, end_angle=203)
+    scan_and_build_sonar_image(sonar, False, jpeg_save_path="/sampleData/testImgage.jpg", start_angle=197, end_angle=203)
