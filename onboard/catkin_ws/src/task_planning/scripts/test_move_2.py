2fe41b3ea33f49671afd9f56d5fca0ea0bbd0daf
Ethan Horowitz <ethan.horowitz@duke.edu>
2023-03-22 21:53:52 -0400

    fixed whitespace for linter

--- onboard/catkin_ws/src/task_planning/scripts/test_move_2.py
+++ onboard/catkin_ws/src/task_planning/scripts/test_move_2.py
@@ -8,10 +8,10 @@ class TestMoveSubscriber:
         # gate, gate_side, gate_tick, gate_top, start_gate, buoy_bootlegger, buoy_gman
         path = 'gate'
         self.sub = rospy.Subscriber(f"/move_base/result/{path}", CVObject, self.callback)
-        
+
     def callback(self, obj: CVObject):
         MoveToPoseGlobalTask(*self.imlazy(obj.pose.position, 'x', 'y', 'z'), 0, 0, 0).execute()
-        
+
     def imlazy(obj, *args):
         '''Returns a list of attributes of an object.'''
         items = []
