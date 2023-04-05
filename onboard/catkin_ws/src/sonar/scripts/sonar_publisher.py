846eb9bb1042447a2d69bae132ebd34b86cf60ff
Rico Zhu <ricozhuthegreat@hotmail.com>
2023-04-05 19:45:19 -0400

    Changed dependencies

--- onboard/catkin_ws/src/sonar/scripts/sonar_publisher.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar_publisher.py
@@ -4,12 +4,13 @@ from sonar import Sonar
 from custom_msgs.msg import sweepResult, sweepGoal
 from sonar_utils import degrees_to_centered_gradians
 from sonar_image_processing import scan_and_build_sonar_image
-
+from cv.image_tools import ImageTools
 
 class SonarPublisher:
 
     SONAR_REQUEST_TOPIC = 'sonar/request'
     SONAR_RESPONSE_TOPIC = 'sonar/cv/response'
+    SONAR_IMAGE_TOPIC = 'sonar/image'
 
     NODE_NAME = "sonar_pub"
 
@@ -17,11 +18,14 @@ class SonarPublisher:
 
     def __init__(self):
         rospy.init_node(self.NODE_NAME)
-        self.debug = rospy.get_param('~debug')
+        self.stream = rospy.get_param('~stream')
         self.port = rospy.get_param('~port')
         self.sonar = Sonar(10, serial_port_number=self.port)
+        self.image_tools = ImageTools()
         self._pub_request = rospy.Publisher(self.SONAR_RESPONSE_TOPIC,
                                             sweepResult, queue_size=10)
+        self.sonar_image_publisher = rospy.Publisher(self.SONAR_IMAGE_TOPIC
+                                            )
 
     def on_request(self, request):
         if (request.distance_of_scan == -1):
@@ -34,7 +38,7 @@ class SonarPublisher:
         sonar_xy_result = self.sonar.get_xy_of_object_in_sweep(left_gradians,
                                                                right_gradians)
 
-        if self.debug:
+        if self.stream:
             scan_and_build_sonar_image(self.sonar, False,
                                        jpeg_save_path="Sonar_Image.jpeg",
                                        start_angle=left_gradians,
