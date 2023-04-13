0ca040765e12380f5630ec6c032e62df9a701227
Cthulhu <--global>
2023-04-12 22:00:16 -0400

    added test_id

--- onboard/catkin_ws/src/sonar/scripts/test_id_usb.py
+++ onboard/catkin_ws/src/sonar/scripts/test_id_usb.py
@@ -1,15 +1,22 @@
-import re
 import subprocess
 
-device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
-df = subprocess.check_output("lsusb")
-devices = []
-for i in df.split(b'\n'):
-    if i:
-        info = device_re.match(i)
-        if info:
-            dinfo = info.groupdict()
-            dinfo['device'] = '/dev/bus/usb/%s/%s' % (dinfo.pop('bus'), dinfo.pop('device'))
-            devices.append(dinfo)
-            
-print(devices)
+# Run the lsusb command and capture the output
+usb_output = subprocess.check_output(['ls', '-l', '/dev/serial/by-id/'])
+
+# Split the output into lines
+usb_lines = usb_output.decode().split('\n')
+
+sonar_string = "usb-FTDI_FT230X_Basic_UART_DK0C1WF7-if00-port0"
+
+# Iterate over each line and get the device path using udevadm
+for line in usb_lines[1:]:
+    if line:
+        fields = line.split(' ')
+        FTDI_Number = fields[8]
+        USB_port = fields[10][-7:]
+        #print('Number {:s} Device {:s}'.format(FTDI_Number, USB_port))
+        if FTDI_Number == sonar_string:
+            print(USB_port)
+
+
+
