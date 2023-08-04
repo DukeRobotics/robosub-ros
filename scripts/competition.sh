ff71cca28c3e6fe73303369e2d33d1582e24072a
Maxwell Lin <maxwellmlin@gmail.com>
2023-08-03 23:01:29 -0700

    Update

--- scripts/competition.sh
+++ scripts/competition.sh
@@ -12,20 +12,20 @@ tmux select-pane -t 0
 tmux rename-window -t $session:$window 'onboard_1'
 tmux send-keys -t $session:$window 'ssh robot@192.168.1.1' 'Enter'
 tmux send-keys -t $session:$window 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
-tmux send-keys -t $session:$window 'rostopic echo state' 'Enter'
+tmux send-keys -t $session:$window 'roslaunch execute motion.launch'
 
 tmux select-pane -t 1
 tmux rename-window -t $session:$window 'onboard_2'
 tmux send-keys -t $session:$window 'ssh robot@192.168.1.1' 'Enter'
 tmux send-keys -t $session:$window 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
-tmux send-keys -t $session:$window 'roslaunch execute motion.launch'
+tmux send-keys -t $session:$window 'rostopic echo state'
 
 tmux select-pane -t 2
 tmux rename-window -t $session:$window 'onboard_3'
 tmux send-keys -t $session:$window 'ssh robot@192.168.1.1' 'Enter'
 tmux send-keys -t $session:$window 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
 tmux send-keys -t $session:$window 'rosservice call /enable_controls 0' 'Enter'
-tmux send-keys -t $session:$window 'rosservice call /enable_controls true'
+tmux send-keys -t $session:$window 'rosservice call /enable_controls 1'
 
 tmux select-pane -t 3
 tmux rename-window -t $session:$window 'onboard_4'
@@ -33,6 +33,6 @@ tmux send-keys -t $session:$window 'ssh robot@192.168.1.1' 'Enter'
 tmux send-keys -t $session:$window 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
 tmux send-keys -t $session:$window 'rosrun controls test_state_publisher.py'
 
-tmux select-pane -t 1
+tmux select-pane -t 0
 
 tmux attach-session -t $session
