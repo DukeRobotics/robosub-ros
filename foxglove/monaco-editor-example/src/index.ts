b993e66ec0a41732f8bdf51204c9084e6ceab097
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-09-13 20:14:06 -0400

    Remove manoco

--- foxglove/monaco-editor-example/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { ExtensionContext } from "@foxglove/studio";
-import { initExamplePanel } from "./ExamplePanel";
-
-export function activate(extensionContext: ExtensionContext) {
-  extensionContext.registerPanel({ name: "Monaco Editor", initPanel: initExamplePanel });
-}
