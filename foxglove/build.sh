c29b36ada132cecf7ca10c1a49e898967ff4a4a6
viethungle-vt1401 <brandon.viethungRI2001@gmail.com>
2023-09-15 02:09:58 -0400

    Completed automatic build script for Foxglove extensions

--- foxglove/build.sh
+++ foxglove/build.sh
@@ -1,39 +1,11 @@
 #!/bin/bash
 
-extension_dirs=$(find . -type d -maxdepth 1 -name "*extension")
-# args=()
-# arr=()
-# for dir in ${extension_dirs//;/$'\n'}; do
-#     args+=("$dir"/src/"*")
-#     arr+=( "$(ls $dir/src/)" )
-# done
+extensions=$(find . -type d -maxdepth 1 -name "*extension")
 
-# var=$( IFS=$' '; echo "${args[*]}" )
-# echo "var $var"
+function build {
+    for dir in ${extensions//;/$'\n'}; do
+        cd $dir; npm run local-install; cd ..;
+    done
+}
 
-# pwd
-
-# arr=$( IFS=$' '; echo "${arr[*]}" )
-# echo "arr $arr"
-
-# while true; do echo $arr | entr -pd 
-# for dir in ${extension_dirs//;/$'\n'}; do
-#     cd "$dir/"; pwd; npm run local-install; cd ..
-# done
-
-# echo $( "ls $var" )
-
-# var=$( ls */src/* )
-# echo $var
-
-while true; do ls */src/* | entr -pd 
-for dir in ${extension_dirs//;/$'\n'}; do
-    echo "detected"; cd $dir; npm run local-install; cd ..;
-done
-done
-
-# while true; do ls ./call-service-panel-extension/src/* ./custom-image-extension/src/* ./publish-topic-extension/src/* |
-#     entr -pd echo "detected stuff"; cd call-service-panel-extension; npm run local-install; cd ..;
-#              cd custom-image-extension; npm run local-install; cd ..;
-#              cd publish-topic-extension; npm run local-install; cd ..;
-#     done
+while true; do ls *-extension/src/* | entr -pd -s 'kill $PPID'; build; done
