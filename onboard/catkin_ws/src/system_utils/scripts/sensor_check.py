067be22af4a4024017c5c6ea0ac799a5f43d6fb2
Rico Zhu <ricozhuthegreat@hotmail.com>
2023-09-20 19:11:35 -0400

    Created sensor_check.py

--- onboard/catkin_ws/src/system_utils/scripts/sensor_check.py
+++ onboard/catkin_ws/src/system_utils/scripts/sensor_check.py
@@ -21,9 +21,9 @@ class SensorCheckNode:
 
     def __init__(self):
         rospy.init_node('sensor_check')
-        self.sensor_rate = { DVL_ODOM_TOPIC: 0, IMU_TOPIC: 0, PRESSURE_TOPIC: 0,
-                             STATE_TOPIC: 0, CAMERA_TOPIC: 0, SONAR_TOPIC: 0,
-                             CV_TOPIC: 0, OFFBOARD_THRUSTER_SPEEDS_TOPIC: 0}
+        self.sensor_rate = { DVL_ODOM_TOPIC: [], IMU_TOPIC: [], PRESSURE_TOPIC: [],
+                             STATE_TOPIC: [], CAMERA_TOPIC: [], SONAR_TOPIC: [],
+                             CV_TOPIC: [], OFFBOARD_THRUSTER_SPEEDS_TOPIC: []}
         # Subscribe to all possible sensors
         self.dvl = rospy.Subscriber(DVL_ODOM_TOPIC, Odometry, self.callback(DVL_ODOM_TOPIC))
         self.imu = rospy.Subscriber(IMU_TOPIC, Imu, self.callback(IMU_TOPIC))
@@ -35,7 +35,27 @@ class SensorCheckNode:
         self.thrusters = rospy.Subscriber(OFFBOARD_THRUSTER_SPEEDS_TOPIC, ThrusterSpeeds, self.callback(OFFBOARD_THRUSTER_SPEEDS_TOPIC))
 
     def callback(self, topic_name):
-        self.sensor_rate[topic_name] += 1
+        # Record time of response
+        self.sensor_rate[topic_name].append(rospy.get_rostime())
+        # Collected enough samples, report average response gap
+        if len(self.sensor_rate[topic_name]) == 20:
+            self.echo_status(topic_name)
+
+    def echo_status(self, topic_name):
+        # Find the average intervals/gaps in between responses
+        response_times = self.sensor_rate[topic_name]
+        topic_average = sum([response_times[t] - response_times[t-1] for t in range(1, len(response_times))]) / len(response_times)
+        rospy.loginfo("\033[32m", topic_name, "is up and running... ", f"average gap between responses: {topic_average: .3f} \033[0m")
+
+    def run(self):
+        # Sleep for 5 seconds to collect messages
+        rospy.sleep(5)
+        # Iterate through topics/sensors to report any sensors that aren't active
+        for topic_name, responses in self.sensor_rate.items():
+            if not responses:
+                rospy.logwarn("\033[31m", topic_name, " is down \033[0m")
+            elif 0 < len(responses) < 20:
+                rospy.logwarn("\033[31m", topic_name, " is responding slow ", len(responses), " responses in the last 5 seconds \033[0m")
 
 if __name__ == "__main__":
-    SensorCheckNode()
+    SensorCheckNode().run()
