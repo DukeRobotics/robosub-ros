d64269ba3f27118e9b63d9398353f816117e6a39
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-09-23 10:49:16 -0400

    Oops

--- foxglove/install.sh
+++ foxglove/install.sh
@@ -6,5 +6,5 @@
 extensions=$(find . -type d -maxdepth 1 -name "*extension")
 
 for dir in ${extensions//;/$'\n'}; do
-    cd $dir; npm ci; npm run local-install; cd ..;
+    cd $dir; npm ci; npm install yarn; npm run local-install; cd ..;
 done
