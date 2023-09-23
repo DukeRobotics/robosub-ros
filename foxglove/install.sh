eedef1f8c0ccbfead270098718c7125369d139ac
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-09-23 10:38:04 -0400

    Update build

--- foxglove/install.sh
+++ foxglove/install.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+# A convenience script to automatically install all Foxglove extensions.
+# Foxglove extensions are defined as any folder matching '*-extension' in the current directory.
+# Usage: ./install.sh (Must be run from the root of the Foxglove directory)
+
 extensions=$(find . -type d -maxdepth 1 -name "*extension")
 
 for dir in ${extensions//;/$'\n'}; do
-    cd $dir; npm ci; npm install yarn; npm run local-install; cd ..;
+    cd $dir; npm ci; npm run local-install; cd ..;
 done
