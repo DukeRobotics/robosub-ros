8c22e1835f7347105420c035accfca6a07e14c78
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-09-22 23:20:39 -0400

    Update build

--- foxglove/build.sh
+++ foxglove/build.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-extensions=$(find . -type d -maxdepth 1 -name "*extension")
+# Check if entr is installed
+if ! command -v entr &> /dev/null
+then
+    echo "entr not found, please install it first: https://github.com/eradman/entr"
+    exit 1
+fi
 
-function build {
-    for dir in ${extensions//;/$'\n'}; do
-        cd $dir; npm run local-install; cd ..;
-    done
+trap SIGINT SIGTERM
+
+monitor_folder() {
+    local folder="$1"
+    # Find all files in the given folder and monitor them using entr
+    ls $folder/src/* | entr -d sh -c "cd $folder && npm run local-install"
 }
 
-while true; do ls *-extension/src/* | entr -pd -s 'kill $PPID'; build; done
+# Find all folders matching '*extension' and start monitoring them
+extensions=$(find . -type d -maxdepth 1 -name "*extension")
+for folder in ${extensions//;/$'\n'}; do
+    echo $folder
+    monitor_folder "$folder" &
+done
+
+while true; do sleep 86400; done
