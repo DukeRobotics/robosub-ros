e346877a091db02289e14303cc68944e7651c636
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-09-23 11:12:47 -0400

    Add yarn

--- foxglove/install.sh
+++ foxglove/install.sh
@@ -6,5 +6,10 @@
 extensions=$(find . -type d -maxdepth 1 -name "*extension")
 
 for dir in ${extensions//;/$'\n'}; do
-    cd $dir; npm ci; npm install yarn; npm run local-install; cd ..;
+    cd $dir
+    npm install
+    npm install yarn
+    yarn install --frozen-lockfile
+    npm run local-install
+    cd ..
 done
