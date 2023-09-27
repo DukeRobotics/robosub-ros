d57af3dfe2166f960ba05325c98076fac7f3e6e5
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-09-26 20:01:58 -0400

    Update names

--- foxglove/publish-topic-extension/src/index.ts
+++ foxglove/publish-topic-extension/src/index.ts
@@ -2,5 +2,5 @@ import { ExtensionContext } from "@foxglove/studio";
 import { initPublishTopicPanel } from "./PublishTopicPanel";
 
 export function activate(extensionContext: ExtensionContext): void {
-  extensionContext.registerPanel({ name: "publish-topic", initPanel: initPublishTopicPanel });
+  extensionContext.registerPanel({ name: "Custom Publish Topic Extension", initPanel: initPublishTopicPanel });
 }
