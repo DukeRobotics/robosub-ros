43ca113502c3cee2997e5f6091a785171ac9bb67
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-09-30 16:50:30 -0400

    Implement toggle-controls-panel
    Co-authored-by: Nathanael Ren <nathanaelren@users.noreply.github.com>
    Co-authored-by: Mason Wu <mmmmason@users.noreply.github.com>
    Co-authored-by: calvinlchen <calvinlchen@users.noreply.github.com>
    Co-authored-by: roguerreiro <roguerreiro@users.noreply.github.com>
    Co-authored-by: Hung Le <viethungle-vt1401@users.noreply.github.com>

--- foxglove/toggle-controls-panel/src/ToggleControlsPanel.tsx
+++ foxglove/toggle-controls-panel/src/ToggleControlsPanel.tsx
@@ -1,4 +1,7 @@
 import { PanelExtensionContext, RenderState } from "@foxglove/studio";
+import Button from "@mui/material/Button/Button";
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import { useCallback, useEffect, useLayoutEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import ReactJson from "react-json-view";
@@ -10,11 +13,12 @@ type State = {
   response?: unknown;
   error?: Error | undefined;
   colorScheme?: RenderState["colorScheme"];
+  controlsEnabled: boolean;
 };
 
 function ToggleControlsPanel({ context }: { context: PanelExtensionContext }): JSX.Element {
   const [renderDone, setRenderDone] = useState<(() => void) | undefined>();
-  const [state, setState] = useState<State>({ serviceName: "", request: "{}" });
+  const [state, setState] = useState<State>({ serviceName: "", request: "{}", controlsEnabled: false });
 
   useLayoutEffect(() => {
     context.onRender = (renderState: RenderState, done) => {
@@ -29,27 +33,30 @@ function ToggleControlsPanel({ context }: { context: PanelExtensionContext }): J
     renderDone?.();
   }, [renderDone]);
 
-  const callService = useCallback(
-    async (serviceName: string, request: string) => {
-      if (!context.callService) {
-        return;
-      }
+  const toggleControls = async () => {
+    if (!context.callService) {
+      return;
+    }
 
-      try {
-        const response = await context.callService(serviceName, JSON.parse(request));
-        JSON.stringify(response); // Attempt serializing the response, to throw an error on failure
-        setState((oldState) => ({
-          ...oldState,
-          response,
-          error: undefined,
-        }));
-      } catch (error) {
-        setState((oldState) => ({ ...oldState, error: error as Error }));
-        console.error(error);
-      }
-    },
-    [context.callService],
-  );
+    const serviceName = "/enable_controls";
+    const request = { "data": !state.controlsEnabled };
+
+    setState({ ...state, controlsEnabled: !state.controlsEnabled })
+
+    try {
+      const response = await context.callService(serviceName, request);
+      JSON.stringify(response); // Attempt serializing the response, to throw an error on failure
+      setState((oldState) => ({
+        ...oldState,
+        response,
+        error: undefined,
+      }));
+    } catch (error) {
+      setState((oldState) => ({ ...oldState, error: error as Error }));
+      console.error(error);
+    }
+
+  }
 
   return (
     <div style={{ padding: "1rem" }}>
@@ -58,40 +65,14 @@ function ToggleControlsPanel({ context }: { context: PanelExtensionContext }): J
         <p style={{ color: "red" }}>Calling services is not supported by this connection</p>
       )}
 
-      <h4>Service Name</h4>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter service name"
-          style={{ width: "100%" }}
-          value={state.serviceName}
-          onChange={(event) => {
-            setState({ ...state, serviceName: event.target.value });
-          }}
-        />
-      </div>
-      <h4>Request</h4>
-      <div>
-        <textarea
-          style={{ width: "100%", minHeight: "3rem" }}
-          value={state.request}
-          onChange={(event) => {
-            setState({ ...state, request: event.target.value });
-          }}
-        />
-      </div>
-      <div>
-        <button
-          disabled={context.callService == undefined || state.serviceName === ""}
-          style={{ width: "100%", minHeight: "2rem" }}
-          // eslint-disable-next-line @typescript-eslint/no-misused-promises
-          onClick={async () => {
-            await callService(state.serviceName, state.request);
-          }}
-        >
-          {`Call ${state.serviceName}`}
-        </button>
-      </div>
+      <Button
+        variant="contained"
+        color={state.controlsEnabled ? "error" : "success"}
+        endIcon={state.controlsEnabled ? <HighlightOffIcon /> : <CheckCircleOutlineIcon />}
+        onClick={async () => { await toggleControls(); }}
+      >
+        {state.controlsEnabled ? "Disable Controls" : "Enable Controls"}
+      </Button>
 
       <div>
         <h4>Response</h4>
