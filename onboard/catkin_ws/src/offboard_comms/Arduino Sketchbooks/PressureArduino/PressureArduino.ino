f9c07e1fb44fd8781851be7fd1a88707f1fd82d4
Michael Bryant <mcb115@duke.edu>
2023-10-05 20:53:00 -0400

    arduino serial for pressure, no ros

--- onboard/catkin_ws/src/offboard_comms/Arduino Sketchbooks/PressureArduino/PressureArduino.ino
+++ onboard/catkin_ws/src/offboard_comms/Arduino Sketchbooks/PressureArduino/PressureArduino.ino
@@ -1,8 +1,8 @@
-#include <ros.h>
+// #include <ros.h>
 #include <Arduino.h>
 #include <Wire.h>
 #include "MS5837.h"
-#include <std_msgs/Float64.h>
+// #include <std_msgs/Float64.h>
 
 MS5837 sensor;
 
@@ -10,22 +10,27 @@ MS5837 sensor;
 #define BAUD_RATE 9600
 
 // 1 publisher for depth sensor
-ros::NodeHandle_<ArduinoHardware,1,1,128,128> nh;
-
-std_msgs::Float64 depth;
-ros::Publisher pressure_pub("/offboard/pressure", &depth);
+//ros::NodeHandle_<ArduinoHardware,1,1,128,128> nh;
+// ros::NodeHandle nh;
+// std_msgs::Float64 depth;
+// ros::Publisher pressure_pub = ros::Publisher("/offboard/pressure", &depth);
 
 void setup(){
 
-    Serial1.begin(BAUD_RATE);
-    nh.getHardware()->setBaud(BAUD_RATE);
-    nh.initNode();
-    nh.advertise(pressure_pub);
+    // Serial1.begin(0); //weird hack
+    delay(100);
+    // Serial1.end(); // Close the serial port
+    // Serial1.begin(9600);
+    Serial.begin(9600);
+
+    // nh.getHardware()->setBaud(BAUD_RATE);
+    // nh.initNode();
+    // nh.advertise(pressure_pub);
 
     Wire.begin();
 
     while (!sensor.init()) {
-        nh.logwarn("Pressure sensor not initialized. Will attempt every second until found.");
+        // nh.logwarn("Pressure sensor not initialized. Will attempt every second until found.");
         delay(1000);
         break;
     }
@@ -36,34 +41,13 @@ void setup(){
 void loop(){
 
     sensor.read();
+    // depth = std_msgs::Float64();
+    // depth.data = sensor.depth();
+    
+    // pressure_pub.publish(&depth);
+    Serial.flush();
+    // nh.spinOnce();
 
-    depth = sensor.depth()
-
-    pressure_pub.publish(&depth);
-
-    nh.spinOnce();
-
-    // odom_msg.header.frame_id = "odom";
-
-    // // Set all values to 0 except for depth
-    // // Zeroed values are not used by the EKF
-    // geometry_msgs::Point point;
-    // point.x = 0.0;
-    // point.y = 0.0;
-    // point.z = sensor.depth();
-
-    // // Unit quaternion -- also unused by EKF
-    // geometry_msgs::Quaternion quat;
-    // quat.x = 0.0;
-    // quat.y = 0.0;
-    // quat.z = 0.0;
-    // quat.w = 1.0;
-
-    // geometry_msgs::Pose pose;
-    // pose.position = point;
-    // pose.orientation = quat;
-    // odom_msg.pose.pose = pose;
-    // odom_msg.pose.covariance[14] = 0.01; // depth variance
-
-    // pressure_pub.publish(&odom_msg);
+    //publish the depth over serial
+    Serial.println(sensor.depth());
 }
