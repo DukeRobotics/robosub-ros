9954d3693c473abf8c013c5d70fa3c8dbe890ea9
Vedarsh Shah <vedarsh.shah@gmail.com>
2023-10-11 21:03:46 -0400

    Created file for PID controller implementation

---
+++ onboard/catkin_ws/src/controls/scripts/pid.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+TODO: Implement a PID controller
+The PID controller should be a class that is initialized with the PID gains (Kp, Ki, Kd, and feedforward).
+
+There should be a method that takes in the error value and delta time and returns the control effort. The feedfoward
+term should be added to the control effort just before returning. The final returned value must always be
+between -1 and 1 (inclusive).
+
+There should be a method that can reset the PID controller; this would be called when a new setpoint is given.
+
+It should be possible for the PID gains to be changed at any time, even while the controller is running.
+
+Context: One instance of this class will be created per axis (x, y, z, roll, pitch, yaw) per control type
+(position, velocity). The first method described above will be called every time /state is published. The second
+method will be called every time a new setpoint is published to /desired_pose or /desired_twist. The PID gains will
+be updated every time a service is called to change them (this service will be advertised by separate node).
+"""
