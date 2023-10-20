794ea2937ae6e8c11ab4f294c0657fc39f7a6067
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-10-20 12:01:21 -0400

    Update lint

--- foxglove/foxglove.py
+++ foxglove/foxglove.py
@@ -83,6 +83,8 @@ def install_extensions(extension_paths: Sequence[pathlib.Path]):
     run_at_path("npm ci", FOXGLOVE_PATH)
     run_at_path("npx patch-package --patch-dir patches", FOXGLOVE_PATH)
 
+    run_at_path("npm ci", FOXGLOVE_PATH)
+
     successes = 0
     for extension in extension_paths:
         run = functools.partial(run_at_path, directory=extension)
