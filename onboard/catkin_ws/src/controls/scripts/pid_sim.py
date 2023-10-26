1e2c58ef3159a6bf2142f73364a68e5b50b78369
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-10-26 18:31:38 -0400

    PID sim

--- onboard/catkin_ws/src/controls/scripts/pid_sim.py
+++ onboard/catkin_ws/src/controls/scripts/pid_sim.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from custom_msgs.msg import PIDGain
+from custom_msgs.msg import PIDGain, PIDGains
 from custom_msgs.srv import SetPIDGains
 import rospy
 
@@ -29,9 +29,7 @@ class PIDSim():
             PIDGain.VELOCITY_PID: axes
         }
 
-        print(self.pid)
-
-        self.pid_pub = rospy.Publisher('current_pid', PIDGain, queue_size=1)
+        self.pid_pub = rospy.Publisher('current_pid', PIDGains, queue_size=1)
         self.set_pid = rospy.Service('set_pid', SetPIDGains, self.handle_set_pid)
 
         rospy.loginfo("pid_sim initialized")
@@ -39,20 +37,25 @@ class PIDSim():
 
     def pid_publish_loop(self):
         rate = rospy.Rate(20)
-        pid_gain = PIDGain()
+
         while not rospy.is_shutdown():
-            for pid_loop, axes in enumerate(self.pid):
+            pid_gain = PIDGain()
+            pid_gains = PIDGains()
+
+            for pid_loop, axes in self.pid.items():
                 pid_gain.pid_loop = pid_loop
-                for axis, gains in enumerate(axes):
-                    print(axes)
+
+                for axis, gains in axes.items():
                     pid_gain.axis = axis
-                    for gain_type, gain in enumerate(gains):
+                    for gain_type, gain in gains.items():
                         pid_gain.gain_type = gain_type
                         pid_gain.gain = gain
 
                         print(pid_gain)
+                        pid_gains.pid_gains.append(pid_gain)
 
-                        self.pid_pub.publish(pid_gain)
+            self.pid_pub.publish(pid_gains)
+
             rate.sleep()
 
     def handle_set_pid(self, req):
