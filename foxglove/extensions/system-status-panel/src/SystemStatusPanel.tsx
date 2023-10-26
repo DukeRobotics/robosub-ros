f58e357c816eb73190c0b9bd31edeaf43634ee38
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-10-25 20:50:25 -0400

    Some linting

--- foxglove/extensions/system-status-panel/src/SystemStatusPanel.tsx
+++ foxglove/extensions/system-status-panel/src/SystemStatusPanel.tsx
@@ -1,23 +1,19 @@
 import { PanelExtensionContext, RenderState, Topic, MessageEvent } from "@foxglove/studio";
-import { useLayoutEffect, useEffect, useState, useRef, useMemo } from "react";
-import ReactDOM from "react-dom";
-import ReactJson from "react-json-view";
-import Alert from '@mui/material/Alert';
-import { useTheme } from '@mui/material/styles';
+import Alert from "@mui/material/Alert";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { useTheme } from "@mui/material/styles";
+import { useLayoutEffect, useEffect, useState, useMemo } from "react";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
 
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
-function createData(
-  Status: string,
-  Value: number,
-) {
+function createData(Status: string, Value: number) {
   return { Status, Value };
 }
 
@@ -39,22 +35,16 @@ function SystemStatusPanel({ context }: { context: PanelExtensionContext }): JSX
   // Restore our state from the layout via the context.initialState property.
   const [state, setState] = useState<State>(() => {
     const initialState = context.initialState as State;
-    initialState.topic = '/system/usage';
+    initialState.topic = "/system/usage";
     return initialState;
   });
 
   // Get topics
-  const imageTopics = useMemo(
-    () => (topics ?? []),
-    [topics],
-  );
+  const imageTopics = useMemo(() => topics ?? [], [topics]);
 
   //define values in table
-  const rows = [
-    createData('CPU', state.cpuUsage),
-    createData('RAM', state.ramUsage),
-  ];
-  
+  const rows = [createData("CPU", state.cpuUsage), createData("RAM", state.ramUsage)];
+
   useEffect(() => {
     // Save our state to the layout when the topic changes.
     context.saveState({ topic: state.topic });
@@ -78,22 +68,22 @@ function SystemStatusPanel({ context }: { context: PanelExtensionContext }): JSX
       setRenderDone(() => done);
       setTopics(renderState.topics);
       setState((oldState) => ({ ...oldState, colorScheme: renderState.colorScheme }));
-      
+
       // Updating CPU/RAM/Voltage data.
       if (renderState.currentFrame && renderState.currentFrame.length > 0) {
         const latestFrame = renderState.currentFrame[renderState.currentFrame.length - 1] as MessageEvent<any>;
-        setState((oldState) => ({ ...oldState,
-                                  cpuUsage : latestFrame?.message?.cpu_percent,
-                                  ramUsage : latestFrame?.message?.ram.percentage}))
+        setState((oldState) => ({
+          ...oldState,
+          cpuUsage: latestFrame?.message?.cpu_percent,
+          ramUsage: latestFrame?.message?.ram.percentage,
+        }));
         console.log(latestFrame?.message?.["cpu_percent"]);
       }
-      
     };
 
     context.watch("topics");
     context.watch("currentFrame");
     context.watch("colorScheme");
-
   }, [context]);
 
   // Call our done function at the end of each render.
@@ -105,47 +95,46 @@ function SystemStatusPanel({ context }: { context: PanelExtensionContext }): JSX
     <div style={{ height: "100%", padding: "1rem" }}>
       <h2>System Status</h2>
       {context.subscribe == undefined && (
-        <Alert variant="filled" severity="error">Subscribing to topics is not supported by this connection</Alert>
+        <Alert variant="filled" severity="error">
+          Subscribing to topics is not supported by this connection
+        </Alert>
       )}
       <div>
         <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 100 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Component</TableCell>
-                    <TableCell align="right">Usage</TableCell>
-                    
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row) => (
-                    <TableRow
-                      key={row.Status}
-                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                      style={{ backgroundColor: (row.Value >= 90) ? theme.palette.error.main : "white" }}
-                    >
-                      <TableCell component="th" scope="row">
-                        {row.Status}
-                      </TableCell>
-                      <TableCell align="right">{row.Value}%</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-
-            </div>
-          </div>
-
-    
+          <Table sx={{ minWidth: 100 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Component</TableCell>
+                <TableCell align="right">Usage</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.Status}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  style={{ backgroundColor: row.Value >= 90 ? theme.palette.error.main : "white" }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.Status}
+                  </TableCell>
+                  <TableCell align="right">{row.Value}%</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
   );
 }
 
 export function initSystemStatusPanel(context: PanelExtensionContext): () => void {
-  ReactDOM.render(<SystemStatusPanel context={context} />, context.panelElement);
+  const root = createRoot(context.panelElement as HTMLElement);
+  root.render(<SystemStatusPanel context={context} />);
 
   // Return a function to run when the panel is removed
   return () => {
-    ReactDOM.unmountComponentAtNode(context.panelElement);
+    root.unmount();
   };
 }
