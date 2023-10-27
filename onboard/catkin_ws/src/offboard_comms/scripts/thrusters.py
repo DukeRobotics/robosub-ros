4ccb483eb0d24969e820f2dc6e35c472562fd482
Rico Zhu <ricozhuthegreat@hotmail.com>
2023-10-26 20:02:47 -0400

    Updated thrusters.py

--- onboard/catkin_ws/src/offboard_comms/scripts/thrusters.py
+++ onboard/catkin_ws/src/offboard_comms/scripts/thrusters.py
@@ -2,11 +2,13 @@
 import rospy
 import numpy as np
 
+from bisect import bisect_left
+
 from std_msgs.msg import Float32, Int32
 from custom_msgs.msg import ThrusterAllocs
 
 CONTROL_EFFORTS_TOPIC = "controls/thruster_allocs"
-VOLTAGE_TOPIC = "controls/voltage"
+VOLTAGE_TOPIC = "controls/voltage" # This could change
 PWM_PUBLISHER_TOPIC = "offboard_comms/pwm"
 
 COEFFICIENTS = [-0.72598292, 17.51636272, -1.96635442, 341.37137439, 17.68477542,
@@ -65,13 +67,23 @@ class ThrusterConverter:
         self.voltage_efforts_to_pwm = dict()
         # Load all computed power curves from 14.0v to 18.0v
         for voltage in range(14.0, 18.1, 0.1):
-            file_path = "./data/" + str(voltage) + "_interpolated.csv"
-            pass
+            # Read .npy file to load pre-calculated linear interpolation data
+            file_path = "./data/" + str(voltage) + "_interpolated.npy"
+            loaded_np_array = np.load(file_path)
+            pwm, force = loaded_np_array[:, 0], loaded_np_array[:, 1]
+            # self.voltage_efforts_to_pwm[15.5] = [forces array for 15.5, pwm array for 15.5]
+            self.voltage_efforts_to_pwm[voltage] = [force, pwm]
 
     # This method takes in voltage and desired control effort and outputs pwn
     # via the linearly interpolated lookup tables
     def lookup(self, voltage, force):
-        pass
+        # Round voltage to the nearest tenth decimal place
+        voltage = round(voltage, 1)
+        # Efficiently look up the requested index for a given query (voltage, force) in table via binary search
+        # We use binary search to find the left insertion position of force in the associated force array
+        # Note that "force" is always in sorted increasing order, so we can use an array bisection algorithm
+        left_index = bisect_left(self.voltage_efforts_to_pwm[voltage][0], force)
+        return self.voltage_efforts_to_pwm[voltage][1][left_index]
 
     # This method takes in voltage and desired control effort and outputs pwm
     # via the fitted polynomial model
