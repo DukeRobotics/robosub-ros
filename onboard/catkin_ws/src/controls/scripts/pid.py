8bf3b84f482db68d22c4a1975342f42031d652dc
vyw2 <vyw2@duke.edu>
2023-10-28 14:04:37 +0900

    Initial commit of Python PID script ported from ROS PID

--- onboard/catkin_ws/src/controls/scripts/pid.py
+++ onboard/catkin_ws/src/controls/scripts/pid.py
@@ -23,9 +23,72 @@ to impose a windup limit. See https://wiki.ros.org/pid for more information on t
 """
 
 
+import numpy as np
+
 class PID:
-    def __init__(self, Kp, Ki, Kd, Ff):
+    def __init__(self, Kp, Ki, Kd, Ff, intClamp = 1000.0, cutoffFreq = 5, angleCorrection = True):
+        self.cutoffFreq = cutoffFreq # 1/4 sampling = 5Hz default
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
         self.Ff = Ff
+        self.intClamp = intClamp
+        self.angleCorrection = angleCorrection
+        
+        # Error/Time Array
+        self.prevValues = np.zeros((3, 3)) # Rows are error, time, derivative
+        # Integral error
+        self.intError = 0
+    
+    def reset(self):
+        '''
+        Function to reset all params in memory
+        '''
+        self.intError = 0
+        self.prevValues.fill(0)
+
+    def setCutoff(self, cutoffFreq):
+        self.cutoffFreq = cutoffFreq
+   
+    def setFf(self, Ff):
+        self.Ff = Ff
+       
+    def setWindupClamp(self, intClamp):
+        self.intClamp = intClamp
+       
+    def setKp(self, Kp):
+        self.Kp = Kp
+        
+    def setKi(self, Ki):
+        self.Ki = Ki
+        
+    def setKd(self, Kd):
+        self.Kd = Kd
+        
+    def calcControlEffort(self, error, dt):
+        # Update value array
+        self.prevValues[0, 0], self.prevValues[0, 1], self.prevValues[0, 2] = error, self.prevValues[0, 0], self.prevValues[0, 1]
+        self.prevValues[1, 0], self.prevValues[1, 1], self.prevValues[1, 2] = dt + self.prevValues[1, 0], self.prevValues[1, 0], self.prevValues[1, 1]
+        
+        # Angle wrap to cast all angles [-180, 180]
+        # Not sure if this is needed since the error is passed rather than the PV. If error is clamped to [-180, 180] already, this is redundant.
+        if self.angleCorrection:
+            error = (error - np.pi) % (2 * np.pi) - np.pi
+        
+        # Update int.
+        self.intError += error * dt
+        
+        # Integral windup to protect against sudden setpoint changes or unactuatable states
+        #self.intError = min(max(self.intError, -self.intClamp), self.intClamp)
+        
+        self.prevValues[2, 0], self.prevValues[2, 1], self.prevValues[2, 2] = (self.prevValues[0, 1] - self.prevValues[0, 0]) / dt, self.prevValues[2, 0], self.prevValues[2, 1]
+        
+        # Add filtering of error here if needed to smooth out error and errorDerivative
+        
+        # Calculate control effort 
+        ### TODO: Replace with filtered params ###
+        controlOutput = self.Kp * self.prevValues[0, 0] + self.Ki * self.intError + self.Kd * self.prevValues[2, 0] + self.Ff
+        
+        # Clamp to effort limits
+        controlOutput = min(max(controlOutput, -1), 1)
+        return controlOutput
