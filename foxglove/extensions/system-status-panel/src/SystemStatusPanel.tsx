60436db53f1f675b1af93208aab93ef5149f41c1
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-11-03 22:19:38 -0400

    Cleanup

--- foxglove/extensions/system-status-panel/src/SystemStatusPanel.tsx
+++ foxglove/extensions/system-status-panel/src/SystemStatusPanel.tsx
@@ -1,14 +1,16 @@
 import { PanelExtensionContext, RenderState, MessageEvent, Immutable } from "@foxglove/studio";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
 import { useTheme } from "@mui/material/styles";
 import { useLayoutEffect, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+
+const SYSTEM_USAGE_TOPIC = "/system/usage";
 
 interface SystemUsage {
   cpu_percent: number;
@@ -25,47 +27,35 @@ interface Memory {
   percentage: number; // As a percentage value, e.g., 50 for 50%
 }
 
-function createData(Status: string, Value: number) {
-  return { Status, Value };
+function createData(status: string, value?: number) {
+  return { Status: status, Value: value };
 }
 
 type State = {
-  topic: string;
-  colorScheme?: RenderState["colorScheme"];
-  cpuUsage: number;
-  ramUsage: number;
+  cpuUsage?: number;
+  ramUsage?: number;
 };
 
 function SystemStatusPanel({ context }: { context: PanelExtensionContext }): JSX.Element {
   const [renderDone, setRenderDone] = useState<(() => void) | undefined>();
 
-  const theme = useTheme();
+  context.subscribe([{ topic: SYSTEM_USAGE_TOPIC }]);
 
   // Restore our state from the layout via the context.initialState property.
-  const [state, setState] = useState<State>(() => {
-    const initialState = context.initialState as State;
-    initialState.topic = "/system/usage";
-    return initialState;
-  });
+  const [state, setState] = useState<State>({});
 
-  //define values in table
+  // Define values in table
   const rows = [createData("CPU", state.cpuUsage), createData("RAM", state.ramUsage)];
-
-  useEffect(() => {
-    // Save our state to the layout when the topic changes.
-    context.saveState({ topic: state.topic });
-
-    if (state.topic) {
-      // Subscribe to the new image topic when a new topic is chosen.
-      context.subscribe([{ topic: state.topic }]);
-    }
-  }, [context, state.topic]);
 
   // Setup our onRender function and start watching topics and currentFrame for messages.
   useLayoutEffect(() => {
     context.onRender = (renderState: Immutable<RenderState>, done) => {
       setRenderDone(() => done);
-      setState((oldState) => ({ ...oldState, colorScheme: renderState.colorScheme }));
+
+      // Reset state when the user seeks the video
+      if (renderState.didSeek ?? false) {
+        setState({});
+      }
 
       // Updating CPU/RAM/Voltage data.
       if (renderState.currentFrame && renderState.currentFrame.length > 0) {
@@ -80,28 +70,45 @@ function SystemStatusPanel({ context }: { context: PanelExtensionContext }): JSX
 
     context.watch("topics");
     context.watch("currentFrame");
-    context.watch("colorScheme");
+    context.watch("didSeek");
   }, [context]);
 
-  // Call our done function at the end of each render.
+  // Call our done function at the end of each render
   useEffect(() => {
     renderDone?.();
   }, [renderDone]);
 
+  // Render a table with the current system usage data
+  const theme = useTheme();
   return (
     <Box m={1}>
       <TableContainer component={Paper}>
-        <Table size="small">
+        <Table
+          size="small"
+          sx={{
+            [`& .${tableCellClasses.root}`]: {
+              borderBottom: "none",
+            },
+          }}
+        >
           <TableBody>
             {rows.map((row) => (
               <TableRow
                 key={row.Status}
-                style={{ backgroundColor: row.Value >= 90 ? theme.palette.error.main : "white" }}
+                style={{
+                  backgroundColor: (row.Value ?? 100) >= 90 ? theme.palette.error.main : theme.palette.success.main,
+                }}
               >
-                <TableCell component="th" scope="row" style={{ borderBottom: "none" }}>
-                  {row.Status}
+                <TableCell component="th" scope="row">
+                  <Typography variant="subtitle2" color={theme.palette.common.white}>
+                    {row.Status}
+                  </Typography>
                 </TableCell>
-                <TableCell align="right">{row.Value}%</TableCell>
+                <TableCell align="right">
+                  <Typography variant="subtitle2" color={theme.palette.common.white}>
+                    {row.Value}%
+                  </Typography>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
