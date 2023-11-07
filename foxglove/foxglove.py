9ac684245bb2581daef78d124c22d67bb8692398
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2023-11-07 11:23:47 -0500

    Simplify foxglove build

--- foxglove/foxglove.py
+++ foxglove/foxglove.py
@@ -88,10 +88,8 @@ def build_deps():
     # Patch dependencies
     run("npx patch-package --patch-dir patches")
 
-    # Compile local shared dependencies
-    dependencies = [d for d in (FOXGLOVE_PATH / "shared").iterdir() if d.is_dir()]
-    for dep in dependencies:
-        run_at_path("npx tsc", dep)
+    # Build
+    run("npm run build --workspaces --if-present")
 
 
 def install_extensions(extension_paths: Sequence[pathlib.Path]):
