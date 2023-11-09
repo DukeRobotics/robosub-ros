4b4e0ce3b237618ee985d9614d4270e0f1d1b7c3
Vedarsh Shah <vedarsh.shah@gmail.com>
2023-11-08 20:36:24 -0500

    Added test C++ code and modified cmake to compile C++

---
+++ onboard/catkin_ws/src/controls/scripts/test.cpp
@@ -0,0 +1,35 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+int main(int argc, char** argv)
+{
+  // Initialize the ROS node
+  ros::init(argc, argv, "hello_world_publisher");
+
+  // Create a ROS node handle
+  ros::NodeHandle nh;
+
+  // Create a publisher for the "hello_world" topic
+  ros::Publisher pub = nh.advertise<std_msgs::String>("hello_world_cpp", 10);
+
+  // Set the publishing rate (in Hz)
+  ros::Rate rate(50000);  // Publish at 1 Hz (1 message per second)
+
+  while (ros::ok())
+  {
+    // Create a message and set its data
+    std_msgs::String msg;
+    msg.data = "Hello, World!";
+
+    // Publish the message
+    pub.publish(msg);
+
+    // Spin once to process callbacks
+    ros::spinOnce();
+
+    // Sleep to maintain the desired publishing rate
+    rate.sleep();
+  }
+
+  return 0;
+}
