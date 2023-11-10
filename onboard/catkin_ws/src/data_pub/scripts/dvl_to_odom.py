03adeeb181fb4d23f32710c2dcab63c6bd3a9156
wrd10 <wrd10@duke.edu>
2023-11-09 20:16:27 -0500

    fixed dvl x / y

--- onboard/catkin_ws/src/data_pub/scripts/dvl_to_odom.py
+++ onboard/catkin_ws/src/data_pub/scripts/dvl_to_odom.py
@@ -43,8 +43,8 @@ def callback(msg):
 
     # bs velocity, normalized to meters (given in mm)
     # parentheses denote new negative signs
-    vx = np.float64(msg.bs_longitudinal) / 1000
-    vy = np.float64(msg.bs_transverse) / 1000
+    vx = np.float64(msg.bs_transverse) / 1000
+    vy = np.float64(msg.bs_longitudinal) / 1000
     vz = np.float64(msg.bs_normal) / 1000
 
     if config_data["negate_x_vel"]:
@@ -64,7 +64,7 @@ def callback(msg):
     odom.pose.pose = Pose(Point(x, y, z), Quaternion(*odom_quat))
     odom.child_frame_id = "dvl_link"
     # set twist (set angular velocity to (0, 0, 0), should not be used)
-    odom.twist.twist = Twist(Vector3(vy, vx, vz), Vector3(0, 0, 0))
+    odom.twist.twist = Twist(Vector3(vx, vy, vz), Vector3(0, 0, 0))
     odom.twist.covariance[0] = 0.01
     odom.twist.covariance[7] = 0.01
     odom.twist.covariance[14] = 0.01
