0625b9cfe81f27c194d8fd9590a5b4875a4b0ffa
Vedarsh Shah <vedarsh.shah@gmail.com>
2023-11-15 20:55:35 -0500

    Setup PID controller methods

---
+++ onboard/catkin_ws/src/controls/src/pid.cpp
@@ -0,0 +1,27 @@
+#include "pid.h"
+
+PID::PID() {};
+
+PID::PID(std::map<PIDGainTypesEnum, double> &pid_gains,
+         double integral_clamp,
+         double cutoff_freq,
+         bool angle_correction)
+{
+    this->pid_gains = &pid_gains;
+    this->integral_clamp = integral_clamp;
+    this->cutoff_freq = cutoff_freq;
+    this->angle_correction = angle_correction;
+}
+
+void PID::reset()
+{
+    integral = 0.0;
+    prev_values = Eigen::Matrix<double, 3, 3>::Zero();
+}
+
+double PID::run_loop(double error, double deltaTime)
+{
+    // TODO: Implement PID controller
+    return 0;
+}
+
