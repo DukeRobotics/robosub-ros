5748305f7f3aee8e3a6923e435af8db36da5aa00
Vedarsh Shah <vedarsh.shah@gmail.com>
2023-12-06 14:46:45 -0500

    Modified test.cpp

--- onboard/catkin_ws/src/controls/src/test.cpp
+++ onboard/catkin_ws/src/controls/src/test.cpp
@@ -5,17 +5,36 @@
 
 int main(int argc, char** argv)
 {
-  tf2::Vector3 v_orig(0, 0, -1) ;
-  tf2::Quaternion q( 0, 0.7071068, 0, 0.7071068 ) ;
-  tf2::Vector3 v_new = quatRotate(q.inverse(), v_orig);
-  std::cout << "v_orig: " << v_orig.getX() << ", " << v_orig.getY() << ", " << v_orig.getZ() << std::endl;
-  std::cout << "q: " << q.getX() << ", " << q.getY() << ", " << q.getZ() << ", " << q.getW() << std::endl;
-  std::cout << "v_new: " << v_new.getX() << ", " << v_new.getY() << ", " << v_new.getZ() << std::endl;
-  return 0;
+  // tf2::Vector3 v_orig(0, 0, -1) ;
+  // tf2::Quaternion q( 0, 0.7071068, 0, 0.7071068 ) ;
+  // tf2::Vector3 v_new = quatRotate(q.inverse(), v_orig);
+  // std::cout << "v_orig: " << v_orig.getX() << ", " << v_orig.getY() << ", " << v_orig.getZ() << std::endl;
+  // std::cout << "q: " << q.getX() << ", " << q.getY() << ", " << q.getZ() << ", " << q.getW() << std::endl;
+  // std::cout << "v_new: " << v_new.getX() << ", " << v_new.getY() << ", " << v_new.getZ() << std::endl;
+  // return 0;
 
   // Get rpy from quaternion
+  tf2::Quaternion q(0, 0, 0.9999912, -0.0042037);
+  tf2::Quaternion nq = q.normalized();
+  std::cout << "q: " << q.getX() << ", " << q.getY() << ", " << q.getZ() << ", " << q.getW() << std::endl;
+  std::cout << "nq: " << nq.getX() << ", " << nq.getY() << ", " << nq.getZ() << ", " << nq.getW() << std::endl;
+
+  std::cout << std::endl;
+
+  // Equivalent to ZYX and python's quaternion_from_euler
+  // In other words, given a set of roll, pitch, yaw angles (in radians) converted into a quaternion with
+  // quaternion_from_euler with the default value for its axes arg (sxyz), the code below (getRPY), called on that
+  // quaternion, will return the same original roll, pitch, yaw angles (in radians).
   double roll, pitch, yaw;
-  tf2::Matrix3x3(tf2::Quaternion(1, 0.5, 0, 0)).getRPY(roll, pitch, yaw);
+  tf2::Matrix3x3(nq).getRPY(roll, pitch, yaw);
+  std::cout << "roll: " << roll << std::endl;
+  std::cout << "pitch: " << pitch << std::endl;
+  std::cout << "yaw: " << yaw << std::endl;
+
+  std::cout << std::endl;
+
+  //
+  tf2::Matrix3x3(nq).getEulerYPR(roll, pitch, yaw);
   std::cout << "roll: " << roll << std::endl;
   std::cout << "pitch: " << pitch << std::endl;
   std::cout << "yaw: " << yaw << std::endl;
