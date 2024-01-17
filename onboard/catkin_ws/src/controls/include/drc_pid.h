a39c05449db6fc3ab6064aa3e48a842e06046bfb
Vedarsh Shah <vedarsh.shah@gmail.com>
2024-01-17 16:16:44 -0500

    Added comments to PID controller

--- onboard/catkin_ws/src/controls/include/drc_pid.h
+++ onboard/catkin_ws/src/controls/include/drc_pid.h
@@ -1,35 +1,126 @@
+// Adapted from Andrew Zelenak's ROS PID Package.
+// http://wiki.ros.org/pid
+// https://bitbucket.org/AndyZe/pid/src/master
+
 // Named DRC_PID to not conflict with ROS pid package
 #ifndef DRC_PID_H
 #define DRC_PID_H
 
-#include <memory>
 #include <array>
-#include <Eigen/Dense>
+#include <memory>
+
 #include "controls_utils.h"
 
 class PID
 {
 public:
-    std::shared_ptr<PIDGainsMap> pid_gains;
+
+    /**
+     * @brief The maximum absolute value of the integral term. This is used to prevent integral windup.
+     */
     double integral_clamp = 1000.0;
+
+    /**
+     * @brief The cutoff frequency of the butterworth filter used to filter the derivative term. The default value is
+     *  1.0, which means that the filter will have a cutoff frequency at 1/4 of the sampling rate.
+     */
     double c = 1.0;
-    double integral = 0;
+
+    /**
+     * @brief Control effort will not be greater than this value.
+     */
     double control_effort_max = 1.0;
+
+    /**
+     * @brief Control effort will not be less than this value.
+     */
     double control_effort_min = -1.0;
 
+    /**
+     * @brief The integral of the error function. Updated every iteration of the PID loop.
+     */
+    double integral = 0;
+
+    /**
+     * @brief The three most recent errors, in order from newest to oldest.
+     */
     std::array<double, 3> errors;
+
+    /**
+     * @brief The three most recent filtered errors, in order from newest to oldest.
+     */
     std::array<double, 3> filtered_errors;
+
+    /**
+     * @brief The three most recent unfiltered derivatives, in order from newest to oldest.
+     */
     std::array<double, 3> derivs;
+
+    /**
+     * @brief The three most recent filtered derivatives, in order from newest to oldest.
+     */
     std::array<double, 3> filtered_derivs;
 
+    /**
+     * @brief The weights of the proportional, integral, derviative, and feedforward terms. Updated _only_ by
+     *  instantiators of the PID class. Changes to this map will be immediately reflected in the next iteration of the
+     *  PID loop.
+     */
+    std::shared_ptr<PIDGainsMap> pid_gains;
+
+    /**
+     * @brief The proportional, integral, derviative, and feedforward terms that are summed to get the control effort.
+     *  Updated every iteration of the PID loop.
+     */
     std::shared_ptr<PIDGainsMap> pid_terms;
 
+    /**
+     * @brief Construct a new PID object with `pid_gains` and `pid_terms` set to `nullptr`.
+     */
     PID();
+
+    /**
+     * @brief Construct a new PID object with `pid_gains` and `pid_terms` set to the given pointers.
+     *
+     * @param pid_gains The gains that will be updated by instantiators of this class and used to weight the PID terms.
+     * @param pid_terms The terms that will be updated by this class and summed to get the control effort.
+     */
     PID(std::shared_ptr<PIDGainsMap> pid_gains, std::shared_ptr<PIDGainsMap> pid_terms);
 
+    /**
+     * @brief Reset PID loop in preparation for change in setpoint.
+    */
     void reset();
+
+    /**
+     * @brief Clip value between `min` and `max`. If `value` is less than `min`, return `min`. If `value` is greater
+     *  than `max`, return `max`. Otherwise, return `value`.
+     *
+     * @param value The value to clip.
+     * @param min The minimum value.
+     * @param max The maximum value.
+     * @return The clipped value, guaranteed to be between `min` and `max` (inclusive).
+     */
     double clip(const double value, const double min, const double max);
+
+    /**
+     * @brief Second order butterworth filter. Used to reduce noise and maintain stability.
+     *
+     * @param values The three most recent unfiltered values, in order from newest to oldest.
+     * @param filtered_values The three most recent filtered values, in order from newest to oldest. Only the second and
+     *  third values are used.
+     * @return The filtered value.
+     */
     double second_order_butterworth(const std::array<double, 3> &values, const std::array<double, 3> &filtered_values);
+
+    /**
+     * @brief Run one iteration of the PID loop. `pid_gains` and `pid_terms` must be set before calling this function.
+     *
+     * @param error The difference between the setpoint and the current value. If angular, this should be in radians in
+     *  the range [-pi, pi].
+     * @param delta_time The time elapsed since the last iteration of the PID loop. Must be positive.
+     * @return The control effort.
+     */
     double run_loop(double error, double delta_time);
 };
 
--- onboard/catkin_ws/src/controls/src/drc_pid.cpp
+++ onboard/catkin_ws/src/controls/src/drc_pid.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <ros/ros.h>
+
 #include "drc_pid.h"
 #include "controls_utils.h"
 
@@ -13,6 +14,7 @@ PID::PID(std::shared_ptr<PIDGainsMap> pid_gains, std::shared_ptr<PIDGainsMap> pi
 
 void PID::reset()
 {
+    // Reset integral to zero
     integral = 0.0;
 }
 
@@ -21,10 +23,11 @@ double PID::clip(const double value, const double min, const double max)
     return std::max(min, std::min(value, max));
 }
 
-// Implementation of second order butterworth filter from Julius O. Smith III, Intro. to Digital Filters With Audio Applications.
-// See https://ccrma.stanford.edu/~jos/filters/Example_Second_Order_Butterworth_Lowpass.html
 double PID::second_order_butterworth(const std::array<double, 3> &values, const std::array<double, 3> &filtered_values)
 {
+    // Reference: Julius O. Smith III, Intro. to Digital Filters With Audio Applications.
+    // See https://ccrma.stanford.edu/~jos/filters/Example_Second_Order_Butterworth_Lowpass.html for a description of
+    // the math. Implementation adapted from https://bitbucket.org/AndyZe/pid/src/master/src/pid.cpp#lines-271.
     return (1 / (1 + (c * c) + (1.414 * c))) *
            (values.at(2) + (2 * values.at(1)) + values.at(0) -
             ((c * c) - (1.414 * c) + 1) * filtered_values.at(2) -
@@ -35,9 +38,19 @@ double PID::run_loop(double error, double delta_time)
 {
     // If pid_gains is not set, return 0 and print error
     if (pid_gains == nullptr || pid_gains->size() == 0 || !pid_gains->count(PIDGainTypesEnum::KP) ||
-        !pid_gains->count(PIDGainTypesEnum::KI) || !pid_gains->count(PIDGainTypesEnum::KD))
+        !pid_gains->count(PIDGainTypesEnum::KI) || !pid_gains->count(PIDGainTypesEnum::KD) ||
+        !pid_gains->count(PIDGainTypesEnum::FF))
     {
         ROS_ERROR("PID gains not set.");
+        return 0;
+    }
+
+    // If pid_terms is not set, return 0 and print error
+    if (pid_terms == nullptr || pid_terms->size() == 0 || !pid_terms->count(PIDGainTypesEnum::KP) ||
+        !pid_terms->count(PIDGainTypesEnum::KI) || !pid_terms->count(PIDGainTypesEnum::KD) ||
+        !pid_terms->count(PIDGainTypesEnum::FF))
+    {
+        ROS_ERROR("PID terms not set.");
         return 0;
     }
 
@@ -48,12 +61,16 @@ double PID::run_loop(double error, double delta_time)
         return 0;
     }
 
+    // Update errors
+    // Don't need to deal with angular discontinuities as angular errors will always be in range [-pi, pi]
     errors.at(2) = errors.at(1);
     errors.at(1) = errors.at(0);
     errors.at(0) = error;
 
-    // Angular errors are in radians
-    // Don't need to deal with angular discontinuities as angles will always be in range [-pi, pi]
+    // Update filtered errors
+    filtered_errors.at(2) = filtered_errors.at(1);
+    filtered_errors.at(1) = filtered_errors.at(0);
+    filtered_errors.at(0) = second_order_butterworth(errors, filtered_errors);
 
     // Update integral
     integral += error * delta_time;
@@ -61,29 +78,24 @@ double PID::run_loop(double error, double delta_time)
     // Integral windup protection against sudden setpoint changes or unactuatable states
     integral = clip(integral, -integral_clamp, integral_clamp);
 
-    // Update filtered error
-    filtered_errors.at(2) = filtered_errors.at(1);
-    filtered_errors.at(1) = filtered_errors.at(0);
-    filtered_errors.at(0) = second_order_butterworth(errors, filtered_errors);
-
-    // Update derivative
+    // Update derivatives
     derivs.at(2) = derivs.at(1);
     derivs.at(1) = derivs.at(0);
     derivs.at(0) = (errors.at(0) - errors.at(1)) / delta_time;
 
-    // Update filtered derivative
+    // Update filtered derivatives
     filtered_derivs.at(2) = filtered_derivs.at(1);
     filtered_derivs.at(1) = filtered_derivs.at(0);
     filtered_derivs.at(0) = second_order_butterworth(derivs, filtered_derivs);
 
-    // Calculate PID output
+    // Calculate terms, weighted by their respective gains
     double p = pid_gains->at(PIDGainTypesEnum::KP) * filtered_errors.at(0);
     double i = pid_gains->at(PIDGainTypesEnum::KI) * integral;
     double d = pid_gains->at(PIDGainTypesEnum::KD) * filtered_derivs.at(0);
     double f = pid_gains->at(PIDGainTypesEnum::FF);
     double control_effort = p + i + d + f;
 
-    // Save values of PID terms
+    // Update term values
     pid_terms->at(PIDGainTypesEnum::KP) = p;
     pid_terms->at(PIDGainTypesEnum::KI) = i;
     pid_terms->at(PIDGainTypesEnum::KD) = d;
