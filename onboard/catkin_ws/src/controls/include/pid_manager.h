714d73e45af6eb0bdff9bae807b1e298ce064c94
Vedarsh Shah <vedarsh.shah@gmail.com>
2024-01-19 21:31:28 -0500

    Added comments to PID manager

--- onboard/catkin_ws/src/controls/include/pid_manager.h
+++ onboard/catkin_ws/src/controls/include/pid_manager.h
@@ -9,13 +9,40 @@
 class PIDManager
 {
 public:
+    /**
+     * @brief PID controllers for each axis
+     */
     std::unordered_map<AxesEnum, PID> pid_controllers;
 
+    /**
+     * @brief Default constructor
+     */
     PIDManager();
+
+    /**
+     * @brief Constructor
+     *
+     * @param pid_gains_for_axes Pointers to PID gains for each axis
+     * @param pid_terms_for_axes Pointers to PID terms for each axis
+     */
     PIDManager(AxesPIDGainsMap pid_gains_for_axes, AxesPIDGainsMap pid_terms_for_axes);
+
+    /**
+     * @brief Run PID loops
+     *
+     * @param errors Errors for each axis
+     * @param deltaTimes Delta times for each axis
+     * @param[out] outputs PID outputs for each axis
+     */
     void run_loops(const std::unordered_map<AxesEnum, double> &errors,
                    const std::unordered_map<AxesEnum, double> &deltaTimes,
                    std::unordered_map<AxesEnum, double> &outputs);
+
+    /**
+     * @brief Reset PID loop for a given axis
+     *
+     * @param axis_to_reset Axis to reset
+     */
     void reset(AxesEnum axis_to_reset);
 };
 
--- onboard/catkin_ws/src/controls/src/pid_manager.cpp
+++ onboard/catkin_ws/src/controls/src/pid_manager.cpp
@@ -5,14 +5,14 @@
 
 PIDManager::PIDManager()
 {
-    // Initialize PID controllers
+    // Initialize PID controllers with gains and terms as nullptr
     for (const AxesEnum &axis : AXES)
         pid_controllers[axis] = PID();
 }
 
 PIDManager::PIDManager(AxesPIDGainsMap pid_gains_for_axes, AxesPIDGainsMap pid_terms_for_axes)
 {
-    // Initialize PID controllers
+    // Initialize PID controllers with given gains and terms pointers
     for (const AxesEnum &axis : AXES)
         pid_controllers[axis] = PID(pid_gains_for_axes[axis], pid_terms_for_axes[axis]);
 }
@@ -21,13 +21,14 @@ void PIDManager::run_loops(const std::unordered_map<AxesEnum, double> &errors,
                            const std::unordered_map<AxesEnum, double> &deltaTimes,
                            std::unordered_map<AxesEnum, double> &outputs)
 {
-    // Run PID loops
+    // Run PID loops with given errors and delta times
+    // Store outputs in given outputs map
     for (const AxesEnum &axis : AXES)
         outputs[axis] = pid_controllers.at(axis).run_loop(errors.at(axis), deltaTimes.at(axis));
 }
 
 void PIDManager::reset(AxesEnum axis_to_reset)
 {
-    // Reset PID loop
+    // Reset PID loop at given axis
     pid_controllers.at(axis_to_reset).reset();
 }
