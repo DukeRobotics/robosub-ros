5e39b4e662649a5ecd8b6b979aaff8ee6e492a01
Vedarsh Shah <vedarsh.shah@gmail.com>
2024-01-24 20:31:08 -0500

    Made pose transforms more robust

--- onboard/catkin_ws/src/system_utils/scripts/pose_transforms.py
+++ onboard/catkin_ws/src/system_utils/scripts/pose_transforms.py
@@ -3,22 +3,67 @@
 from transforms3d.euler import quat2euler
 
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Vector3
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
 
 
 class PoseTransformData:
-    def __init__(self, input_topic, input_type, input_type_to_pose_function, output_topic):
+    def __init__(self, input_topic, input_type, input_type_conversion, output_topic, output_type,
+                 output_type_conversion):
         self.input_topic = input_topic
         self.input_type = input_type
-        self.input_type_to_pose_function = input_type_to_pose_function
+        self.input_type_conversion = input_type_conversion
         self.output_topic = output_topic
+        self.output_type = output_type
+        self.output_type_conversion = output_type_conversion
+
+
+def convert_pose_to_twist(pose_msg):
+    # Create a Twist message with identical linear position
+    twist_msg = Twist()
+    twist_msg.linear.x = pose_msg.position.x
+    twist_msg.linear.y = pose_msg.position.y
+    twist_msg.linear.z = pose_msg.position.z
+
+    # Convert quaternion to euler angles
+    euler_angles = quat2euler(
+        [pose_msg.orientation.x, pose_msg.orientation.y, pose_msg.orientation.z, pose_msg.orientation.w])
+
+    # Convert to degrees
+    euler_angles = [angle * 180 / 3.14159265359 for angle in euler_angles]
+
+    # Set angular position
+    twist_msg.angular.x = euler_angles[0]
+    twist_msg.angular.y = euler_angles[1]
+    twist_msg.angular.z = euler_angles[2]
+
+    return twist_msg
+
+
+def convert_quat_to_vector(quat_msg):
+    # Convert quaternion to euler angles
+    euler_angles = quat2euler(
+        [quat_msg.x, quat_msg.y, quat_msg.z, quat_msg.w])
+
+    # Convert to degrees
+    euler_angles = [angle * 180 / 3.14159265359 for angle in euler_angles]
+
+    vector_msg = Vector3()
+    vector_msg.x = euler_angles[0]
+    vector_msg.y = euler_angles[1]
+    vector_msg.z = euler_angles[2]
+
+    return vector_msg
 
 
 class PoseTransforms:
 
     POSE_TRANSFORM_DATA = [
-        PoseTransformData('/state', Odometry, lambda x: x.pose.pose, '/state/pose_transformed')
+        PoseTransformData('/state', Odometry, lambda x: x.pose.pose, '/state/pose_transformed', Twist,
+                          convert_pose_to_twist),
+        PoseTransformData('/vectornav/IMU', Imu, lambda x: x.orientation, '/vectornav/IMU/quat_transformed', Vector3,
+                          convert_quat_to_vector)
     ]
 
     def __init__(self):
@@ -30,33 +75,12 @@ class PoseTransforms:
         for data in self.POSE_TRANSFORM_DATA:
             self.subscribers[data.input_topic] = rospy.Subscriber(data.input_topic, data.input_type, self.callback,
                                                                   data)
-            self.publishers[data.output_topic] = rospy.Publisher(data.output_topic, Twist, queue_size=1)
+            self.publishers[data.output_topic] = rospy.Publisher(data.output_topic, data.output_type, queue_size=1)
 
     def callback(self, msg, data):
-        pose = data.input_type_to_pose_function(msg)
-        twist = self.convert_pose_to_twist(pose)
-        self.publishers[data.output_topic].publish(twist)
-
-    def convert_pose_to_twist(self, pose_msg):
-        # Create a Twist message with identical linear position
-        twist_msg = Twist()
-        twist_msg.linear.x = pose_msg.position.x
-        twist_msg.linear.y = pose_msg.position.y
-        twist_msg.linear.z = pose_msg.position.z
-
-        # Convert quaternion to euler angles
-        euler_angles = quat2euler(
-            [pose_msg.orientation.x, pose_msg.orientation.y, pose_msg.orientation.z, pose_msg.orientation.w])
-
-        # Convert to degrees
-        euler_angles = [angle * 180 / 3.14159265359 for angle in euler_angles]
-
-        # Set angular position
-        twist_msg.angular.x = euler_angles[0]
-        twist_msg.angular.y = euler_angles[1]
-        twist_msg.angular.z = euler_angles[2]
-
-        return twist_msg
+        converted_input_type = data.input_type_conversion(msg)
+        ouput_msg = data.output_type_conversion(converted_input_type)
+        self.publishers[data.output_topic].publish(ouput_msg)
 
     def run(self):
         rospy.spin()
