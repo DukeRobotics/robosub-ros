3b9f5dc67ebee4742c532b52aa753b7a30c794cf
Will Denton <willdenton@outlook.com>
2024-01-24 20:10:01 -0500

    re added everything

--- onboard/catkin_ws/src/sonar/scripts/sonar_pool_test.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar_pool_test.py
@@ -4,10 +4,6 @@ import rospy
 import sys
 from sonar_utils import degrees_to_centered_gradians
 
-# COMMAND TO SETUP SERVER:
-# python -m http.server
-# Search 192.168.1.1:8000 in google :)
-
 if __name__ == "__main__":
     rospy.init_node("local_pool_test")
     sonar = Sonar(5)
