b307fbaf5daac4e7c71ec48d56b8c72f06c25220
vedarshshah <33162616+vedarshshah@users.noreply.github.com>
2024-01-24 20:13:46 -0500

    Pressure Filter (#509)
    * Implemented median filter
    
    * Updated pressure ftdi string
    
    * Updated data_pub README
    
    * Updated pressure FTDI string in dual upload
    
    ---------
    
    Co-authored-by: Oogway <bot@duke-robotics.com>

--- onboard/catkin_ws/src/offboard_comms/scripts/dual_upload.sh
+++ onboard/catkin_ws/src/offboard_comms/scripts/dual_upload.sh
@@ -53,7 +53,7 @@ onboard/catkin_ws/src/offboard_comms/scripts/copy_offset.sh 0 # Remove the offse
 
 if [ "$ARD_UPLOAD" = true ]; then
     # E49AFA8B51514C4B39202020FF024242 is thruster arduino
-    # FAD98F6E51514C4B39202020FF020B42 is pressure arduino
+    # 3FE1330851544B5933202020FF070938 is pressure arduino
     if [ "$ARDUINO1" = "E49AFA8B51514C4B39202020FF024242" ]; then
         TEMP=$SRC_CODE1
         SRC_CODE1=$SRC_CODE2
