2d69f4e18b0f3d25cbcf8010c1eca137271f9b25
Patrick Zheng <pzheng111@gmail.com>
2024-01-25 20:47:07 -0500

    added circularity and working on making image creation work for all scan angle ranges

--- onboard/catkin_ws/src/sonar/scripts/sonar_image_processing2.py
+++ onboard/catkin_ws/src/sonar/scripts/sonar_image_processing2.py
@@ -13,8 +13,11 @@ def getImage(path):
     Returns:
         ndarray: image
     """
+    if(path.endswith(".npy")):
+        return np.load(path)
     img = Image.open(path)
     npImg = np.asarray(img)
+    
     return npImg
 
 
@@ -30,12 +33,24 @@ def createImage(path):
         ndarray: image
     """
     npImg = getImage(path)
-
     radius = npImg.shape[1]
-    polarImg = np.zeros((radius + 1, radius + 1, 3))
+    angle = npImg.shape[0]
+    width = 0
+    height = 0
+    if angle <= 101:
+        width = radius
+        height = radius
+    elif angle <=200:
+        width = 2*radius
+        height = radius
+    else:
+        width = 2*radius
+        height = 2*radius
+    
+    polarImg = np.zeros((width + 1, height + 1, 3))
 
-    x = np.linspace(0, radius, radius + 1)
-    y = np.linspace(0, radius, radius + 1)
+    x = np.linspace(0,width, width + 1)
+    y = np.linspace(0, height, height + 1)
     xx, yy = np.meshgrid(x, y)
 
     theta = np.rad2deg(np.arctan2(yy, xx))*200/180    
@@ -48,9 +63,9 @@ def createImage(path):
     theta = theta.astype(np.int32)
     r = r.astype(np.int32)
 
-    for j in range(radius):
-        for i in range(radius + 1):
-            polarImg[radius-j][i] = npImg[theta[j][i]][r[j][i]]/255
+    for j in range(width):
+        for i in range(height + 1):
+            polarImg[radius -j][i] = npImg[theta[j][i]][r[j][i]]/255
     
     return polarImg
 
@@ -71,39 +86,65 @@ def addContours(image, lower_bound=(0, 0.5, 0), upper_bound=(1, 1, 1), kernel=(1
 
     contours, hierarchy = cv2.findContours(blurred_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    shapes = []
     for i in range(len(contours)):
         moments = cv2.moments(contours[i])
         if moments['m00'] > area_threshold:
+            shapes.append(contours[i])
             cx = int(moments['m10'] / moments['m00'])
             cy = int(moments['m01'] / moments['m00'])
             cv2.circle(image, (cx, cy), 8, (255, 0, 0), -1)
             cv2.drawContours(image, contours, i, (255, 0, 0), 2)
+    printCirularity(shapes)
+
+def printCirularity(contours):
+
+    for con in contours:
+        perimeter = cv2.arcLength(con, True)
+        area = cv2.contourArea(con)
+        if perimeter == 0:
+            break
+        circularity = 4*np.pi*(area/perimeter*perimeter)
+        print(circularity)
+
+       
 
 
 def main():
-    path1 = 'onboard/catkin_ws/src/sonar/scripts/sampleData/Sonar_Image.jpeg'
+    path1 = 'onboard/catkin_ws/src/sonar/scripts/sampleData/buoy.npy'
     path2 = 'onboard/catkin_ws/src/sonar/scripts/sampleData/Sonar_Image2.jpeg'
     path3 = 'onboard/catkin_ws/src/sonar/scripts/sampleData/Sonar_Image3.jpeg'
 
     paths = [path1, path2, path3]
     images = []
+    for path in paths:
+        image = getImage(path)
+        print(image.shape)
+        images.append(image)
+    
+    fig, ax = plt.subplots(1, len(images))
 
     for path in paths:
         image = createImage(path)
-        addContours(image)
+        #addContours(image)
         images.append(image)
+    
+    ax[0].imshow(images[0])
 
-    fig, ax = plt.subplots(1, len(images))
+    np.save("savedimg.npy", images[3])
+    ax[1].imshow(np.load("savedimg.npy"))
+    
+    # fig, ax = plt.subplots(1, len(images))
 
-    tr = transforms.Affine2D().rotate_deg(135)
+    # tr = transforms.Affine2D().rotate_deg(135)
 
-    for i in range(len(images)):
-        ax[i].imshow(images[i], transform=tr+ax[i].transData)
+    # for i in range(len(images)):
+    #     ax[i].imshow(images[i], transform=tr+ax[i].transData)
         
-        ax[i].set_xlim(-1*images[i].shape[0]*np.sqrt(2), 0)
-        ax[i].set_ylim(-1*images[i].shape[0]*np.sqrt(2)/2, images[i].shape[0]*np.sqrt(2)/2)
+    #     ax[i].set_xlim(-1*images[i].shape[0]*np.sqrt(2), 0)
+    #     ax[i].set_ylim(-1*images[i].shape[0]*np.sqrt(2)/2, images[i].shape[0]*np.sqrt(2)/2)
 
-        ax[i].set_aspect('equal', 'box')
+    #     ax[i].set_aspect('equal', 'box')
     
     plt.show()
 
