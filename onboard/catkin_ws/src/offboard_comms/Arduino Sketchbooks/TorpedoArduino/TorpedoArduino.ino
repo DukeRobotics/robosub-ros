449de87fbd929cfefeb2a2c3de230ea217d36ed3
Joyce Hu <jh829@duke.edu>
2024-02-08 19:19:32 -0500

    uploaded torpedo servo code

---
+++ onboard/catkin_ws/src/offboard_comms/Arduino Sketchbooks/TorpedoArduino/TorpedoArduino.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>
+
+Servo myservo;
+
+unsigned long myTime;
+boolean servoMoved = false;
+unsigned long servoTime;
+unsigned long rateServo = 1000;  // how much to wait before returning servo to default position
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(9);  // servo pin 9
+  myservo.write(90);
+}
+
+void loop() {
+  myTime = millis();
+  
+  if (Serial.available()) {
+    char state = Serial.read();
+
+    if (state == 'L') {
+        myservo.write(160);
+        servoMoved = true;
+        servoTime = myTime;
+      }
+      if (state == 'R') {
+        myservo.write(20);
+        servoMoved = true;
+        servoTime = myTime;
+      }
+  }
+  
+  if(servoMoved && myTime - servoTime > rateServo) {
+      myservo.write(90);
+      servoMoved = false;
+    }
+}
