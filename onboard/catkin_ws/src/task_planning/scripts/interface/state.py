18e18cc3286cbab7745f8d2ab3c418b601e5a9e4
Raul Galvez <rg288@duke.edu>
2024-02-29 20:03:41 -0500

    Added a reset pose function to state.py using PoseWithCovarianceStamped

--- onboard/catkin_ws/src/task_planning/scripts/interface/state.py
+++ onboard/catkin_ws/src/task_planning/scripts/interface/state.py
@@ -2,14 +2,22 @@ import rospy
 import os
 import yaml
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseWithCovarianceStamped
 
 class StateInterface:
     STATE_TOPIC = 'state'
+    RESET_POSE_SERVICE = '/set_pose'
+
 
     def __init__(self, tfBuffer):
+
         self.tfBuffer = tfBuffer
         rospy.Subscriber(self.STATE_TOPIC, Odometry, self._on_receive_state) 
         self._state = None
+
+        rospy.wait_for_service(self.RESET_POSE_SERVICE)
+        self._reset_pose = rospy.ServiceProxy(self.RESET_POSE_SERVICE, PoseWithCovarianceStamped)
+
 
     @property
     def state(self):
@@ -17,6 +25,13 @@ class StateInterface:
 
     def _on_receive_state(self, state):
         self._state = state
-        
+
     def get_state(self):
         return self._state
+    
+    def reset_pose(self):
+        poseCov = PoseWithCovarianceStamped()
+        poseCov.pose.pose.orientation.w = 1
+        self._reset_pose(poseCov)
+        
+        return
