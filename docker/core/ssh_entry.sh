3749d3593ba2e7530d91a6a0c840f1f92d6edc1b
AndrewCouncil <andrew.p.council@gmail.com>
2024-03-03 17:58:18 -0500

    moved comment location

--- docker/core/ssh_entry.sh
+++ docker/core/ssh_entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# We know this directory exists, since it was created in the dockerfile
 # shellcheck disable=SC2164
 
 source /root/ros_bashrc.sh
 
+# We know this directory exists, since it was created in the dockerfile
 cd /root/dev/robosub-ros
