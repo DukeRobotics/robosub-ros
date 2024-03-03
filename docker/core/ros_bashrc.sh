595680db6205ef3a8e3eadbd0fb6a7395df07bd9
AndrewCouncil <andrew.p.council@gmail.com>
2024-03-03 17:32:10 -0500

    add sourcing and aliases to bashrc not bash_profile

---
+++ docker/core/ros_bashrc.sh
@@ -0,0 +1,17 @@
+# To be appended to /root/.bashrc
+source /opt/ros/noetic/setup.bash
+
+if [ -f /opt/ros/noetic/setup_network.bash ]
+then
+    source /opt/ros/noetic/setup_network.bash
+fi
+
+if [ -f "${COMPUTER_TYPE}"/catkin_ws/devel/setup.bash ]
+then
+    source "${COMPUTER_TYPE}"/catkin_ws/devel/setup.bash
+fi
+
+# Alias to start foxglove websocket
+alias fg-ws="roslaunch --screen foxglove_bridge foxglove_bridge.launch port:=8765"
+alias thruster-test="rostopic pub -r 20 /controls/thruster_allocs custom_msgs/ThrusterAllocs '{allocs: [0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05]}'"
+
