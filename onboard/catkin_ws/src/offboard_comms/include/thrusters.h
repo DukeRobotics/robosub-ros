f59157c28d2f75b665af7ac63ee73167b9cf41da
Vedarsh Shah <vedarsh.shah@gmail.com>
2024-03-06 19:34:18 -0500

    Formatted thrusters cpp files

--- onboard/catkin_ws/src/offboard_comms/include/thrusters.h
+++ onboard/catkin_ws/src/offboard_comms/include/thrusters.h
@@ -1,10 +1,11 @@
 #ifndef THRUSTERS_H
 #define THRUSTERS_H
 
-#include <array>
+#include <custom_msgs/ThrusterAllocs.h>
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
-#include <custom_msgs/ThrusterAllocs.h>
+
+#include <array>
 
 // Number of entries in each lookup table
 const int NUM_LOOKUP_ENTRIES = 201;
@@ -13,29 +14,28 @@ const int NUM_LOOKUP_ENTRIES = 201;
 const double VOLTAGE_LOWER = 14.0;
 const double VOLTAGE_UPPER = 18.0;
 
-class Thrusters
-{
-private:
-  double voltage;
-  std::array<uint16_t, NUM_LOOKUP_ENTRIES> v14_lookup_table;
-  std::array<uint16_t, NUM_LOOKUP_ENTRIES> v16_lookup_table;
-  std::array<uint16_t, NUM_LOOKUP_ENTRIES> v18_lookup_table;
+class Thrusters {
+   private:
+    double voltage;
+    std::array<uint16_t, NUM_LOOKUP_ENTRIES> v14_lookup_table;
+    std::array<uint16_t, NUM_LOOKUP_ENTRIES> v16_lookup_table;
+    std::array<uint16_t, NUM_LOOKUP_ENTRIES> v18_lookup_table;
 
-  ros::Subscriber thruster_allocs_sub;
-  ros::Subscriber voltage_sub;
+    ros::Subscriber thruster_allocs_sub;
+    ros::Subscriber voltage_sub;
 
-  ros::Publisher pwm_pub;
+    ros::Publisher pwm_pub;
 
-  void load_lookup_tables();
-  void read_lookup_table_csv(const std::string &filename, std::array<uint16_t, NUM_LOOKUP_ENTRIES> &lookup_table);
-  void voltage_callback(const std_msgs::Float64 &msg);
-  void thruster_allocs_callback(const custom_msgs::ThrusterAllocs &msg);
-  double lookup(double force);
-  double interpolate(double x1, uint16_t y1, double x2, uint16_t y2, double x_interpolate);
-  int round_to_two_decimals(double num);
+    void load_lookup_tables();
+    void read_lookup_table_csv(const std::string &filename, std::array<uint16_t, NUM_LOOKUP_ENTRIES> &lookup_table);
+    void voltage_callback(const std_msgs::Float64 &msg);
+    void thruster_allocs_callback(const custom_msgs::ThrusterAllocs &msg);
+    double lookup(double force);
+    double interpolate(double x1, uint16_t y1, double x2, uint16_t y2, double x_interpolate);
+    int round_to_two_decimals(double num);
 
-public:
-  Thrusters(int argc, char **argv, ros::NodeHandle &nh);
+   public:
+    Thrusters(int argc, char **argv, ros::NodeHandle &nh);
 };
 
 #endif
