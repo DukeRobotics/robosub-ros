4e5e0b3f907f866e94a714840a05f999365f9295
viethungle-vt1401 <brandon.viethungRI2001@gmail.com>
2024-03-23 12:51:04 -0400

    linting usb_camera_connect.py

--- onboard/catkin_ws/src/cv/scripts/usb_camera_connect.py
+++ onboard/catkin_ws/src/cv/scripts/usb_camera_connect.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import rospy
-import roslaunch
-
 import yaml
 import resource_retriever as rr
 
@@ -12,10 +10,9 @@ CAMERA_CONFIG_PATH = 'package://cv/configs/usb_cameras.yaml'
 
 
 def connect_all():
-    
     with open(rr.get_filename(CAMERA_CONFIG_PATH, use_protocol=False)) as f:
         cameras = yaml.safe_load(f)
-    
+
     for _, data in cameras.items():
 
         channel = data["channel"]
@@ -23,6 +20,7 @@ def connect_all():
 
         # Set framerate to -1 to bypass reading rosparam
         USBCamera(topic=topic, channel=channel, framerate=-1)
+
 
 if __name__ == "__main__":
     rospy.init_node("usb_camera_connect")
