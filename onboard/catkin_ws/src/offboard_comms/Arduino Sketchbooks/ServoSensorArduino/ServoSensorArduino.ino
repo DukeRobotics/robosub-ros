4121cd5f79df0a87b59203c686588a226d261338
Joyce Hu <jh829@duke.edu>
2024-03-31 00:21:24 -0400

    added temp/humidity sensor to arduino code

--- onboard/catkin_ws/src/offboard_comms/Arduino Sketchbooks/ServoSensorArduino/ServoSensorArduino.ino
+++ onboard/catkin_ws/src/offboard_comms/Arduino Sketchbooks/ServoSensorArduino/ServoSensorArduino.ino
@@ -1,16 +1,28 @@
+// from L to R: data (pin 4), power (5V), ground
 #include <Servo.h>
+#include <dht11.h>
+#define DHT11PIN 4  // digital pin 4
 
+dht11 DHT11;
 Servo myservo;
 
 unsigned long myTime;
 boolean servoMoved = false;
 unsigned long servoTime;
 unsigned long rateServo = 1000;  // how much to wait before returning servo to default position
+unsigned long rateTemp = 0;  // how often to print temp/humidity (no delay)
+unsigned long prevTime = 0; 
+String humiditytag = "H:";
+String temperaturetag = "T:";
+String printHumidity = "";
+String printTemp = "";
+
 
 void setup() {
   Serial.begin(9600);
   myservo.attach(9);  // servo pin 9
-  myservo.write(90);
+  myservo.writeMicroseconds(1500); // 1500 micros = 90 write
+
 }
 
 void loop() {
@@ -23,12 +35,12 @@ void loop() {
     }
 
     if (state == 'L') {
-        myservo.write(160);
+        myservo.writeMicroseconds(1250);  // 1200 = 90 degrees left  // 1250 micros =  20 write
         servoMoved = true;
         servoTime = myTime;
       }
       if (state == 'R') {
-        myservo.write(20);
+        myservo.writeMicroseconds(1750);  // 1800 = 90 degrees right // 1750 micros = 160 write
         servoMoved = true;
         servoTime = myTime;
       }
@@ -36,7 +48,19 @@ void loop() {
   
   // return to default position after 1 second
   if(servoMoved && myTime - servoTime > rateServo) {
-      myservo.write(90);
+      myservo.writeMicroseconds(1500);  // 1500 micros = 90 write
       servoMoved = false;
     }
+
+  // printing humidity and temperature
+  if(myTime - prevTime >= rateTemp) {
+    prevTime = myTime;
+    int chk = DHT11.read(DHT11PIN);
+
+    printHumidity = humiditytag + String((float)DHT11.humidity);
+    Serial.println(printHumidity);
+
+    printTemp = temperaturetag + String((float)DHT11.temperature * 1.8 + 32);
+    Serial.println(printTemp);
+  }
 }
