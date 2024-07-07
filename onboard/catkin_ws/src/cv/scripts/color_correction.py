ce731183498126f93156cc89cc32671e9abb6041
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-07-07 12:20:51 -0700

    Add color_correction script

---
+++ onboard/catkin_ws/src/cv/scripts/color_correction.py
@@ -0,0 +1,13 @@
+"""
+This script performs underwater color correction on images from a DepthAI camera.
+It must be run on the DepthAI device itself, using a script node in the pipeline.
+See depthai_spatial_detection.py for an example.
+"""
+
+# pyright: reportMissingImports=false, reportUnusedImport=false, reportUnboundVariable=false, reportUndefinedVariable=false
+# flake8: noqa
+
+while True:
+    node.warn('Performing NN inferencing')
+    frame = node.io['rgb'].get()
+    node.io['nn'].send(frame)
