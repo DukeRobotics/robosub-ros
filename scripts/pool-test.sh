6a745df428ad91f9405db80d67dd94fb19de219e
DRC Lenovo Laptop <hello@duke-robotics.com>
2024-08-05 12:22:24 -0700

    Delete docker pool test since pool test now runs docker container

--- scripts/pool-test.sh
+++ scripts/pool-test.sh
@@ -25,26 +25,32 @@ tmux send-keys 'tmux kill-server'
 
 tmux select-pane -t 2
 tmux send-keys 'ssh robot@192.168.1.1' 'Enter'
+tmux send-keys 'onboard' 'Enter'
+tmux send-keys 'sleep 0.5' C-m # need shorter delay here so that roscore is started first
 tmux send-keys 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
 tmux send-keys 'roscore' 'Enter'
 
 tmux select-pane -t 4
 tmux send-keys 'ssh robot@192.168.1.1' 'Enter'
+tmux send-keys 'sleep 1' C-m
 tmux send-keys 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
 tmux send-keys 'fg-ws' 'Enter'
 
 tmux select-pane -t 1
 tmux send-keys 'ssh robot@192.168.1.1' 'Enter'
+tmux send-keys 'sleep 1' C-m
 tmux send-keys 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
 tmux send-keys 'roslaunch execute motion.launch enable_recording:=true'
 
 tmux select-pane -t 3
 tmux send-keys 'ssh robot@192.168.1.1' 'Enter'
+tmux send-keys 'sleep 1' C-m
 tmux send-keys 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
 tmux send-keys 'roslaunch task_planning task_runner.launch'
 
 tmux select-pane -t 5
 tmux send-keys 'ssh robot@192.168.1.1' 'Enter'
+tmux send-keys 'sleep 1' C-m
 tmux send-keys 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
 tmux send-keys 'rosservice call /controls/enable" True'
 
