98781bfa0df03a0b34774c693ef198c3cfbb9224
DRC Lenovo Laptop <hello@duke-robotics.com>
2024-08-05 10:17:31 -0700

    Update pool-test.sh

--- scripts/pool-test.sh
+++ scripts/pool-test.sh
@@ -14,7 +14,6 @@ tmux split-window -v -l 100
 tmux split-window -v -l 83
 tmux split-window -v -l 66
 tmux split-window -v -l 50
-tmux split-window -v -l 33
 
 tmux select-pane -t 0
 tmux send-keys 'ssh robot@192.168.1.1' 'Enter'
@@ -28,22 +27,17 @@ tmux send-keys 'roscore' 'Enter'
 tmux select-pane -t 2
 tmux send-keys 'ssh robot@192.168.1.1' 'Enter'
 tmux send-keys 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
-tmux send-keys 'roslaunch execute motion.launch'
+tmux send-keys 'fg-ws' 'Enter'
 
 tmux select-pane -t 3
 tmux send-keys 'ssh -XY robot@192.168.1.1' 'Enter'
 tmux send-keys 'sshpass -p robotics ssh -XY -p 2201 root@192.168.1.1' 'Enter'
-tmux send-keys 'roslaunch gui cv_gui.launch'
+tmux send-keys 'roslaunch execute motion.launch enable_recording:=true'
 
 tmux select-pane -t 4
 tmux send-keys 'ssh robot@192.168.1.1' 'Enter'
 tmux send-keys 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
-tmux send-keys 'rosservice call /enable_controls True'
-
-tmux select-pane -t 5
-tmux send-keys 'ssh robot@192.168.1.1' 'Enter'
-tmux send-keys 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
-tmux send-keys './record.sh .bag'
+tmux send-keys 'roslaunch task_planning task_runner.launch'
 
 tmux select-pane -t 6
 tmux send-keys 'ssh robot@192.168.1.1' 'Enter'
@@ -53,7 +47,7 @@ tmux send-keys 'rostopic echo /state'
 tmux select-pane -t 7
 tmux send-keys 'ssh robot@192.168.1.1' 'Enter'
 tmux send-keys 'sshpass -p robotics ssh -p 2200 root@192.168.1.1' 'Enter'
-tmux send-keys 'rosrun controls test_state_publisher.py'
+tmux send-keys 'rosservice call /controls/enable" True'
 
 tmux select-pane -t 1
 
