5bdf4eeb0e2c961df90dc16106b98d4b068227a3
Michael Bryant <mbryant2025@gmail.com>
2024-08-06 13:25:58 -0700

    saleae connect

--- onboard/catkin_ws/src/acoustics/scripts/record.py
+++ onboard/catkin_ws/src/acoustics/scripts/record.py
@@ -1,5 +1,28 @@
 import saleae
+import signal
 
-s = saleae.Saleae(args='-disablepopups socket')
+class TimeoutException(Exception):
+    pass
 
-print('Saleae connected')
+def timeout_handler(signum, frame):
+    raise TimeoutException
+
+# Set the timeout duration (in seconds)
+timeout_duration = 5
+
+# Register the timeout handler
+signal.signal(signal.SIGALRM, timeout_handler)
+signal.alarm(timeout_duration)
+
+try:
+    s = saleae.Saleae(args='-disablepopups socket')
+    print('Saleae connected')
+    # Disable the alarm after successful connection
+    signal.alarm(0)
+except TimeoutException:
+    print('Failed to connect to Saleae: Connection timed out')
+except Exception as e:
+    print(f'Failed to connect to Saleae: {e}')
+    # Disable the alarm in case of other exceptions
+    signal.alarm(0)
+    
