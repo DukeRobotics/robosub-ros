60a6046cb63b7378e5b7d670cbaa18d7168e3c81
Michael Bryant <mbryant2025@gmail.com>
2024-08-06 14:49:09 -0700

    more channels

--- onboard/catkin_ws/src/acoustics/scripts/record.py
+++ onboard/catkin_ws/src/acoustics/scripts/record.py
@@ -17,7 +17,7 @@ signal.alarm(timeout_duration)
 
 try:
     s = saleae.Saleae(args='-disablepopups socket')
-    s.set_active_channels([], [0, 1, 2, 3, 4, 5, 6, 7])
+    # s.set_active_channels([], [0, 1, 2, 3, 4, 5, 6, 7])
     s.set_sample_rate(s.get_all_sample_rates()[3]) #6.25 MS/s
     print('Saleae connected')
     # Disable the alarm after successful connection
@@ -35,7 +35,7 @@ def record(seconds: int, filename: str='data') -> bool:
         s.set_capture_seconds(seconds)
         s.capture_start_and_wait_until_finished()
         # Export as binary
-        s.export_data2(f'{filename}.bin', analog_channels=[0, 1, 2, 3], format='binary')
+        s.export_data2(f'{filename}.bin', analog_channels=[0, 1, 2, 3, 4, 5, 6, 7], format='csv')
         print(f'Data recorded to {filename}.bin')
         return True
     except Exception as e:
