c0b1b5ad0108dcc11f69fa9970a4f28e86761e2d
Ethan Horowitz <ethan.horowitz@duke.edu>
2024-08-06 12:28:30 -0700

    added end timer

--- onboard/catkin_ws/src/acoustics/scripts/localization.py
+++ onboard/catkin_ws/src/acoustics/scripts/localization.py
@@ -1,6 +1,5 @@
 import rospy
 from std_msgs.msg import Int8
-import random
 from spike_detection import PipelineConfig, spikes_pipeline, get_first_hydrophones
 
 CENTER_FREQUENCY = 35000
@@ -22,11 +21,17 @@ def localization_pub():
         spikes = spikes_pipeline('./savefiles', config)
         firsts = get_first_hydrophones(spikes, config)
         
-        counts = [0, 0, 0, 0]
-        for i in firsts:
-            counts[i] += 1
+        times = [s.time for s in spikes[-1]]
+        range = max(times) - min(times)
+        if range < 1e-5:
+            counts = -1
+        else:
+            counts = [0, 0, 0, 0]
+            for i in firsts:
+                counts[i] += 1
+            localization = counts.index(max(counts))
             
-        localization = counts.index(max(counts))
+        
         
         rospy.loginfo(localization)
         pub.publish(localization)
