9441fedc38a5748a51b1b470b08439f4652668e6
Michael Bryant <mbryant2025@gmail.com>
2024-08-07 09:24:14 -0700

    finish

--- onboard/catkin_ws/src/acoustics/scripts/saleae.sh
+++ onboard/catkin_ws/src/acoustics/scripts/saleae.sh
@@ -7,17 +7,22 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+function finish {
+  killall Logic
+  killall Xvfb
+  killall xvfb-run
+}
+
 # If we pass in a 1, then we start the Saleae Logic software
 if [ $1 == 1 ] 
 then
-  xvfb-run Logic --automation --no-sandbox
+    finish
+    xvfb-run Logic --automation --no-sandbox
 fi
 
 # If we pass in a 0, then we stop the Saleae Logic software
 if [ $1 == 0 ] 
 then
-  killall Logic
-  killall Xvfb
-  killall xvfb-run
+    finish
 fi
 
